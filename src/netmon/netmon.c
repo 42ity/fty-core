172e60aaa6aab31745d8a4691bd1d6ed8e55ee28
Michal Vyskocil <michalvyskocil@eaton.com>
2014-07-23 08:34:36 +0200

    netmon: network monitoring component
     * does listen on Linux netlink interface and reports the state of
       interfaces and IP addresses
    
    Example:
    type ./netmon
    //this output will appear immediately and lists already configured interfaces
    ([NEWADDR], enp0s25, inet, 10.130.38.187/24, a0:1d:48:b7:e2:4e)
    ([NEWADDR], enp0s25, inet6, fe80::a21d:48ff:feb7:e24e/64, a0:1d:48:b7:e2:4e)
    //to trigger the interface change, type (on another console)
    //sudo ip link set enp0s25 down
    //and following text will appear
    ([DELLADDR], enp0s25, inet6, fe80::a21d:48ff:feb7:e24e/64, a0:1d:48:b7:e2:4e)
    ([DELLADDR], enp0s25, inet, 10.130.38.187/24, a0:1d:48:b7:e2:4e)
    //bring interface back
    //sudo ip link set enp0s25 up
    ([DELLADDR], enp0s25, inet6, fe80::a21d:48ff:feb7:e24e/64, a0:1d:48:b7:e2:4e)
    ([NEWADDR], enp0s25, inet6, fe80::a21d:48ff:feb7:e24e/64, a0:1d:48:b7:e2:4e)
    ([NEWADDR], enp0s25, inet, 10.130.38.187/24, a0:1d:48:b7:e2:4e)
    
    Signed-off-by: Michal Vyskocil <michalvyskocil@eaton.com>

---
+++ src/netmon/netmon.c
@@ -0,0 +1,695 @@
+/*
+ * netmon.c		"ip monitor".
+ *
+ *		This program is free software; you can redistribute it and/or
+ *		modify it under the terms of the GNU General Public License
+ *		as published by the Free Software Foundation; either version
+ *		2 of the License, or (at your option) any later version.
+ *
+ * Authors:	Alexey Kuznetsov, <kuznet@ms2.inr.ac.ru>
+ *
+ * Retrieved from: https://github.com/infoburp/iproute2.git
+ * Adapted for BIOS-247, MichalVyskocil, <michalvyskocil@eaton.com>
+ *
+ */
+
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <asm/types.h>
+#include <libnetlink.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <linux/if.h>
+#include <linux/if_arp.h>
+#include <linux/if_packet.h>
+#include <linux/if_ether.h>
+#include <linux/netlink.h>
+#include <linux/rtnetlink.h>
+#include<linux/netdevice.h>
+
+//#include "log.h"
+
+// include/utils.h
+#define SPRINT_BSIZE 64
+#define SPRINT_BUF(x)	char x[SPRINT_BSIZE]
+#define DN_MAXADDL 20
+
+// ip/ip_utils.h
+#ifndef	INFINITY_LIFE_TIME
+#define     INFINITY_LIFE_TIME      0xFFFFFFFFU
+#endif
+
+// ip/ipaddr.c
+struct rtnl_handle rth = { .fd = -1 };
+//char * _SL_ = NULL;
+#define _SL_ "\n"
+
+// ip/ipaddress.c
+// by GDB
+static int filter_family = 0;
+// ip a s scope link
+static int filter_scope = 253;
+static int filter_scopemask = -1;
+static int preferred_family = 0;
+
+
+// include/utils.h
+struct dn_naddr
+{
+        unsigned short          a_len;
+        unsigned char a_addr[DN_MAXADDL];
+};
+
+// ip/ipaddress.c
+struct nlmsg_list
+{
+	struct nlmsg_list *next;
+	struct nlmsghdr	  h;
+};
+
+// ip/ipaddress.c
+struct nlmsg_chain
+{
+	struct nlmsg_list *head;
+	struct nlmsg_list *tail;
+};
+
+// lib/ll_map.c
+const char *ll_index_to_name(unsigned idx);
+
+// include/rt_names.h
+const char *ll_addr_n2a(unsigned char *addr, int alen,
+			int type, char *buf, int blen);
+
+// lib/ll_addr.c
+const char *ll_addr_n2a(unsigned char *addr, int alen, int type, char *buf, int blen)
+{
+	int i;
+	int l;
+
+	if (alen == 4 &&
+	    (type == ARPHRD_TUNNEL || type == ARPHRD_SIT || type == ARPHRD_IPGRE)) {
+		return inet_ntop(AF_INET, addr, buf, blen);
+	}
+	if (alen == 16 && type == ARPHRD_TUNNEL6) {
+		return inet_ntop(AF_INET6, addr, buf, blen);
+	}
+	l = 0;
+	for (i=0; i<alen; i++) {
+		if (i==0) {
+			snprintf(buf+l, blen, "%02x", addr[i]);
+			blen -= 2;
+			l += 2;
+		} else {
+			snprintf(buf+l, blen, ":%02x", addr[i]);
+			blen -= 3;
+			l += 3;
+		}
+	}
+	return buf;
+}
+
+// include/utils.h
+static inline __u32 nl_mgrp(__u32 group)
+{
+	if (group > 31 ) {
+		fprintf(stderr, "Use setsockopt for this group %d\n", group);
+		exit(-1);
+	}
+	return group ? (1 << (group - 1)) : 0;
+}
+
+// ip/ipaddress.c
+static unsigned int get_ifa_flags(struct ifaddrmsg *ifa,
+				  struct rtattr *ifa_flags_attr)
+{
+	return ifa_flags_attr ? rta_getattr_u32(ifa_flags_attr) :
+				ifa->ifa_flags;
+}
+
+// lib/utils.c
+const char *rt_addr_n2a(int af, int len, const void *addr, char *buf, int buflen)
+{
+	switch (af) {
+	case AF_INET:
+	case AF_INET6:
+		return inet_ntop(af, addr, buf, buflen);
+/*
+ *MVY: stripped obscure protocols
+	case AF_IPX:
+		return ipx_ntop(af, addr, buf, buflen);
+	case AF_DECnet:
+	{
+		struct dn_naddr dna = { 2, { 0, 0, }};
+		memcpy(dna.a_addr, addr, 2);
+		return dnet_ntop(af, &dna, buf, buflen);
+	}
+*/
+	default:
+		return "???";
+	}
+}
+
+// lib/utils.c
+const char *format_host(int af, int len, const void *addr,
+			char *buf, int buflen)
+{
+#ifdef RESOLVE_HOSTNAMES
+	if (resolve_hosts) {
+		const char *n;
+
+		if (len <= 0) {
+			switch (af) {
+			case AF_INET:
+				len = 4;
+				break;
+			case AF_INET6:
+				len = 16;
+				break;
+			case AF_IPX:
+				len = 10;
+				break;
+#ifdef AF_DECnet
+			/* I see no reasons why gethostbyname
+			   may not work for DECnet */
+			case AF_DECnet:
+				len = 2;
+				break;
+#endif
+			default: ;
+			}
+		}
+		if (len > 0 &&
+		    (n = resolve_address(addr, len, af)) != NULL)
+			return n;
+	}
+#endif
+	return rt_addr_n2a(af, len, addr, buf, buflen);
+}
+
+// MVY: quick and dirty mac address resolution - as I've kicked out most of the code dealing
+//      with link, this is a code returns the mac for given ethname from /sysfs
+//      not thread safe, never ever use or copy this code!!!
+const char *qd_mac(const char* ethname) {
+    static char mac[MAX_ADDR_LEN];
+    char *path;
+    ssize_t r;
+    int fd;
+    
+    r = asprintf(&path, "/sys/class/net/%s/address", ethname);
+    if (!r) {
+        //TODO logging
+        fprintf(stderr, "BUG: can't allocate string");
+        return NULL;
+    }
+
+    fd = open(path, O_RDONLY);
+    if (!fd) {
+        //TODO logging
+        fprintf(stderr, "BUG: can't open /sys/class/net/%s/address", ethname);
+        free(path);
+        return NULL;
+    }
+
+    r = read(fd, mac, MAX_ADDR_LEN);
+    mac[r-1] = '\0'; // kill \n
+    free(path);
+    close(fd);
+    return (const char*) mac;
+}
+
+static int print_addrinfo(const struct sockaddr_nl *who,
+		      struct nlmsghdr *n, void *arg) {
+	FILE *fp = (FILE*)arg;
+	struct ifaddrmsg *ifa = NLMSG_DATA(n);
+	//struct ifinfomsg *ifi = NLMSG_DATA(n);
+	int len = n->nlmsg_len;
+	int deprecated = 0;
+	/* Use local copy of ifa_flags to not interfere with filtering code */
+	unsigned int ifa_flags;
+	struct rtattr * rta_tb[IFA_MAX+1];
+	char abuf[256];
+	SPRINT_BUF(b1);
+
+    //MVY: what we want to have
+    //MVY: I now it, however not sure why it happens, ll_name_to_index does return const char*
+    const char *ethname = ll_index_to_name(ifa->ifa_index);
+    const char* ipfamily = NULL;
+    const char *ipaddress = NULL;
+    int prefixlen = 0;
+    const char *mac = qd_mac(ethname);
+	
+    // sanity check
+    if (n->nlmsg_type != RTM_NEWADDR && n->nlmsg_type != RTM_DELADDR)
+		return 0;
+
+    const char *type = n->nlmsg_type == RTM_NEWADDR ? "NEWADDR" : "DELLADDR";
+	
+    len -= NLMSG_LENGTH(sizeof(*ifa));
+	if (len < 0) {
+        //TODO change to logging interface!
+		fprintf(stderr, "BUG: wrong nlmsg len %d\n", len);
+		return -1;
+	}
+
+    switch (ifa->ifa_family) {
+        case AF_INET:
+            ipfamily = "inet";
+            break;
+        case AF_INET6:
+            ipfamily = "inet6";
+            break;
+        default:
+            fprintf(stderr, "WARNING: unsupported family\n");
+            return 0;
+    }
+    
+    //this is needed to have correct ip addresses...
+    parse_rtattr(rta_tb, IFA_MAX, IFA_RTA(ifa),
+		     n->nlmsg_len - NLMSG_LENGTH(sizeof(*ifa)));
+
+    if (!rta_tb[IFA_LOCAL])
+		rta_tb[IFA_LOCAL] = rta_tb[IFA_ADDRESS];
+	if (!rta_tb[IFA_ADDRESS])
+		rta_tb[IFA_ADDRESS] = rta_tb[IFA_LOCAL];
+	
+    /*if (n->nlmsg_type == RTM_DELADDR)
+		fprintf(fp, "Deleted ");*/
+		
+    //fprintf(fp, "%u: %s", ifa->ifa_index, ll_index_to_name(ifa->ifa_index));
+
+	/*if (ifa->ifa_family == AF_INET)
+		fprintf(fp, "    inet ");
+	else if (ifa->ifa_family == AF_INET6)
+		fprintf(fp, "    inet6 ");
+	else if (ifa->ifa_family == AF_DECnet)
+		fprintf(fp, "    dnet ");
+	else if (ifa->ifa_family == AF_IPX)
+		fprintf(fp, "     ipx ");
+	else
+		fprintf(fp, "    family %d ", ifa->ifa_family);
+    */
+
+	if (rta_tb[IFA_LOCAL]) {
+		ipaddress = format_host(ifa->ifa_family,
+					      RTA_PAYLOAD(rta_tb[IFA_LOCAL]),
+					      RTA_DATA(rta_tb[IFA_LOCAL]),
+					      abuf, sizeof(abuf));
+
+        
+		if (rta_tb[IFA_ADDRESS] == NULL ||
+		    memcmp(RTA_DATA(rta_tb[IFA_ADDRESS]), RTA_DATA(rta_tb[IFA_LOCAL]),
+			   ifa->ifa_family == AF_INET ? 4 : 16) == 0) {
+			//fprintf(fp, "/%d ", ifa->ifa_prefixlen);
+            prefixlen = ifa->ifa_prefixlen;
+        }
+        /*
+		} else {
+			fprintf(fp, " peer %s/%d ",
+				format_host(ifa->ifa_family,
+					    RTA_PAYLOAD(rta_tb[IFA_ADDRESS]),
+					    RTA_DATA(rta_tb[IFA_ADDRESS]),
+					    abuf, sizeof(abuf)),
+				ifa->ifa_prefixlen);
+		}*/
+	}
+		
+    /* XXX: following code does not work! The problem is in ifa/ifi above ...
+    if (rta_tb[IFLA_ADDRESS]) {
+			mac = ll_addr_n2a(RTA_DATA(rta_tb[IFLA_ADDRESS]),
+						      RTA_PAYLOAD(rta_tb[IFLA_ADDRESS]),
+						      ifi->ifi_type,
+						      abuf, sizeof(abuf));
+    }
+    */
+
+    fprintf(fp, "([%s], %s, %s, %s/%d, %s)\n", type, ethname, ipfamily, ipaddress, prefixlen, mac);
+
+	fflush(fp);
+	return 0;
+}
+
+
+static int store_nlmsg(const struct sockaddr_nl *who, struct nlmsghdr *n,
+		       void *arg)
+{
+	struct nlmsg_chain *lchain = (struct nlmsg_chain *)arg;
+	struct nlmsg_list *h;
+
+	h = malloc(n->nlmsg_len+sizeof(void*));
+	if (h == NULL)
+		return -1;
+
+	memcpy(&h->h, n, n->nlmsg_len);
+	h->next = NULL;
+
+	if (lchain->tail)
+		lchain->tail->next = h;
+	else
+		lchain->head = h;
+	lchain->tail = h;
+
+	ll_remember_index(who, n, NULL);
+	return 0;
+}
+
+static void ipaddr_filter(struct nlmsg_chain *linfo, struct nlmsg_chain *ainfo)
+{
+	struct nlmsg_list *l, **lp;
+
+	lp = &linfo->head;
+	while ( (l = *lp) != NULL) {
+		int ok = 0;
+		int missing_net_address = 1;
+		struct ifinfomsg *ifi = NLMSG_DATA(&l->h);
+		struct nlmsg_list *a;
+
+		for (a = ainfo->head; a; a = a->next) {
+			struct nlmsghdr *n = &a->h;
+			struct ifaddrmsg *ifa = NLMSG_DATA(n);
+			struct rtattr *tb[IFA_MAX + 1];
+			unsigned int ifa_flags;
+
+			if (ifa->ifa_index != ifi->ifi_index)
+				continue;
+			missing_net_address = 0;
+			if (filter_family && filter_family != ifa->ifa_family)
+				continue;
+			if ((filter_scope^ifa->ifa_scope)&filter_scopemask)
+				continue;
+
+			parse_rtattr(tb, IFA_MAX, IFA_RTA(ifa), IFA_PAYLOAD(n));
+			ifa_flags = get_ifa_flags(ifa, tb[IFA_FLAGS]);
+
+			/*if ((filter.flags ^ ifa_flags) & filter.flagmask)
+				continue;
+			if (filter.pfx.family || filter.label) {
+				if (!tb[IFA_LOCAL])
+					tb[IFA_LOCAL] = tb[IFA_ADDRESS];
+
+				if (filter.pfx.family && tb[IFA_LOCAL]) {
+					inet_prefix dst;
+					memset(&dst, 0, sizeof(dst));
+					dst.family = ifa->ifa_family;
+					memcpy(&dst.data, RTA_DATA(tb[IFA_LOCAL]), RTA_PAYLOAD(tb[IFA_LOCAL]));
+					if (inet_addr_match(&dst, &filter.pfx, filter.pfx.bitlen))
+						continue;
+				}
+				if (filter.label) {
+					SPRINT_BUF(b1);
+					const char *label;
+					if (tb[IFA_LABEL])
+						label = RTA_DATA(tb[IFA_LABEL]);
+					else
+						label = ll_idx_n2a(ifa->ifa_index, b1);
+					if (fnmatch(filter.label, label, 0) != 0)
+						continue;
+				}
+			}*/
+
+			ok = 1;
+			break;
+		}
+		if (missing_net_address &&
+		    (filter_family == AF_UNSPEC || filter_family == AF_PACKET))
+			ok = 1;
+		if (!ok) {
+			*lp = l->next;
+			free(l);
+		} else
+			lp = &l->next;
+	}
+}
+
+static void free_nlmsg_chain(struct nlmsg_chain *info)
+{
+	struct nlmsg_list *l, *n;
+
+	for (l = info->head; l; l = n) {
+		n = l->next;
+		free(l);
+	}
+}
+
+static int print_selected_addrinfo(int ifindex, struct nlmsg_list *ainfo, FILE *fp)
+{
+	for ( ;ainfo ;  ainfo = ainfo->next) {
+		struct nlmsghdr *n = &ainfo->h;
+		struct ifaddrmsg *ifa = NLMSG_DATA(n);
+
+		if (n->nlmsg_type != RTM_NEWADDR)
+			continue;
+
+		if (n->nlmsg_len < NLMSG_LENGTH(sizeof(ifa)))
+			return -1;
+
+		if (ifa->ifa_index != ifindex) {
+		    //(filter.family && filter.family != ifa->ifa_family))
+			continue;
+        }
+
+		print_addrinfo(NULL, n, fp);
+	}
+	return 0;
+}
+
+/*
+ * print the info about network interfaces in form
+ * (name, type, network, mac)
+ *
+ * (enp0s25, inet, 10.130.38.187/24, a0:1d:48:b7:e2:4e)
+ * (enp0s25, inet6, fe80::a21d:48ff:feb7:e24e/64, a0:1d:48:b7:e2:4e)
+ *
+ * BTW: the printing is done in callback print_selected_addrinfo/print_addrinfo
+ */
+static int ipaddr_list()
+{
+	struct nlmsg_chain linfo = { NULL, NULL};
+	struct nlmsg_chain ainfo = { NULL, NULL};
+	struct nlmsg_list *l;
+	char *filter_dev = NULL;
+	int no_link = 0;
+
+	//ipaddr_reset_filter(oneline);
+	//filter.showqueue = 1;
+
+	/*if (filter.family == AF_UNSPEC)
+		filter.family = preferred_family;
+
+	filter.group = -1;
+    */
+
+    /*
+	while (argc > 0) {
+		if (strcmp(*argv, "to") == 0) {
+			NEXT_ARG();
+			get_prefix(&filter.pfx, *argv, filter.family);
+			if (filter.family == AF_UNSPEC)
+				filter.family = filter.pfx.family;
+		} else if (strcmp(*argv, "scope") == 0) {
+			unsigned scope = 0;
+			NEXT_ARG();
+			filter.scopemask = -1;
+			if (rtnl_rtscope_a2n(&scope, *argv)) {
+				if (strcmp(*argv, "all") != 0)
+					invarg("invalid \"scope\"\n", *argv);
+				scope = RT_SCOPE_NOWHERE;
+				filter.scopemask = 0;
+			}
+			filter.scope = scope;
+		} else if (strcmp(*argv, "up") == 0) {
+			filter.up = 1;
+		} else if (strcmp(*argv, "dynamic") == 0) {
+			filter.flags &= ~IFA_F_PERMANENT;
+			filter.flagmask |= IFA_F_PERMANENT;
+		} else if (strcmp(*argv, "permanent") == 0) {
+			filter.flags |= IFA_F_PERMANENT;
+			filter.flagmask |= IFA_F_PERMANENT;
+		} else if (strcmp(*argv, "secondary") == 0 ||
+			   strcmp(*argv, "temporary") == 0) {
+			filter.flags |= IFA_F_SECONDARY;
+			filter.flagmask |= IFA_F_SECONDARY;
+		} else if (strcmp(*argv, "primary") == 0) {
+			filter.flags &= ~IFA_F_SECONDARY;
+			filter.flagmask |= IFA_F_SECONDARY;
+		} else if (strcmp(*argv, "tentative") == 0) {
+			filter.flags |= IFA_F_TENTATIVE;
+			filter.flagmask |= IFA_F_TENTATIVE;
+		} else if (strcmp(*argv, "deprecated") == 0) {
+			filter.flags |= IFA_F_DEPRECATED;
+			filter.flagmask |= IFA_F_DEPRECATED;
+		} else if (strcmp(*argv, "home") == 0) {
+			filter.flags |= IFA_F_HOMEADDRESS;
+			filter.flagmask |= IFA_F_HOMEADDRESS;
+		} else if (strcmp(*argv, "nodad") == 0) {
+			filter.flags |= IFA_F_NODAD;
+			filter.flagmask |= IFA_F_NODAD;
+		} else if (strcmp(*argv, "mngtmpaddr") == 0) {
+			filter.flags |= IFA_F_MANAGETEMPADDR;
+			filter.flagmask |= IFA_F_MANAGETEMPADDR;
+		} else if (strcmp(*argv, "noprefixroute") == 0) {
+			filter.flags |= IFA_F_NOPREFIXROUTE;
+			filter.flagmask |= IFA_F_NOPREFIXROUTE;
+		} else if (strcmp(*argv, "dadfailed") == 0) {
+			filter.flags |= IFA_F_DADFAILED;
+			filter.flagmask |= IFA_F_DADFAILED;
+		} else if (strcmp(*argv, "label") == 0) {
+			NEXT_ARG();
+			filter.label = *argv;
+		} else if (strcmp(*argv, "group") == 0) {
+			NEXT_ARG();
+			if (rtnl_group_a2n(&filter.group, *argv))
+				invarg("Invalid \"group\" value\n", *argv);
+		} else {
+			if (strcmp(*argv, "dev") == 0) {
+				NEXT_ARG();
+			}
+			if (matches(*argv, "help") == 0)
+				usage();
+			if (filter_dev)
+				duparg2("dev", *argv);
+			filter_dev = *argv;
+		}
+		argv++; argc--;
+	}
+    */
+
+	/*if (filter_dev) {
+		filter.ifindex = ll_name_to_index(filter_dev);
+		if (filter.ifindex <= 0) {
+			fprintf(stderr, "Device \"%s\" does not exist.\n", filter_dev);
+			return -1;
+		}
+	}*/
+
+
+	if (rtnl_wilddump_request(&rth, preferred_family, RTM_GETLINK) < 0) {
+		perror("Cannot send dump request");
+		exit(1);
+	}
+
+	if (rtnl_dump_filter(&rth, store_nlmsg, &linfo) < 0) {
+		fprintf(stderr, "Dump terminated\n");
+		exit(1);
+	}
+
+	if (filter_family != AF_PACKET) {
+		/*if (filter.oneline)
+			no_link = 1;*/
+
+		if (rtnl_wilddump_request(&rth, filter_family, RTM_GETADDR) < 0) {
+			perror("Cannot send dump request");
+			exit(1);
+		}
+
+		if (rtnl_dump_filter(&rth, store_nlmsg, &ainfo) < 0) {
+			fprintf(stderr, "Dump terminated\n");
+			exit(1);
+		}
+
+		ipaddr_filter(&linfo, &ainfo);
+	}
+
+	for (l = linfo.head; l; l = l->next) {
+		//if (no_link || print_linkinfo(NULL, &l->h, stdout) == 0) {
+			struct ifinfomsg *ifi = NLMSG_DATA(&l->h);
+			if (filter_family != AF_PACKET)
+				print_selected_addrinfo(ifi->ifi_index,
+							ainfo.head, stdout);
+		//}
+	}
+	free_nlmsg_chain(&ainfo);
+	free_nlmsg_chain(&linfo);
+
+	return 0;
+}
+
+static int accept_msg(const struct sockaddr_nl *who,
+		      struct nlmsghdr *n, void *arg)
+{
+	FILE *fp = (FILE*)arg;
+
+    switch (n->nlmsg_type) {
+
+        case RTM_NEWADDR:
+        case RTM_DELADDR:
+            print_addrinfo(who, n, arg);
+            break;
+
+        case RTM_NEWROUTE:
+        case RTM_DELROUTE:
+        case RTM_NEWLINK:
+        case RTM_DELLINK:
+        case RTM_NEWADDRLABEL:
+        case RTM_DELADDRLABEL:
+        case RTM_NEWNEIGH:
+        case RTM_DELNEIGH:
+        case RTM_NEWPREFIX:
+        case RTM_NEWRULE:
+        case RTM_DELRULE:
+        case RTM_NEWNETCONF:
+        case 15:
+        case RTM_NEWQDISC:
+        case RTM_DELQDISC:
+        case RTM_NEWTCLASS:
+        case RTM_DELTCLASS:
+        case RTM_NEWTFILTER:
+        case RTM_DELTFILTER:
+        case NLMSG_ERROR:
+        case NLMSG_NOOP:
+        case NLMSG_DONE:
+            break;
+        default:
+            //TODO: error
+            fprintf(fp, "n->nlmsg_type = <default>\n");
+            break;
+    }
+    return 0;
+}
+
+int main(int argc, char **argv) {
+
+    char *file = NULL;
+	unsigned groups = ~RTMGRP_TC;
+	int llink=0;
+	int laddr=0;
+	int lroute=0;
+	int lmroute=0;
+	int lprefix=0;
+	int lneigh=0;
+	int lnetconf=0;
+
+    int prefix_banner=0;
+    int preferred_family=0;
+
+	rtnl_close(&rth);
+    /*
+	ipaddr_reset_filter(1);
+	iproute_reset_filter();
+	ipmroute_reset_filter();
+	ipneigh_reset_filter();
+    */
+	const char *prog = *argv;
+    argc--;	argv++;
+
+    groups |= nl_mgrp(RTNLGRP_IPV4_IFADDR);
+    groups |= nl_mgrp(RTNLGRP_IPV6_IFADDR);
+
+	if (rtnl_open(&rth, groups) < 0)
+		exit(1);
+
+    ipaddr_list();
+	if (rtnl_listen(&rth, accept_msg, stdout) < 0)
+		exit(2);
+    
+	return 0;
+
+}
