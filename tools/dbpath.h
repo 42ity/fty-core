d1d19522b7daf050bdd83bc333d2b2d6a733ec70
Alena Chernikava <alenachernikava@eaton.com>
2014-09-30 09:00:18 +0200

    corrected issues with configure.ac (dbpath.h.in)
    Signed-off-by: Alena Chernikava <alenachernikava@eaton.com>

--- tools/dbpath.h
+++
@@ -1,7 +0,0 @@
-#ifndef DBPATH_H_
-#define DBPATH_H_
-
-static const std::string url =  "mysql:db=box_utf8;user=root";
-//static const std::string url = "sqlite:/home/achernikava/work/core/tools/my1db.db";
-
-#endif //DBPATH_H_
