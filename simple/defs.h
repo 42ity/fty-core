aabaa631b422229015a65d99bf25a21f651d318f
Alena Chernikava <alenachernikava@eaton.com>
2014-10-02 14:10:07 +0200

    added some very simple implementation of using persistance layer
    Signed-off-by: Alena Chernikava <alenachernikava@eaton.com>

---
+++ simple/defs.h
@@ -0,0 +1,10 @@
+
+#pragma once
+
+#include <assert.h>
+
+#include <czmq.h>
+
+static const char* DB_SOCK = "ipc://@/poord/persistence";
+static const char* NETLOGIC_SOCK = "ipc://@/poord/netlogic";
+static const char* CLI_SOCK = "ipc://@/poord/cli";
