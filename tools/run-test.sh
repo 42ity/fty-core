af80b06651f4591503dd7567a8a23c8068bff2a1
Alena Chernikava <alenachernikava@eaton.com>
2014-10-06 10:53:41 +0200

    AT
    Signed-off-by: Alena Chernikava <alenachernikava@eaton.com>

--- tools/run-test.sh
+++ tools/run-test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo "${@:1:$#-1}"
-echo "${@:$#:$#}"
+NEWARGS="${@:1:$#-1}"
+NEWPROGRAM="${@:$#:$#}"
 declare -r HARD_ERROR=99
-if [[ -z "${1}" ]]; then
+if [[ -z "${NEWPROGRAM}" ]]; then
     echo "FATAL: name of test is missing, exiting!"
     exit ${HARD_ERROR}
 fi
 
 mkdir -p tests/junit/
 
-exec "${1}" -r junit -o "tests/junit/${1}.xml"
+exec "${NEWPROGRAM}" "${NEWARGS}" -r junit -o "tests/junit/${1}.xml"
