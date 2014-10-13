9a1ba3dcf128fe9e991356138ff9a915ac09c878
Michal Hrusecky <MichalHrusecky@Eaton.com>
2014-10-13 13:00:50 +0200

    Run through astyle
    Cleaning up the coding style at least in cpp files. Using provided
    astyle.conf with two exceptions - indent=spaces=4 and no
    remove-comment-prefix
    
    Signed-off-by: Michal Hrusecky <Michal@Hrusecky.net>

--- src/web/src/sasl.cpp
+++ src/web/src/sasl.cpp
@@ -12,71 +12,71 @@
 
 /**************************************************************
  * I/O wrapper to attempt to write out the specified vector.
- * data, without any guarantees. If the function returns 
+ * data, without any guarantees. If the function returns
  * -1, the vector wasn't completely written.
  **************************************************************/
 int retry_writev(int fd, struct iovec *iov, int iovcnt) {
-	int n;               /* return value from writev() */
-	int i;               /* loop counter */
-	int written;         /* bytes written so far */
-	static int iov_max;  /* max number of iovec entries */
+    int n;               /* return value from writev() */
+    int i;               /* loop counter */
+    int written;         /* bytes written so far */
+    static int iov_max;  /* max number of iovec entries */
 
 #ifdef MAXIOV
-	iov_max = MAXIOV;
-#else 
+    iov_max = MAXIOV;
+#else
 # ifdef IOV_MAX
-	iov_max = IOV_MAX;
-# else 
-	iov_max = 8192;
-# endif 
+    iov_max = IOV_MAX;
+# else
+    iov_max = 8192;
+# endif
 #endif
 
-	written = 0;
+    written = 0;
 
-	for (;;) {
+    for (;;) {
 
-		while (iovcnt && iov[0].iov_len == 0) {
-			iov++;
-			iovcnt--;
-		}
+        while (iovcnt && iov[0].iov_len == 0) {
+            iov++;
+            iovcnt--;
+        }
 
-		if (!iovcnt) {
-			return written;
-		}
+        if (!iovcnt) {
+            return written;
+        }
 
-		n = writev(fd, iov, iovcnt > iov_max ? iov_max : iovcnt);
+        n = writev(fd, iov, iovcnt > iov_max ? iov_max : iovcnt);
 
-		if (n == -1) {
-			if (errno == EINVAL && iov_max > 10) {
-				iov_max /= 2;
-				continue;
-			}
+        if (n == -1) {
+            if (errno == EINVAL && iov_max > 10) {
+                iov_max /= 2;
+                continue;
+            }
 
-			if (errno == EINTR) {
-				continue;
-			}
+            if (errno == EINTR) {
+                continue;
+            }
 
-			return -1;
+            return -1;
 
-		} else {
-			written += n;
-		}
+        } else {
+            written += n;
+        }
 
-		for (i = 0; i < iovcnt; i++) {
-			if ((int) iov[i].iov_len > n) {
-				iov[i].iov_base = (char *)iov[i].iov_base + n;
-				iov[i].iov_len -= n;
-				break;
-			}
+        for (i = 0; i < iovcnt; i++) {
+            if ((int) iov[i].iov_len > n) {
+                iov[i].iov_base = (char *)iov[i].iov_base + n;
+                iov[i].iov_len -= n;
+                break;
+            }
 
-			n -= iov[i].iov_len;
-			iov[i].iov_len = 0;
-		}
+            n -= iov[i].iov_len;
+            iov[i].iov_len = 0;
+        }
 
-		if (i == iovcnt) {
-			return written;
-		}
-	}
+        if (i == iovcnt) {
+            return written;
+        }
+    }
 }
 
 
@@ -84,31 +84,36 @@ int retry_writev(int fd, struct iovec *iov, int iovcnt) {
  * Keep calling the read() system call with 'fd', 'buf', and 'nbyte'
  * until all the data is read in or an error occurs.
  */
-int retry_read(int fd, void *inbuf, unsigned nbyte)
-{
+int retry_read(int fd, void *inbuf, unsigned nbyte) {
     int n;
     int nread = 0;
     char *buf = (char *)inbuf;
 
-    if (nbyte == 0) return 0;
+    if (nbyte == 0) {
+        return 0;
+    }
 
     for (;;) {
-	n = read(fd, buf, nbyte);
-	if (n == -1 || n == 0) {
-	    if (errno == EINTR || errno == EAGAIN) continue;
-	    return -1;
-	}
+        n = read(fd, buf, nbyte);
+        if (n == -1 || n == 0) {
+            if (errno == EINTR || errno == EAGAIN) {
+                continue;
+            }
+            return -1;
+        }
 
-	nread += n;
+        nread += n;
 
-	if (n >= (int) nbyte) return nread;
+        if (n >= (int) nbyte) {
+            return nread;
+        }
 
-	buf += n;
-	nbyte -= n;
+        buf += n;
+        nbyte -= n;
     }
 }
 
-bool authenticate(const char *userid, const char *passwd, const char* service) {
+bool authenticate(const char *userid, const char *passwd, const char *service) {
     char response[1024];
     char query[8192];
     char *query_end = query;
@@ -120,38 +125,48 @@ bool authenticate(const char *userid, const char *passwd, const char* service) {
     char pwpath[sizeof(srvaddr.sun_path)];
     const char *p = NULL;
 
-    if(!service) service = "imap";
-    if(!userid || !passwd) return false;
-    
-	strcpy(pwpath, "/var/run/saslauthd/mux");
+    if (!service) {
+        service = "imap";
+    }
+    if (!userid || !passwd) {
+        return false;
+    }
+
+    strcpy(pwpath, "/var/run/saslauthd/mux");
 
     /*
      * build request of the form:
      *
      * count authid count password count service count realm
      */
- 	unsigned short u_len, p_len, s_len, r_len;
- 	struct iovec iov[8];
- 
- 	u_len = htons(strlen(userid));
- 	p_len = htons(strlen(passwd));
-	s_len = htons(strlen(service));
-	r_len = htons(0);
+    unsigned short u_len, p_len, s_len, r_len;
+    struct iovec iov[8];
 
-	memcpy(query_end, &u_len, sizeof(unsigned short));
-	query_end += sizeof(unsigned short);
-	while (*userid) *query_end++ = *userid++;
+    u_len = htons(strlen(userid));
+    p_len = htons(strlen(passwd));
+    s_len = htons(strlen(service));
+    r_len = htons(0);
 
-	memcpy(query_end, &p_len, sizeof(unsigned short));
-	query_end += sizeof(unsigned short);
-	while (*passwd) *query_end++ = *passwd++;
+    memcpy(query_end, &u_len, sizeof(unsigned short));
+    query_end += sizeof(unsigned short);
+    while (*userid) {
+        *query_end++ = *userid++;
+    }
 
-	memcpy(query_end, &s_len, sizeof(unsigned short));
-	query_end += sizeof(unsigned short);
-	while (*service) *query_end++ = *service++;
+    memcpy(query_end, &p_len, sizeof(unsigned short));
+    query_end += sizeof(unsigned short);
+    while (*passwd) {
+        *query_end++ = *passwd++;
+    }
 
-	memcpy(query_end, &r_len, sizeof(unsigned short));
-	query_end += sizeof(unsigned short);
+    memcpy(query_end, &s_len, sizeof(unsigned short));
+    query_end += sizeof(unsigned short);
+    while (*service) {
+        *query_end++ = *service++;
+    }
+
+    memcpy(query_end, &r_len, sizeof(unsigned short));
+    query_end += sizeof(unsigned short);
 
     s = socket(AF_UNIX, SOCK_STREAM, 0);
     if (s == -1) {
@@ -171,46 +186,46 @@ bool authenticate(const char *userid, const char *passwd, const char* service) {
 
     {
 
- 	struct iovec iov[8];
- 
-	iov[0].iov_len = query_end - query;
-	iov[0].iov_base = query;
+        struct iovec iov[8];
 
-	if (retry_writev(s, iov, 1) == -1) {
-        fprintf(stderr,"write failed\n");
-  	    return false;
-    }
-  
-    /*
-     * read response of the form:
-     *
-     * count result
-     */
-    if (retry_read(s, &count, sizeof(count)) < (int) sizeof(count)) {
-        fprintf(stderr,"size read failed\n");
-	    return false;
-    }
-  
-    count = ntohs(count);
-    if (count < 2) { /* MUST have at least "OK./saslauthd/testsaslauthd.c" or "NO" */
-	    close(s);
-        fprintf(stderr,"bad response from saslauthd\n");
-	    return false;
-    }
-  
-    count = (int)sizeof(response) < count ? sizeof(response) : count;
-    if (retry_read(s, response, count) < count) {
-	    close(s);
-        fprintf(stderr,"read failed\n");
-	    return false;
-    }
-    response[count] = '\0';
-  
-    close(s);
-  
-    if (!strncmp(response, "OK", 2)) {
-	    return true;
-    }
+        iov[0].iov_len = query_end - query;
+        iov[0].iov_base = query;
+
+        if (retry_writev(s, iov, 1) == -1) {
+            fprintf(stderr, "write failed\n");
+            return false;
+        }
+
+        /*
+         * read response of the form:
+         *
+         * count result
+         */
+        if (retry_read(s, &count, sizeof(count)) < (int) sizeof(count)) {
+            fprintf(stderr, "size read failed\n");
+            return false;
+        }
+
+        count = ntohs(count);
+        if (count < 2) { /* MUST have at least "OK./saslauthd/testsaslauthd.c" or "NO" */
+            close(s);
+            fprintf(stderr, "bad response from saslauthd\n");
+            return false;
+        }
+
+        count = (int)sizeof(response) < count ? sizeof(response) : count;
+        if (retry_read(s, response, count) < count) {
+            close(s);
+            fprintf(stderr, "read failed\n");
+            return false;
+        }
+        response[count] = '\0';
+
+        close(s);
+
+        if (!strncmp(response, "OK", 2)) {
+            return true;
+        }
 
     }
     return false;
--- src/web/src/tokens.cpp
+++ src/web/src/tokens.cpp
@@ -6,11 +6,11 @@
 
 #include "tokens.h"
 
-tokens* tokens::get_instance() {
-    static tokens* inst = NULL;
+tokens *tokens::get_instance() {
+    static tokens *inst = NULL;
     static std::mutex mtx;
     mtx.lock();
-    if(!inst) {
+    if (!inst) {
         inst = new tokens;
         randombytes_buf(inst->nonce, sizeof nonce);
         randombytes_buf(inst->key, sizeof key);
@@ -26,32 +26,35 @@ std::string tokens::gen_token(int& valid) {
     tme *= ROUND;
     valid = (tme - time(NULL));
 
-    for (int i = 0; i < MESSAGE_LEN; i++)
+    for (int i = 0; i < MESSAGE_LEN; i++) {
         buff[i] = 0x20;
+    }
 
     sprintf(buff, "%ld", tme);
     buff[strlen(buff)] = 0x20;
     buff[16] = 0;
 
-    crypto_secretbox_easy(ciphertext, (unsigned char*)buff, MESSAGE_LEN, nonce, key);
+    crypto_secretbox_easy(ciphertext, (unsigned char *)buff, MESSAGE_LEN, nonce, key);
 
-    return cxxtools::Base64Codec::encode((char*)ciphertext, CIPHERTEXT_LEN);
+    return cxxtools::Base64Codec::encode((char *)ciphertext, CIPHERTEXT_LEN);
 }
 
-void tokens::decode_token(char* buff, const std::string token) {
+void tokens::decode_token(char *buff, const std::string token) {
     std::string data;
 
     try {
         data = cxxtools::Base64Codec::decode(token);
-    } catch(std::exception& e) {
+    } catch (std::exception& e) {
         data = "";
     }
 
-    if((crypto_secretbox_open_easy((unsigned char*)buff,
-                                    (const unsigned char*)data.c_str(),
+    if ((crypto_secretbox_open_easy((unsigned char *)buff,
+                                    (const unsigned char *)data.c_str(),
                                     data.length(), nonce, key) != 0) ||
-      (data.empty())) {
-        for (int i = 0; i <= MESSAGE_LEN; i++) buff[i] = 0;
+        (data.empty())) {
+        for (int i = 0; i <= MESSAGE_LEN; i++) {
+            buff[i] = 0;
+        }
     }
 }
 
