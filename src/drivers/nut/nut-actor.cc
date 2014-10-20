2ab51b352b4428aa5d69f44eaeb2c70d121b0c42
Alena Chernikava <alenachernikava@eaton.com>
2014-10-20 15:25:13 +0200

    problem: cored doesn't compiled . Solution: 1. the version of tntnet is lowered to 0.7. Added missing flags. Corrected type-convert to (void *).'
    Signed-off-by: Alena Chernikava <alenachernikava@eaton.com>

--- src/drivers/nut/nut-actor.cc
+++ src/drivers/nut/nut-actor.cc
@@ -32,7 +32,7 @@ zmsg_t *nutdevice_to_powerdev_msg(const NUTDevice &dev) {
     }
     // get extra properties
     for( auto &iter : properties ) {
-        zhash_insert(otherProperties, iter.first.c_str(), (void *)( iter.second.c_str() ) );
+        zhash_insert(otherProperties, (void*)iter.first.c_str(), (void *)( iter.second.c_str() ) );
     }
     msg = powerdev_msg_encode_powerdev_status(
          dev.name().c_str(),
