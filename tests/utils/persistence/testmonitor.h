b1b9e940fb7665b52ae74f5fb88572217f9f19ae
Alena Chernikava <alenachernikava@eaton.com>
2014-10-29 15:51:05 +0100

    BIOS-385,354.
    PROBLEM:
    1. multiple issues with client_info functions.
    2. info could contain any character
    3. lowercase letters in SQL keywords
    
    Solution:
    1. Corrected functionality. Added basic test for client-info.
    2. string changed to zchunk_t
    3. changed to uppercase
    
    Signed-off-by: Alena Chernikava <alenachernikava@eaton.com>

--- tests/utils/persistence/testmonitor.h
+++ tests/utils/persistence/testmonitor.h
@@ -7,9 +7,11 @@ common_msg_t* select_client(const char* url, uint32_t id);
 common_msg_t* insert_client(const char* url,const char* name);
 common_msg_t* delete_client(const char* url, uint32_t id_client);
 common_msg_t* update_client(const char* url, uint32_t id, common_msg_t** client);
-common_msg_t* _generate_client_info(uint32_t id, uint32_t client_id, uint32_t device_id,time_t mytime, const char* data, uint32_t size);
-common_msg_t* _insert_client_info (const char* url, uint32_t device_id, uint32_t client_id, const char* info, uint32_t infolen);
+common_msg_t* _generate_client_info(uint32_t client_id, uint32_t device_id, time_t mytime, byte* data, uint32_t datasize);
+common_msg_t* _insert_client_info (const char* url, uint32_t device_id, uint32_t client_id, byte* info, uint32_t infolen);
 common_msg_t* _delete_client_info (const char* url, uint32_t id);
 common_msg_t* _update_client_info (const char* url, common_msg_t* newclientinfo);
 common_msg_t* select_client_info_last(const char* url, uint32_t client_id, uint32_t device_id);
 common_msg_t* _select_client_info(const char* url, uint32_t id);
+common_msg_t* _generate_return_client_info(uint32_t client_info_id, common_msg_t** client_info);
+
