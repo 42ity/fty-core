cab73a434df096e4d944ea9941db639f077acee8
Michal Hrusecky <MichalHrusecky@Eaton.com>
2014-10-30 09:10:40 +0100

    Initial webtesting script
    Signed-off-by: Michal Hrusecky <Michal@Hrusecky.net>

--- tests/test_web.sh
+++ tests/test_web.sh
@@ -6,6 +6,8 @@ TOTAL=0
 USER="morbo"
 PASSWD="iwilldestroyyou"
 
+PATH="$PATH:/sbin:/usr/sbin"
+
 BASE_URL="http://127.0.0.1:8000/api/v1"
 
 print_result() {
@@ -16,14 +18,28 @@ print_result() {
     else
         echo " * FAILED"
         _ret=1
+        FAILED="$FAILED $NAME"
     fi
     TOTAL="`expr $TOTAL + 1`"
     echo
     return $_ret
 }
 
+test_it() {
+    if [ "$1" ]; then
+        NAME="$1"
+    fi
+    [ "$NAME" ] || NAME="`basename "$0" .sh`"
+    echo "Running test $NAME:"
+}
+
 api_get() {
     curl -v --progress-bar "$BASE_URL$1" 2>&1
+}
+
+api_get_json() {
+    curl -v --progress-bar "$BASE_URL$1" 2> /dev/null \
+    | tr \\n \  | sed -e 's|[[:blank:]]\+||g' -e 's|$|\n|'
 }
 
 api_post() {
@@ -37,14 +53,14 @@ api_auth_post() {
 }
 
 # fixture ini
-if [ ! /usr/bin/systemctl is-active -q saslauthd.service ]; then
-    echo "saslauthd does not run, please entrer a root password to run it"
-    /usr/bin/sudo /usr/bin/systemctl start saslauthd.service || exit $?
+if ! pidof saslauthd > /dev/null; then
+    echo "saslauthd does not run, please start it first!"
+    exit 1
 fi
 
 # check the user morbo in system
 # I expects SASL uses Linux PAM, therefor getent will tell us it all
-if ! getent passwd "${USER}"; then
+if ! getent passwd "${USER}" > /dev/null; then
     echo "User ${USER} is not known to system administrative database"
     echo "To add it locally, run: "
     echo "    sudo /usr/sbin/useradd --comment "BIOS REST API testing user" --groups nobody --no-create-home --no-user-group ${USER}"
@@ -52,57 +68,28 @@ if ! getent passwd "${USER}"; then
     exit 2
 fi
 
-if ! testsaslauthd -u "${USER}" -p "${PASSWD}" -s bios; then
+if ! testsaslauthd -u "${USER}" -p "${PASSWD}" -s bios > /dev/null; then
     echo "SASL autentification for user '${USER}' have failed. Check the existence of /etc/sasl2/bios.conf and /etc/pam.d/bios"
     exit 3
 fi
 
-# Check getting token
-echo "Testing login:"
-TOKEN="`api_get "/oauth2/token?username=$USER&password=$PASSWD&grant_type=password" | \
-        sed -n 's|.*"access_token"[[:blank:]]*:[[:blank:]]*"\([^"]*\)".*|\1|p'`"
-[ "$TOKEN" ]
-print_result $?
-if [ $? -ne 0 ]; then
-    echo "FATAL: access token test must not fail, otherwise it does not makes a sense to continue"
-    exit 1
-fi
-
-# Check not getting token
-echo "Testing wrong login:"
-[ "`api_get "/oauth2/token?username=$USER&password=not$PASSWD&grant_type=password" | \
-    grep "HTTP/1.1 401 Unauthorized"`" ]
-print_result $?
-
-# Check getting time
-echo "Testing time get:"
-TIME="`api_get "/admin/time" | \
-       sed -n 's|.*"time"[[:blank:]]*:[[:blank:]]*"\([^"]*\)".*|\1|p'`"
-TIME_S_GET="`date -d"$TIME" +%s 2> /dev/null`"
-TIME_S_NOW="`date +%s`"
-[ "`expr "$TIME_S_NOW" - "$TIME_S_GET"`" -lt 10 ]
-print_result $?
-
-# Check setting time as unprivileged user
-echo "Testing unauthorized time set:"
-[ "`api_post "/admin/time" "1970-01-01T00:00:00Z" | \
-    grep "HTTP/1.1 401 Unauthorized"`" ]
-print_result $?
-
-# Check setting time as privileged user
-echo "Testing authorized time set:"
-TIME_NOW="`date --utc +%FT%TZ`"
-TIME="`api_auth_post "/admin/time" "1970-01-01T00:00:00Z" | \
-       sed -n 's|.*"time"[[:blank:]]*:[[:blank:]]*"\([^"]*\)".*|\1|p'`"
-api_auth_post "/admin/time" "$TIME_NOW" > /dev/null
-TIME_S="`date -d"$TIME" +%s 2> /dev/null`"
-[ "$TIME_S" -lt 10 ]
-print_result $?
-
-# Check setting nonsense
-echo "Testing wrong time format set:"
-[ "`api_auth_post "/admin/time" "stardate 48960.9" | \
-    grep "HTTP/1.1 400 Bad Request"`" ]
-print_result $?
+cd "`dirname "$0"`"
+[ "$LOG_DIR" ] || LOG_DIR="`pwd`/web/log"
+mkdir -p "$LOG_DIR" || exit 4
+cd web/commands
+[ "$1" ] || set *
+while [ "$1" ]; do
+    NAME="$1"
+    . ./"$1" 5> "$LOG_DIR/$1".log
+    if [ -r "../results/$1".res ]; then
+        diff -Naru "../results/$1".res "$LOG_DIR/$1".log | sed 's|^|\ \ \ |'
+        print_result $?
+    fi
+    shift
+done
 
 echo "Testing completed, $PASS/$TOTAL tests passed"
+echo "Following tests failed:"
+for i in $FAILED; do
+    echo " * $i"
+done
