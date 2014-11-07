1c0a951cbaa9c8b3ab0bf87fb4ec92980656e914
Karol Hrdina <karolhrdina@eaton.com>
2014-11-07 16:47:44 +0100

    CLEAN-UP CYCLE #1
    src/utils/<everything persistence related>:
    -> moved to src/persist
    -> suffix (.*)persistence removed from some files
    -> header guard format united
    -> namespace changed to persist
    
    Signed-off-by: Karol Hrdina <karolhrdina@eaton.com>

--- tools/dbinit.h
+++ tools/dbinit.h
@@ -1,14 +1,14 @@
 #ifndef DBINIT_H_
 #define DBINIT_H_
 
-#include "persistence/databaseobject.h"
+#include "../src/persist/databaseobject.h"
 #include "dbpath.h"
 
-static const std::string osnew       = utils::db::objectStatetoString(utils::db::ObjectState::OS_NEW);
-static const std::string osdeleted   = utils::db::objectStatetoString(utils::db::ObjectState::OS_DELETED);
-static const std::string osupdated   = utils::db::objectStatetoString(utils::db::ObjectState::OS_UPDATED);
-static const std::string osselected  = utils::db::objectStatetoString(utils::db::ObjectState::OS_SELECTED);
-static const std::string osinserted  = utils::db::objectStatetoString(utils::db::ObjectState::OS_INSERTED);
+static const std::string osnew       = persist::objectStatetoString(persist::ObjectState::OS_NEW);
+static const std::string osdeleted   = persist::objectStatetoString(persist::ObjectState::OS_DELETED);
+static const std::string osupdated   = persist::objectStatetoString(persist::ObjectState::OS_UPDATED);
+static const std::string osselected  = persist::objectStatetoString(persist::ObjectState::OS_SELECTED);
+static const std::string osinserted  = persist::objectStatetoString(persist::ObjectState::OS_INSERTED);
 
 
 #endif //DBINIT_H_
