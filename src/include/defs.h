cec61f95027e8e9c88b8dccfc28aa17608666c4d
Karol Hrdina <karolhrdina@eaton.com>
2014-11-10 02:59:22 +0100

    CLEAN-UP CYCLE #1
    constants from nmap driver moved to defs.h
    
    Signed-off-by: Karol Hrdina <karolhrdina@eaton.com>

--- src/include/defs.h
+++ src/include/defs.h
@@ -5,5 +5,8 @@ static const char* DB_SOCK = "ipc://@/poord/persistence";
 static const char* NETLOGIC_SOCK = "ipc://@/poord/netlogic";
 static const char* CLI_SOCK = "ipc://@/poord/cli";
 
+static const char* DRIVER_NMAP_SOCK = "ipc://@/bios/driver/nmap";
+static const char* DRIVER_NMAP_REPLY = "ipc://@/bios/driver/nmap_reply";
+
 #endif // SRC_INCLUDE_DEFS_H_
 
