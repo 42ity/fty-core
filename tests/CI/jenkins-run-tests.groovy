78a9c0f91a6608830cadb8674c27a7eea6dff31c
Tomas Halman <TomasHalman@eaton.com>
2014-11-11 14:18:00 +0100

    incorporated comments
    Signed-off-by: Tomas Halman <TomasHalman@eaton.com>

--- tests/CI/jenkins-run-tests.groovy
+++ tests/CI/jenkins-run-tests.groovy
@@ -1,6 +1,31 @@
 import hudson.model.*
 import hudson.utils.*
 
+/**
+ * Copyright (C) 2014 Eaton
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author(s): Tomas Halman <TomasHalman@eaton.com>
+ *           
+ * Description: Runs the "Test plan". Necessary jobs first (like compiling),
+ *              later on functional/integration tests. If one of necessary jobs
+ *              fails, Test plan fails immediatelly too. If one of functional/integration
+ *              tests fails, test plan prints warning and is marked as unstable
+ *              using "Console output parsing" plugin.
+ */
+
 def startJob(name,params) {
     job = Hudson.instance.getJob(name);
     job.scheduleBuild2(0, new Cause.UpstreamCause(build), new ParametersAction(params) );
