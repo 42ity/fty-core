62a4ccce30eccecc0493ed4b1b05c509d8b8beb4
Tomas Halman <TomasHalman@eaton.com>
2014-11-11 14:18:00 +0100

    incorporated comments
    Signed-off-by: Tomas Halman <TomasHalman@eaton.com>

--- tests/CI/ci-make-check.sh
+++ tests/CI/ci-make-check.sh
@@ -1,11 +1,33 @@
 #!/bin/sh
 
+# Copyright (C) 2014 Eaton
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#   
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author(s): Tomas Halman <TomasHalman@eaton.com>,
+#
+# Description: installs dependecies and compiles the project
+
+[ "x$CHECKOUTDIR" = "x" ] && CHECKOUTDIR=~/project
+
 set -e
 
 apt-get update
-mk-build-deps --tool 'apt-get --yes --force-yes' --install ~/core/obs/core.dsc
-cd ~/core
+mk-build-deps --tool 'apt-get --yes --force-yes' --install $CHECKOUTDIR/obs/core.dsc
+cd $CHECKOUTDIR
 
+# TODO: parallelization?
 echo "======================== make and make check ==============================="
 autoreconf -vfi && ./configure --prefix=$HOME && make && make check && make install
 
--- tests/CI/ci-test-db.sh
+++ tests/CI/ci-test-db.sh
@@ -1,11 +1,33 @@
 #/!bin/sh
 
+# Copyright (C) 2014 Eaton
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#   
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author(s): Barbora Stepankova <BarboraStepankova@Eaton.com>,
+#            Tomas Halman <TomasHalman@eaton.com>
+#
+# Description: tests database files import
+
+[ "x$CHECKOUTDIR" = "x" ] && CHECKOUTDIR=~/project
+
 set -u
 set -e
 set -x
 
-DB1="core/tools/initdb.sql"
-DB2="core/tools/load_data.sql"
+DB1="$CHECKOUTDIR/tools/initdb.sql"
+DB2="$CHECKOUTDIR/tools/load_data.sql"
 
 
 mysql -u root < "$DB1"
