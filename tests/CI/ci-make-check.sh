1157915a083c54621f4082b9fdcfd8a1528b4f16
Jim Klimov <jim@jimklimov.com>
2014-11-19 21:54:59 +0100

    CI TESTS: validate CHECKOUTDIR existance (by cd-or-fail-verbosely) before doing other meaningful work which might rely on it
    Signed-off-by: Jim Klimov <jim@jimklimov.com>

--- tests/CI/ci-make-check.sh
+++ tests/CI/ci-make-check.sh
@@ -31,8 +31,8 @@ echo "INFO: Test '$0 $@' will (try to) commence under CHECKOUTDIR='$CHECKOUTDIR'
 set -e
 
 apt-get update
+cd $CHECKOUTDIR || { echo "FATAL: Unusable CHECKOUTDIR='$CHECKOUTDIR'" >&2; exit 1; }
 mk-build-deps --tool 'apt-get --yes --force-yes' --install $CHECKOUTDIR/obs/core.dsc
-cd $CHECKOUTDIR
 
 # TODO: parallelization?
 echo "======================== make and make check ==============================="
