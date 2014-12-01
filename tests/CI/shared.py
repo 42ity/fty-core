f7abe8263f1dc209e7e19c94db759fe52b7e09e3
Michal Vyskocil <michalvyskocil@eaton.com>
2014-12-01 11:18:53 +0100

    Problem: can't share code between tests
    Solution: shared.py
    
     + document the expectations of test-netmon
    
    Signed-off-by: Michal Vyskocil <michalvyskocil@eaton.com>

---
+++ tests/CI/shared.py
@@ -0,0 +1,44 @@
+# shared routines for CI tests written in python
+
+def parse_ip_a_s(out):
+    """
+    parse output of command ip a(ddr) s(how)
+    
+    return - list of tuple (name, ipver, ipaddr, prefixlen: int, mac)
+    """
+
+    state = "start"
+    header = list()
+    ret = list()
+
+    for line in out.split('\n'):
+
+        if line and line[0] != ' ':
+            state = "start"
+            header = list()
+            if not "state UP" in line:
+                continue
+            header = list()
+            nic_name = line.split(':')[1].strip()
+            header.append(nic_name)
+            state = "read-mac"
+            continue
+
+        # skip entries for 'lo'
+        if state == "start" and not header:
+            continue
+
+        if line.startswith("    link/"):
+            try:
+                mac = line.split()[1]
+            except IndexError as e:
+                print(line)
+                print(e)
+            header.append(mac)
+        elif line.startswith("    inet"):
+            name, mac = header
+            ipver, ipaddr = line.split()[:2]
+            ipaddr, prefixlen = ipaddr.split('/')
+
+            ret.append((name, ipver, ipaddr, int(prefixlen), mac))
+    return ret
--- tests/CI/test-netmon.py
+++ tests/CI/test-netmon.py
@@ -8,6 +8,11 @@ Test the netmon -> zeromq -> db integration.
 3.) Check the status again
 4.) Turn it not
 5.) Check it again
+
+ Expects:
+1.) database empty before start
+2.) main daemon running
+3.) netmon running
 """
 
 import subprocess
@@ -17,48 +22,7 @@ import MySQLdb
 import sys
 import copy
 
-def parse_ip_a_s(out):
-    """
-    parse output of command ip a(ddr) s(how)
-    
-    return - list of tuple (name, ipver, ipaddr, prefixlen: int, mac)
-    """
-
-    state = "start"
-    header = list()
-    ret = list()
-
-    for line in out.split('\n'):
-
-        if line and line[0] != ' ':
-            state = "start"
-            header = list()
-            if not "state UP" in line:
-                continue
-            header = list()
-            nic_name = line.split(':')[1].strip()
-            header.append(nic_name)
-            state = "read-mac"
-            continue
-
-        # skip entries for 'lo'
-        if state == "start" and not header:
-            continue
-
-        if line.startswith("    link/"):
-            try:
-                mac = line.split()[1]
-            except IndexError as e:
-                print(line)
-                print(e)
-            header.append(mac)
-        elif line.startswith("    inet"):
-            name, mac = header
-            ipver, ipaddr = line.split()[:2]
-            ipaddr, prefixlen = ipaddr.split('/')
-
-            ret.append((name, ipver, ipaddr, int(prefixlen), mac))
-    return ret
+from shared import parse_ip_a_s
 
 def read_db(db):
     """
