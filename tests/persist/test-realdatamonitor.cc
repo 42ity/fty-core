f9ea6581a57ae3226d5559088a0ecb6454854db2
Karol Hrdina <karolhrdina@eaton.com>
2014-12-04 17:38:31 +0100

    BIOS-421: tests fixed
    Signed-off-by: Karol Hrdina <karolhrdina@eaton.com>

--- tests/persist/test-realdatamonitor.cc
+++ tests/persist/test-realdatamonitor.cc
@@ -3,6 +3,10 @@
 #include <iostream>
 #include <czmq.h>
 
+#include <tntdb/connect.h>
+#include <tntdb/row.h>
+#include <tntdb/result.h>
+
 #include "common_msg.h"
 #include "assetmsg.h"
 #include "monitor.h"
@@ -11,7 +15,7 @@
 /* tests for real monitoring assumed, that DB has the following state
  * In case, the initial data should be changed, the following tests should be changed too
 
-current table:
+MariaDB [box_utf8]> select * from v_bios_client_info_measurements;
 +----+-----------+----------------------+---------------------+--------+-----------+-------+
 | id | id_client | id_discovered_device | timestamp           | id_key | id_subkey | value |
 +----+-----------+----------------------+---------------------+--------+-----------+-------+
@@ -23,24 +27,19 @@ current table:
 |  6 |         1 |                    1 | 2014-11-12 09:59:59 |      3 |         1 |     1 |
 +----+-----------+----------------------+---------------------+--------+-----------+-------+
 
-last:
+MariaDB [box_utf8]> select * from v_bios_client_info_measurements_last;
 +----+----------------------+--------+-----------+-------+---------------------+-------+
 | id | id_discovered_device | id_key | id_subkey | value | timestamp           | scale |
 +----+----------------------+--------+-----------+-------+---------------------+-------+
-|  2 |                    1 |      1 |         1 |     3 | 2014-11-12 09:46:59 |    -2 |
-|  3 |                    1 |      2 |         1 |    31 | 2014-11-12 09:47:59 |     0 |
-|  4 |                    1 |      2 |         2 |    12 | 2014-11-12 09:48:59 |     1 |
-|  5 |                    1 |      1 |         2 |     1 | 2014-11-12 09:49:59 |     1 |
-|  6 |                    1 |      3 |         1 |     1 | 2014-11-12 09:59:59 |     0 |
+|  2 |                    1 |      1 |         1 |     3 | 2014-11-12 09:46:59 |    -1 |
+|  3 |                    1 |      2 |         1 |    31 | 2014-11-12 09:47:59 |    -1 |
+|  4 |                    1 |      2 |         2 |    12 | 2014-11-12 09:48:59 |    -1 |
+|  5 |                    1 |      1 |         2 |     1 | 2014-11-12 09:49:59 |    -1 |
+|  6 |                    1 |      3 |         1 |     1 | 2014-11-12 09:59:59 |    -1 |
 +----+----------------------+--------+-----------+-------+---------------------+-------+
 
 "keytag_id:subkeytag_id:value:scale"
 
-1:1:3:-2
-2:1:31:0
-2:2:12:1
-1:2:1:1
-3:1:1:0
 */
 TEST_CASE("real_measurements: select_last_measurements", "[db][select][lastmeasurements]")
 {
@@ -48,11 +47,11 @@ TEST_CASE("real_measurements: select_last_measurements", "[db][select][lastmeasu
     uint32_t id = 1;
     zlist_t* measurements = select_last_measurements (url.c_str(), id);
     REQUIRE ( measurements );
-    char forth1[10]  = "1:1:3:-2";
-    char third1[10]  = "2:1:31:0";
-    char second1[10] = "2:2:12:1";
-    char first1[10]  = "1:2:1:1";
-    char zero1[10]   = "3:1:1:0";
+    char zero1[10]   = "3:1:1:-1";
+    char first1[10]  = "1:2:1:-1";
+    char second1[10] = "2:2:12:-1";
+    char third1[10]  = "2:1:31:-1";
+    char forth1[10]  = "1:1:3:-1";
     char* zero  = (char*) zlist_first  (measurements);
     REQUIRE ( zero  != NULL );
     char* first  = (char*) zlist_next  (measurements);
@@ -82,30 +81,84 @@ TEST_CASE("real_measurements: select_last_measurements", "[db][select][lastmeasu
 
 TEST_CASE("helper functions: convert_asset_to_monitor", "[db][convert_to_monitor]")
 {
-    uint32_t id = 1;
-    REQUIRE ( convert_asset_to_monitor (url.c_str(), id) == -DB_ERROR_NOTFOUND );
-    id = 4;
-    REQUIRE ( convert_asset_to_monitor (url.c_str(), id) == 1 );
+    tntdb::Connection conn;
+    REQUIRE_NOTHROW (conn = tntdb::connectCached (url));
+    tntdb::Value val;
+    uint32_t id_asset = 0;
+    uint32_t id_monitor = 0;
+
+    tntdb::Statement st = conn.prepareCached (
+       "select id_asset_element from t_bios_asset_element where name = 'DC1'"
+    );
+    REQUIRE_NOTHROW (val = st.selectValue ());
+    REQUIRE_NOTHROW (val.get (id_asset));
+    REQUIRE ( convert_asset_to_monitor (url.c_str(), id_asset) == -DB_ERROR_NOTFOUND );
+
+    st = conn.prepareCached(
+       "select id_asset_element from t_bios_asset_element where name = 'ROW1'"
+    );
+    REQUIRE_NOTHROW (val = st.selectValue ());
+    REQUIRE_NOTHROW (val.get (id_asset));
+
+    st = conn.prepareCached (
+       "select id_discovered_device from t_bios_discovered_device "
+       "where name = 'select_device' AND id_device_type = "
+       "(select id_device_type from t_bios_device_type where name = 'not_classified')"
+    );
+    REQUIRE_NOTHROW (val = st.selectValue ());
+    REQUIRE_NOTHROW (val.get (id_monitor));
+
+    REQUIRE ( convert_asset_to_monitor (url.c_str(), id_asset) == id_monitor);
 }
 
 TEST_CASE("helper functions: convert_monitor_to_asset", "[db][convert_to_asset]")
 {
-    uint32_t id = 1;
-    REQUIRE ( convert_monitor_to_asset (url.c_str(), id) == 4 );
-    id = 4;
-    REQUIRE ( convert_monitor_to_asset (url.c_str(), id) == -DB_ERROR_NOTFOUND );
+    tntdb::Connection conn;
+    REQUIRE_NOTHROW (conn = tntdb::connectCached(url));
+    tntdb::Value val;
+    uint32_t id_asset = 0;
+    uint32_t id_monitor = 0;
+
+    tntdb::Statement st = conn.prepareCached (
+       "select id_discovered_device from t_bios_discovered_device "
+       "where name = 'select_device' AND id_device_type = "
+       "(select id_device_type from t_bios_device_type where name = 'not_classified')"
+    );
+    REQUIRE_NOTHROW (val = st.selectValue ());
+    REQUIRE_NOTHROW (val.get (id_monitor));
+
+    st = conn.prepareCached (
+       "select id_asset_element from t_bios_asset_element where name = 'ROW1'"
+    );
+    REQUIRE_NOTHROW (val = st.selectValue ());
+    REQUIRE_NOTHROW (val.get (id_asset));
+    REQUIRE ( convert_monitor_to_asset (url.c_str(), id_monitor) == id_asset );
+
+    id_monitor = 65530;
+    REQUIRE ( convert_monitor_to_asset (url.c_str(), id_monitor) == -DB_ERROR_NOTFOUND );
 }
 
 
 TEST_CASE("get_last_measurements", "[db][get][lastmeasurements]")
 {
     //SUCCESS
-    uint32_t id = 4;
+    tntdb::Connection conn;
+    REQUIRE_NOTHROW (conn = tntdb::connectCached(url));
+    tntdb::Value val;
+    uint32_t id = 0;
+
+    tntdb::Statement st = conn.prepareCached (
+       "select id_asset_element from t_bios_asset_element where name = 'ROW1'"
+    );
+    REQUIRE_NOTHROW (val = st.selectValue ());
+    REQUIRE_NOTHROW (val.get (id));
+
     zmsg_t* getlastmeasurements = common_msg_encode_get_last_measurements (id);
     common_msg_t* glm = common_msg_decode (&getlastmeasurements);
+    common_msg_print (glm);
     zmsg_t* returnmeasurements = get_last_measurements (url.c_str(), glm);
+    common_msg_print (glm);
     REQUIRE ( returnmeasurements );
-    //common_msg_print(common_msg_decode(&returnmeasurements));
 
     REQUIRE ( is_common_msg (returnmeasurements) );
     common_msg_destroy (&glm);
@@ -115,11 +168,12 @@ TEST_CASE("get_last_measurements", "[db][get][lastmeasurements]")
     REQUIRE ( common_msg_device_id (glm) == id );
     zlist_t* info = common_msg_get_measurements (glm);
 
-    char forth1[10]  = "1:1:3:-2";
-    char third1[10]  = "2:1:31:0";
-    char second1[10] = "2:2:12:1";
-    char first1[10]  = "1:2:1:1";
-    char zero1[10]   = "3:1:1:0";
+    char zero1[10]   = "3:1:1:-1";
+    char first1[10]  = "1:2:1:-1";
+    char second1[10] = "2:2:12:-1";
+    char third1[10]  = "2:1:31:-1";
+    char forth1[10]  = "1:1:3:-1";
+
     char* zero   = (char*) zlist_first (info);
     REQUIRE ( zero  != NULL );
     char* first  = (char*) zlist_next  (info);
@@ -141,7 +195,7 @@ TEST_CASE("get_last_measurements", "[db][get][lastmeasurements]")
     common_msg_destroy (&glm);
 
     //FAIL
-    id = 3;
+    id = 65531;
     getlastmeasurements = common_msg_encode_get_last_measurements (id);
     glm = common_msg_decode (&getlastmeasurements);
     returnmeasurements = get_last_measurements (url.c_str(), glm);
