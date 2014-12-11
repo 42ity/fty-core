bd16319926973dfb352567fc50a068fd84f25de5
Gerald Guillaume <geraldguillaume@eaton.com>
2014-12-11 15:23:59 +0100

    take in account all comments
    Signed-off-by: Gerald Guillaume <geraldguillaume@eaton.com>

--- docker/docker_build_bios.sh
+++ docker/docker_build_bios.sh
@@ -1,16 +1,35 @@
+#!/bin/sh
+#
+#   Copyright (c) 2014 Eaton Corporation <www.eaton.com>
+#   Copyright other contributors as noted in the AUTHORS file.
+#
+#   This file is part of the Eaton $BIOS project.
+#
+#   This is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This software is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#   Description: Script to generate eaton/bios container
 export build_dir=../build
 export temp_dir=./temp 
 
 echo "preparing $temp_dir .. "
 mkdir -p $temp_dir
-cp ressources/* $temp_dir
-cp $build_dir/simple $temp_dir
-cp $build_dir/netmon $temp_dir
-cp $build_dir/driver-nmap $temp_dir
-cp $build_dir/.libs/bios_web.so.0.0.0 $temp_dir
-cp $build_dir/tntnet.xml.example $temp_dir/tntnet.xml
-sed -ri 's:/usr/local/share/core-0.1/web:/var/www:' $temp_dir/tntnet.xml
-sed -ri 's:/usr/local/lib/core:/usr/local/lib:' $temp_dir/tntnet.xml
+export DESTDIR=`cd $temp_dir; pwd`
+CURDIR=`pwd`
+cd $build_dir
+make install
+cd $CURDIR
+cp resources/* $temp_dir
 
 cp ../tools/initdb.sql $temp_dir 
 cp ../tools/load_data.sql $temp_dir
@@ -20,8 +39,7 @@ sudo docker build -t eaton/bios $temp_dir
 
 echo "cleaning .."
 sudo docker rmi $(sudo docker images -q -f dangling=true)
-rm $temp_dir/*
-rm -d $temp_dir
+#rm -r $temp_dir
 
 echo "done"
 sudo docker images | grep eaton/bios
--- docker/docker_run_bios.sh
+++ docker/docker_run_bios.sh
@@ -1,3 +1,24 @@
+#!/bin/bash
+#
+#   Copyright (c) 2014 Eaton Corporation <www.eaton.com>
+#   Copyright other contributors as noted in the AUTHORS file.
+#
+#   This file is part of the Eaton $BIOS project.
+#
+#   This is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This software is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#   Description: Script to run eaton/bios container
 if sudo docker ps | grep dockerfile/mariadb > /dev/null
 then
   echo "dockerfile/mariadb already started"
@@ -8,9 +29,10 @@ else
 fi
 echo "starting eaton/bios .."
 container_id=`sudo docker ps -a | grep bios | awk '{print $1}'`
-if [ "x$container_id" != "x" ]
+if [[ "x$container_id" != "x" ]]
 then
-  sudo docker rm `sudo docker ps -a | grep bios | awk '{print $1}'`
+  sudo docker stop $container_id
+  sudo docker rm $container_id 
 fi
 if [[ "x$1" == "x-ti" ]]
 then
