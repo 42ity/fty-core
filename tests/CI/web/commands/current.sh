7eb5d12c178b214c29b5d6ec3e35f96bf15379be
Michal Vyskocil <michalvyskocil@eaton.com>
2014-12-11 12:37:35 +0100

    Problem: current REST API is not sync with specification
    Solution: remove mapping and fix CI tests
    
    References: BIOS-423
    
    Signed-off-by: Michal Vyskocil <michalvyskocil@eaton.com>

--- tests/CI/web/commands/current.sh
+++ tests/CI/web/commands/current.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
-test_it "current_epdu"
+test_it
 api_get_json '/metric/current?dev=6' >&5
-print_result $?
-
-test_it "current_ups"
 api_get_json '/metric/current?dev=7' >&5
 print_result $?
-
-test_it "current_not_existing"
 api_get_json '/metric/current?dev=3' >&5
 print_result $?
-
-test_it "current_invalid"
 api_get_json '/metric/current' >&5
 print_result $?
