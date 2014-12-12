76f4899094694624af7ba4e4cae21ef37927dfa5
Michal Hrusecky <MichalHrusecky@Eaton.com>
2014-12-12 19:49:38 +0100

    BIOS-416: Recursive location
    Signed-off-by: Michal Hrusecky <Michal@Hrusecky.net>

--- src/web/include/location_helpers.h
+++ src/web/include/location_helpers.h
@@ -1,8 +1,11 @@
 #ifndef SRC_WEB_INCLUDE_LOCATION_HELPERS
 #define SRC_WEB_INCLUDE_LOCATION_HELPERS
 
+#include "asset_msg.h"
+
 int element_id (const std::string& from, int& element_id);
 int asset (const std::string& from);
+int asset_location_r(asset_msg_t** asset_msg, std::string& json);
 
 #endif // SRC_WEB_INCLUDE_LOCATION_HELPERS
 
