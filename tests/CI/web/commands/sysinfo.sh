25a2914bd2f2a732ffdf0d2ee2c56a8f25deb898
Jim Klimov <jim@jimklimov.com>
2015-01-06 15:16:34 +0100

    Now actually do use authorization ;) Also extended sanity testing of detecting and parsing JSON output
    Signed-off-by: Jim Klimov <jim@jimklimov.com>

--- tests/CI/web/commands/sysinfo.sh
+++ tests/CI/web/commands/sysinfo.sh
@@ -1,15 +1,50 @@
+###############################################################
 # Check getting server system info as unprivileged user
 test_it "unauth_sysinfo_get"
 [ "`api_get '/admin/sysinfo' | \
     grep 'HTTP/1.1 401 Unauthorized'`" ]
 print_result $?
 
-# Check getting server system info (authorized only?)
-test_it "sysinfo_get"
+# Check getting server system info as unprivileged user
+test_it "unauth_sysinfo_get_nonjson_detect"
 SYSINFO="`api_get_content '/admin/sysinfo'`"
 RES=$?
 echo "=== SYSINFO:" >&2
 echo "$SYSINFO" >&2
+case "`echo "$SYSINFO" | tr '[:space:]' ' ' | sed 's,^ *,,'`" in
+    "{"*) RES=126 ;;
+    *)	SYSINFO=""; echo "ERROR: Received output is not JSON markup!" >&2 ;;
+esac
+# Properly here, no json result was returned so string should become empty
+[ $RES = 0 -a -z "$SYSINFO" ]
+print_result $?
+
+# Check getting server system info as unprivileged user
+test_it "unauth_sysinfo_get_nonjson_parse"
+SYSINFO="`api_get_content '/admin/sysinfo'`"
+SYSINFO_PARSED="`echo "$SYSINFO" | $CHECKOUTDIR/tools/JSON.sh -b`"
+RES=$?
+echo "$SYSINFO_PARSED" | egrep '^\[\]' && \
+    echo "ERROR: Got empty branch name" >&2 && RES=125
+echo "$SYSINFO_PARSED" | egrep '^\[\"' && \
+    echo "ERROR: Got parsed markup where none was expected" >&2 && RES=124
+# Properly here, no json result was returned so string should become empty
+# JSON.sh generally returns an error, but may return an empty token name
+# like '[] 123' - which is wrong for our usecase
+[ $RES = 0 -a -z "$SYSINFO_PARSED" ]
+print_result $?
+
+###############################################################
+# Check getting server system info (authorized only?)
+test_it "sysinfo_get"
+SYSINFO="`api_auth_get_content '/admin/sysinfo'`"
+RES=$?
+echo "=== SYSINFO:" >&2
+echo "$SYSINFO" >&2
+case "`echo "$SYSINFO" | tr '[:space:]' ' ' | sed 's,^ *,,'`" in
+    "{"*) ;;
+    *)	SYSINFO=""; echo "ERROR: Received output is not JSON markup!" >&2 ;;
+esac
 [ $RES = 0 -a -n "$SYSINFO" ]
 print_result $?
 
