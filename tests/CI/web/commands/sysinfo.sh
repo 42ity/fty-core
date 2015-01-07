fd93df7035589d56043f8f95356020c02d6fd7ee
Michal Vyskocil <michalvyskocil@eaton.com>
2015-01-07 12:51:55 +0100

    Problem: sysinfo test does  not work
    Solution: fix it
    
    Signed-off-by: Michal Vyskocil <michalvyskocil@eaton.com>

--- tests/CI/web/commands/sysinfo.sh
+++ tests/CI/web/commands/sysinfo.sh
@@ -65,31 +65,16 @@ getval() {
 
 test_it "sysinfo_runs_in_container"
 RES=0
-SYSINFO_CONTAINER_TYPE="`getval '"server-os-features","virt","container","type"'`" || RES=$?
-SYSINFO_CONTAINER_FLAG="`getval '"server-os-features","virt","container","flag"'`" || RES=$?
-echo "=== SYSINFO_CONTAINER_TYPE: '$SYSINFO_CONTAINER_TYPE' (code '$SYSINFO_CONTAINER_FLAG')" >&2
-[ $RES = 0 -a -n "$SYSINFO_CONTAINER_TYPE" -a \
-    x"$SYSINFO_CONTAINER_TYPE" != x'""' ]
+SYSINFO_CONTAINER="`getval '"operating-system","container"'`" || RES=$?
+echo "=== SYSINFO_CONTAINER: '$SYSINFO_CONTAINER'" >&2
+[ $RES = 0 -a -n "$SYSINFO_CONTAINER" -a \
+    x"$SYSINFO_CONTAINER" != x'""' ]
 print_result $?
 
 test_it "sysinfo_runs_in_virtmachine"
 RES=0
-SYSINFO_VIRTMACHINE_TYPE="`getval '"server-os-features","virt","virtmachine","type"'`" || RES=$?
-SYSINFO_VIRTMACHINE_FLAG="`getval '"server-os-features","virt","virtmachine","flag"'`" || RES=$?
-echo "=== SYSINFO_VIRTMACHINE_TYPE: '$SYSINFO_VIRTMACHINE_TYPE' (code '$SYSINFO_VIRTMACHINE_FLAG')" >&2
-[ $RES = 0 -a -n "$SYSINFO_VIRTMACHINE_TYPE" -a \
-    x"$SYSINFO_VIRTMACHINE_TYPE" != x'""' ]
+SYSINFO_VIRTMACHINE="`getval '"operating-system","hypervizor"'`" || RES=$?
+echo "=== SYSINFO_VIRTMACHINE: '$SYSINFO_VIRTMACHINE'" >&2
+[ $RES = 0 -a -n "$SYSINFO_VIRTMACHINE" -a \
+    x"$SYSINFO_VIRTMACHINE" != x'""' ]
 print_result $?
-
-if [ "$TEST_RETURN_CONTAINER_FLAG" = yes ]; then
-	[ "$SYSINFO_CONTAINER_FLAG" -gt 255 ] && \
-		SYSINFO_CONTAINER_FLAG="`echo $SYSINFO_CONTAINER_FLAG % 255 | bc`"
-	exit $SYSINFO_CONTAINER_FLAG
-fi
-
-if [ "$TEST_RETURN_VIRTMACHINE_FLAG" = yes ]; then
-	[ "$SYSINFO_VIRTMACHINE_FLAG" -gt 255 ] && \
-		SYSINFO_VIRTMACHINE_FLAG="`echo $SYSINFO_VIRTMACHINE_FLAG % 255 | bc`"
-	exit $SYSINFO_VIRTMACHINE_FLAG
-fi
-
