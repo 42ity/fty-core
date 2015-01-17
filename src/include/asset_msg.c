0dad5878141d09f41d377af5551cc005e4357e2a
Jim Klimov <jim@jimklimov.com>
2015-01-17 14:43:48 +0100

    BIOS-524: hotfix for files automatically generated with zproto - silence warnings about unused variables for currently comitted generated code
    Signed-off-by: Jim Klimov <jim@jimklimov.com>

--- src/include/asset_msg.c
+++ src/include/asset_msg.c
@@ -3416,6 +3416,9 @@ int
 asset_msg_test (bool verbose)
 {
     printf (" * asset_msg: ");
+    if (verbose) {;}	// silence an "unused" warning;
+    // TODO: properly fix this in template zproto : zproto_codec_c_v1.gsl
+    // so as to not lose the fix upon regeneration of code
 
     //  @selftest
     //  Simple create/destroy test
