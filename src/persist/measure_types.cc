5562430d5aa8bc563a7cc35dbb184458f99295c8
Michal Hrusecky <MichalHrusecky@Eaton.com>
2015-01-22 09:08:18 +0100

    More and better error handling
    Signed-off-by: Michal Hrusecky <Michal@Hrusecky.net>

--- src/persist/measure_types.cc
+++ src/persist/measure_types.cc
@@ -45,22 +45,29 @@ zmsg_t* process_measures_meta(common_msg_t** msg) {
                 ret = common_msg_encode_return_measure_type(
                     row.getInt(0), row.getString(1).c_str(),
                     row.getString(2).c_str());
-            } catch (const std::exception &e) {
-                ret = common_msg_encode_fail(0,0,e.what(),NULL);
+            }
+            catch (const tntdb::NotFound &e) {
+                log_warning("Type '%d' not dound", common_msg_mt_id(*msg));
+                ret = common_msg_encode_fail(DB_ERR,DB_ERROR_NOTFOUND,e.what(),NULL);
+            }
+            catch (const std::exception &e) {
+                log_error("While getting type '%d' encountered '%s'",
+                          common_msg_mt_id(*msg), e.what());
+                ret = common_msg_encode_fail(DB_ERR,DB_ERROR_INTERNAL,e.what(),NULL);
             }
             break;
 
         case COMMON_MSG_GET_MEASURE_TYPE_S: {
-            tntdb::Statement st = conn.prepareCached(
-                "insert into t_bios_measurement_types (name, unit) "
-                "select :name, :unit from dual WHERE NOT EXISTS "
-                "(select id from t_bios_measurement_types where name=:name)"
-            );
-            st.setString("name", common_msg_mt_name(*msg)).
-               setString("unit", common_msg_mt_unit(*msg)).
-               execute();
             try {
                 tntdb::Statement st = conn.prepareCached(
+                    "insert into t_bios_measurement_types (name, unit) "
+                    "select :name, :unit from dual WHERE NOT EXISTS "
+                    "(select id from t_bios_measurement_types where name=:name)"
+                );
+                st.setString("name", common_msg_mt_name(*msg)).
+                   setString("unit", common_msg_mt_unit(*msg)).
+                   execute();
+                st = conn.prepareCached(
                     "select id, name, unit from t_bios_measurement_types "
                     "where name = :name "
                     "and id is not null and name is not null");
@@ -71,7 +78,9 @@ zmsg_t* process_measures_meta(common_msg_t** msg) {
                     row.getInt(0), row.getString(1).c_str(),
                     row.getString(2).c_str());
             } catch (const std::exception &e) {
-                ret = common_msg_encode_fail(0,0,e.what(),NULL);
+                log_error("While getting type '%s' encountered '%s'",
+                          common_msg_mt_name(*msg), e.what());
+                ret = common_msg_encode_fail(DB_ERR,DB_ERROR_INTERNAL,e.what(),NULL);
             }}
             break;
 
@@ -89,27 +98,35 @@ zmsg_t* process_measures_meta(common_msg_t** msg) {
                 ret = common_msg_encode_return_measure_subtype(
                     row.getInt(0), row.getShort(1), row.getShort(2),
                     row.getString(3).c_str());
-            } catch (const std::exception &e) {
-                ret = common_msg_encode_fail(0,0,e.what(),NULL);
+            }
+            catch (const tntdb::NotFound &e) {
+                log_warning("Subtype %d/%d not dound", common_msg_mt_id(*msg),
+                            common_msg_mts_id(*msg));
+                ret = common_msg_encode_fail(DB_ERR,DB_ERROR_NOTFOUND,e.what(),NULL);
+            }
+            catch (const std::exception &e) {
+                log_error("While getting subtype %d/%d encountered '%s'",
+                          common_msg_mt_id(*msg), common_msg_mts_id(*msg), e.what());
+                ret = common_msg_encode_fail(DB_ERR,DB_ERROR_INTERNAL,e.what(),NULL);
             }
             break;
 
         case COMMON_MSG_GET_MEASURE_SUBTYPE_S: {
-            tntdb::Statement st = conn.prepareCached(
-                "insert into t_bios_measurement_subtypes (id, name, id_type, scale) "
-                "select "
-                "(select COALESCE(max(id),0)+1 from "
-                "t_bios_measurement_subtypes where id_type=:mt_id), "
-                ":name, :mt_id, :scale from dual WHERE NOT EXISTS "
-                "(select id from t_bios_measurement_subtypes where "
-                " name=:name and id_type=:mt_id)"
-            );
-            st.setString("name", common_msg_mts_name(*msg)).
-               setInt("mt_id", common_msg_mt_id(*msg)).
-               setInt("scale", (int8_t)common_msg_mts_scale(*msg)).
-               execute();
             try {
                 tntdb::Statement st = conn.prepareCached(
+                    "insert into t_bios_measurement_subtypes (id, name, id_type, scale) "
+                    "select "
+                    "(select COALESCE(max(id),0)+1 from "
+                    "t_bios_measurement_subtypes where id_type=:mt_id), "
+                    ":name, :mt_id, :scale from dual WHERE NOT EXISTS "
+                    "(select id from t_bios_measurement_subtypes where "
+                    " name=:name and id_type=:mt_id)"
+                );
+                st.setString("name", common_msg_mts_name(*msg)).
+                   setInt("mt_id", common_msg_mt_id(*msg)).
+                   setInt("scale", (int8_t)common_msg_mts_scale(*msg)).
+                   execute();
+                st = conn.prepareCached(
                     "select id, id_type, scale, name "
                     "from t_bios_measurement_subtypes where name = :name "
                     "and id_type = :mt_id "
@@ -122,12 +139,14 @@ zmsg_t* process_measures_meta(common_msg_t** msg) {
                     row.getInt(0), row.getShort(1), row.getShort(2),
                     row.getString(3).c_str());
             } catch (const std::exception &e) {
-                ret = common_msg_encode_fail(0,0,e.what(),NULL);
+                log_error("While getting sutype %d/'%s' encountered '%s'",
+                          common_msg_mt_id(*msg), common_msg_mt_name(*msg), e.what());
+                ret = common_msg_encode_fail(DB_ERR,DB_ERROR_INTERNAL,e.what(),NULL);
             }}
             break;
 
         default:
-            log_warning("Got wrong meassurements meta message!");
+            log_warning("Got wrong measurements meta message!");
             ret = common_msg_encode_fail(BAD_INPUT, BAD_INPUT_WRONG_INPUT,
                                          "Wrong meassurements meta message",
                                          NULL);
