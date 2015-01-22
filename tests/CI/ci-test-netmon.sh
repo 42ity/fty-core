319ecad0a77855ad416292cdb6c4d7cff1aa619c
Karol Hrdina <karolhrdina@eaton.com>
2015-01-22 17:12:38 +0100

    code review comments
    Signed-off-by: Karol Hrdina <karolhrdina@eaton.com>

--- tests/CI/ci-test-netmon.sh
+++ tests/CI/ci-test-netmon.sh
@@ -26,6 +26,8 @@
 # Usage $0 [-d]
 #   -d  print extra debug output
 
+# TODO
+#   check privilige || exit
 # TODO (nice to have, if there is nothing to do):
 # try to grep second iface and do a couple add/del on it as well (lo is special)
 # later install brctl and add own iface
@@ -37,40 +39,40 @@ if [ -n "$1" ] && [ "$1" = "-d" ]; then
     DEBUG=1
 fi
 
-dirname=$(dirname $0)
-topsrc_dir=$(realpath -e "$dirname")
+DIRNAME=$(dirname $0)
+topsrc_dir=$(realpath -e "$DIRNAME")
 # For those of us with weird operating systems :)
 if [ -z "$topsrc_dir" ]; then
-    topsrc_dir=$(cd "$dirname" && pwd -P)
+    topsrc_dir=$(cd "$DIRNAME" && pwd -P)
     if [ -z "$topsrc_dir" ]; then
         exit 1
     fi
 fi
 topsrc_dir=${topsrc_dir%/tests/CI}
 if [ -n "$DEBUG" ]; then
-    echo "DEBUG: topsrc_dir='$topsrc_dir'"
+    echo "DEBUG: topsrc_dir='$topsrc_dir'" >&2
 fi
 
-dsh_file=$(mktemp -p"$topsrc_dir/tests/CI/")
+dsh_file=$(mktemp -p "$topsrc_dir/tests/CI/")
 if [ -n "$DEBUG" ]; then
-    echo "DEBUG: dsh_file='$dsh_file'"
+    echo "DEBUG: dsh_file='$dsh_file'" >&2
 fi
 
 LOCKFILE=/tmp/ci-test-netmon.lock
 if [ -f $LOCKFILE ]; then
-    [ -n "$DEBUG" ] && echo "Script already running!"
-    exit 0
+    echo -e "Script already running!\nWhy don't you wait for the other one to finish to find out the real output of this test. Eh?"
+    exit 1 
 fi
-touch "$LOCKFILE"
 
 ### Section: actual steps being performed
 function cleanup {
-    rm -f "$LOCKFILE" #"$dsh_file"
     killall malamute
     killall dshell
     killall -9 netmon
+    rm -f "$LOCKFILE" #"$dsh_file"
 }
 
+touch "$LOCKFILE"
 trap cleanup EXIT SIGINT SIGQUIT SIGTERM
 
 # Note:
@@ -80,37 +82,37 @@ trap cleanup EXIT SIGINT SIGQUIT SIGTERM
 # of PATH environment variable.
 export PATH="$topsrc_dir:$topsrc_dir/tools:$PATH"
 if [ -n "$DEBUG" ]; then
-    echo "DEBUG: PATH='$PATH'"
+    echo "DEBUG: PATH='$PATH'" >&2
 fi
 
 killall malamute
 
 malamute "$topsrc_dir/tools/malamute.cfg" &
 if [[ $? -ne 0 ]]; then
-    [ -n "$DEBUG" ] && echo "malamute didn't start properly"
+    echo "malamute didn't start properly" >&2
     exit 1
 fi
 dshell.sh networks ".*" > "$dsh_file" &
 if [[ $? -ne 0 ]]; then
-    [ -n "$DEBUG" ] && echo "dshell didn't start properly"
+    echo "dshell didn't start properly" >&2
     exit 1
 fi
 netmon &
 if [[ $? -ne 0 ]]; then
-    [ -n "$DEBUG" ] && echo "netmon didn't start properly"
+    echo "netmon didn't start properly" >&2
     exit 1
 fi
 sleep 2
 
 # These actions have to be reflected in dsh_file for this test to succeed.
-ip a a 101.25.138.2 dev lo
-ip a a 103.15.3.0/24 dev lo
-ip a a 20.13.5.4/32 dev lo
+ip addr add 101.25.138.2 dev lo
+ip addr add 103.15.3.0/24 dev lo
+ip addr add 20.13.5.4/32 dev lo
 
 # Reverting back
-ip a d 101.25.138.2 dev lo
-ip a d 103.15.3.0/24 dev lo
-ip a d 20.13.5.4/32 dev lo
+ip addr del 101.25.138.2 dev lo
+ip addr del 103.15.3.0/24 dev lo
+ip addr del 20.13.5.4/32 dev lo
 
 file=$(<$dsh_file) # `cat file` for non-bash shell
 
