1a0ea3da22da01fbe5a4333a5338fbf62f0f3890
Michal Vyskocil <michalvyskocil@eaton.com>
2015-01-27 13:37:04 +0100

    Problem: ci-test-netmon.sh does not look like others
    Solution:
     1.) test lockfile as the first item
     2.) use CHECKOUTDIR and BUILDSUBDIR with the same copy&paste snippets
     and replace topsrc_dir, DIRNAME and others
     3.) don't change PATH, rather consistently use CHECKOUTDIR and
     BUILDSUBDIR
     4.) change dshell.sh to check the dshell existence in BUILDSUBDIR
    
    Signed-off-by: Michal Vyskocil <michalvyskocil@eaton.com>

--- tests/CI/ci-test-netmon.sh
+++ tests/CI/ci-test-netmon.sh
@@ -32,41 +32,48 @@
 # - vlan 
 # - perform real parallel-proof locking; now it's kindergarden stuff :)
 
+LOCKFILE=/tmp/ci-test-netmon.lock
+if [ -f $LOCKFILE ]; then
+    echo -e "Script already running. Stopping."
+    exit 1 
+fi
+
 # Check for root
 if [[ $EUID -ne 0 ]]; then
     echo "This script must be run with root permissions." >&2
     exit 1
 fi
+if [ "x$CHECKOUTDIR" = "x" ]; then
+    SCRIPTDIR="$(cd "`dirname $0`" && pwd)" || \
+    SCRIPTDIR="`dirname $0`"
+    case "$SCRIPTDIR" in
+        */tests/CI|tests/CI)
+           CHECKOUTDIR="$( echo "$SCRIPTDIR" | sed 's|/tests/CI$||' )" || \
+           CHECKOUTDIR="" ;;
+    esac
+fi
+[ "x$CHECKOUTDIR" = "x" ] && CHECKOUTDIR=~/project
+echo "INFO: Test '$0 $@' will (try to) commence under CHECKOUTDIR='$CHECKOUTDIR'..."
+
+export BUILDSUBDIR="$CHECKOUTDIR"
+[ ! -x "$BUILDSUBDIR/config.status" ] && BUILDSUBDIR="$PWD"
+if [ ! -x "$BUILDSUBDIR/config.status" ]; then
+    echo "CI-ERROR: Cannot find $BUILDSUBDIR/config.status, did you run configure?"
+    echo "CI-ERROR: Search path checked: $CHECKOUTDIR, $PWD"
+    exit 1
+fi
+
+echo "CI-INFO: Using BUILDSUBDIR='$BUILDSUBDIR' to run the REST API webserver"
 
 ### Section: setting necessary variables
 DEBUG=
-if [ -n "$1" ] && [ "$1" = "-d" ]; then
+if [ x"$1" = "x-d" ]; then
     DEBUG=1
 fi
 
-DIRNAME=$(dirname $0)
-topsrc_dir=$(realpath -e "$DIRNAME")
-# For those of us with weird operating systems :)
-if [ -z "$topsrc_dir" ]; then
-    topsrc_dir=$(cd "$DIRNAME" && pwd -P)
-    if [ -z "$topsrc_dir" ]; then
-        exit 1
-    fi
-fi
-topsrc_dir=${topsrc_dir%/tests/CI}
-if [ -n "$DEBUG" ]; then
-    echo "DEBUG: topsrc_dir='$topsrc_dir'" >&2
-fi
-
-dsh_file=$(mktemp -p "$topsrc_dir/tests/CI/")
+dsh_file=$(mktemp -p "$CHECKOUTDIR/tests/CI/")
 if [ -n "$DEBUG" ]; then
     echo "DEBUG: dsh_file='$dsh_file'" >&2
-fi
-
-LOCKFILE=/tmp/ci-test-netmon.lock
-if [ -f $LOCKFILE ]; then
-    echo -e "Script already running. Stopping."
-    exit 1 
 fi
 
 ### Section: actual steps being performed
@@ -80,29 +87,19 @@ function cleanup {
 touch "$LOCKFILE"
 trap cleanup EXIT SIGINT SIGQUIT SIGTERM
 
-# Note:
-# When obs builds the image, binaries (like netmon for e.g.) are naturally put
-# into /usr/bin. These are different from what we actually build from latest git snapshot.
-# Therefore if we want to invoke these binaries, their location has to be in the front
-# of PATH environment variable.
-export PATH="$topsrc_dir:$topsrc_dir/tools:$PATH"
-if [ -n "$DEBUG" ]; then
-    echo "DEBUG: PATH='$PATH'" >&2
-fi
-
 killall malamute
 
-malamute "$topsrc_dir/tools/malamute.cfg" &
+malamute "$CHECKOUTDIR/tools/malamute.cfg" &
 if [[ $? -ne 0 ]]; then
     echo "malamute didn't start properly" >&2
     exit 1
 fi
-dshell.sh networks ".*" > "$dsh_file" &
+$CHECKOUTDIR/tools/dshell.sh networks ".*" > "$dsh_file" &
 if [[ $? -ne 0 ]]; then
     echo "dshell didn't start properly" >&2
     exit 1
 fi
-netmon &
+$BUILDSUBDIR/netmon &
 if [[ $? -ne 0 ]]; then
     echo "netmon didn't start properly" >&2
     exit 1
