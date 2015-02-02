1dcb325fddd8e6bfaab8f9a5d02a76504dae7cde
Michal Hrusecky <MichalHrusecky@Eaton.com>
2015-02-02 14:40:04 +0100

    Get rid of laste references to assetmsg.h
    Signed-off-by: Michal Hrusecky <Michal@Hrusecky.net>

--- src/persist/assetcrud.h
+++ src/persist/assetcrud.h
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-/*! \file assetmsg.h
+/*! \file assecrud.h
     \brief Basic functions for assets
     \author Alena Chernikava <alenachernikava@eaton.com>
 */
