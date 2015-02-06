d41945df7edb2f6b4adb5b161f6e45b857a574e1
Jim Klimov <jim@jimklimov.com>
2015-02-06 22:34:04 +0100

    BIOS-549: Remainder of PACKAGE_* values and a new object file to override the defines detected by configure
    Signed-off-by: Jim Klimov <jim@jimklimov.com>

--- tools/git_details.sh
+++ tools/git_details.sh
@@ -1,7 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
-# Print the details of the current repository, if any
-# Copyright (C) 2015 by Jim Klimov, <EvgenyKlimov@eaton.com>
+# Description: Print the details of the current repository, if any
+#
+#   Copyright (c) 2014 Eaton Corporation <www.eaton.com>
+#
+#   This file is part of the Eaton $BIOS project.
+#
+#   This is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This software is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#   Author(s): Jim Klimov <EvgenyKlimov@eaton.com>
+
+[ -z "$JSONSH" -o ! -x "$JSONSH" ] && JSONSH="`dirname $0`/JSON.sh"
 
 if git --help >/dev/null 2>&1 ; then
     PACKAGE_GIT_ORIGIN="$(git config --get remote.origin.url)"
@@ -13,13 +33,25 @@ if git --help >/dev/null 2>&1 ; then
     PACKAGE_GIT_TSTAMP="$(git log -n 1 --format='%ct')"
     PACKAGE_GIT_HASH_S="$(git log -n 1 --format='%h')"
     PACKAGE_GIT_HASH_L="$(git rev-parse --verify HEAD)"
-    [ -x "`dirname $0`/JSON.sh" ] && \
-	PACKAGE_GIT_STATUS="$(git status -s | "`dirname $0`/JSON.sh" -Q | sed 's,\\,\\\\,g')" || \
-	PACKAGE_GIT_STATUS="$(git status -s)"
+    PACKAGE_GIT_STATUS="$(git status -s)"
+
+    PACKAGE_BUILD_HOST_UNAME="`uname -a`"
+    PACKAGE_BUILD_HOST_NAME="`uname -n`"
+    PACKAGE_BUILD_HOST_OS="`uname -s -r -v`"
+    PACKAGE_BUILD_TSTAMP="`date -u '+%s'`"
+
+    [ ! -x "$JSONSH" ] && \
+	echo "FAILED to use JSON.sh from '$JSONSH'" >&2 && exit 3
 
     for V in PACKAGE_GIT_ORIGIN PACKAGE_GIT_BRANCH PACKAGE_GIT_TSTAMP \
-	 PACKAGE_GIT_HASH_S PACKAGE_GIT_HASH_L PACKAGE_GIT_STATUS ; do
+	 PACKAGE_GIT_HASH_S PACKAGE_GIT_HASH_L PACKAGE_GIT_STATUS \
+	 PACKAGE_BUILD_HOST_UNAME PACKAGE_BUILD_HOST_NAME \
+	 PACKAGE_BUILD_HOST_OS PACKAGE_BUILD_TSTAMP \
+    ; do
         eval echo $V=\\\"'$'$V\\\""\\;"
+	VE="${V}_ESCAPED"
+	eval $VE=\"$(eval echo -E \"\$\{$V\}\" | $JSONSH -Q)\"
+        eval echo $VE=\\\"'$'$VE\\\""\\;"
     done
 else
     echo "FAILED to execute 'git' program" >&2
