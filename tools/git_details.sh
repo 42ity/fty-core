ffe5344bbf37605f3a7d31d4e2141963dfaf78ce
Jim Klimov <jim@jimklimov.com>
2015-02-12 09:41:05 +0100

    BIOS-549 : git_details.sh : included suggestion from Tomas to make $GIT program name a settable/detectable variable
    Signed-off-by: Jim Klimov <jim@jimklimov.com>

--- tools/git_details.sh
+++ tools/git_details.sh
@@ -21,6 +21,13 @@
 #
 #   Author(s): Jim Klimov <EvgenyKlimov@eaton.com>
 
+# Establish standard environment
+LANG=C
+LC_ALL=C
+TZ=UTC
+export LANG LC_ALL TZ
+
+# Caller can 'export GIT_DETAILS_BLANK=yes' to just generate an empty set
 if [ x"$GIT_DETAILS_BLANK" = xyes ]; then
     for V in PACKAGE_GIT_ORIGIN PACKAGE_GIT_BRANCH PACKAGE_GIT_TSTAMP \
 	 PACKAGE_GIT_HASH_S PACKAGE_GIT_HASH_L PACKAGE_GIT_STATUS \
@@ -35,28 +42,29 @@ if [ x"$GIT_DETAILS_BLANK" = xyes ]; then
 fi
 
 [ -z "$JSONSH" -o ! -x "$JSONSH" ] && JSONSH="`dirname $0`/JSON.sh"
+[ -z "$GIT" -o ! -x "$GIT" ] && GIT="$(which git 2>/dev/null | head -1)"
 
-if git --help >/dev/null 2>&1 ; then
-    PACKAGE_GIT_ORIGIN="$(git config --get remote.origin.url)"
+if [ -x "$GIT" ] && $GIT --help >/dev/null 2>&1 ; then
+    PACKAGE_GIT_ORIGIN="$($GIT config --get remote.origin.url)"
     if [ $? != 0 ]; then
 	echo "SKIPPED: can not get Git metadata in '`pwd`'" >&2
 	exit 0
     fi
-    PACKAGE_GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-    PACKAGE_GIT_TSTAMP="$(git log -n 1 --format='%ct')"
-    PACKAGE_GIT_HASH_S="$(git log -n 1 --format='%h')"
-    PACKAGE_GIT_HASH_L="$(git rev-parse --verify HEAD)"
-    PACKAGE_GIT_STATUS="$(git status -s)"
+    PACKAGE_GIT_BRANCH="$($GIT rev-parse --abbrev-ref HEAD)"
+    PACKAGE_GIT_TSTAMP="$($GIT log -n 1 --format='%ct')"
+    PACKAGE_GIT_HASH_S="$($GIT log -n 1 --format='%h')"
+    PACKAGE_GIT_HASH_L="$($GIT rev-parse --verify HEAD)"
+    PACKAGE_GIT_STATUS="$($GIT status -s)"
 
     ### Ported from bios-infra::obs-service_git_nas.sh
-    PACKAGE_GIT_TAGGED="$(git describe --tags 2>/dev/null)"
+    PACKAGE_GIT_TAGGED="$($GIT describe --tags 2>/dev/null)"
     PACKAGE_GIT_TAGGED="${PACKAGE_GIT_TAGGED/[tv]/}"  # kill the v or t from version
     PACKAGE_GIT_TAGGED="${PACKAGE_GIT_TAGGED/-/\~}"
 
     PACKAGE_BUILD_HOST_UNAME="`uname -a`"
     PACKAGE_BUILD_HOST_NAME="`uname -n`"
     PACKAGE_BUILD_HOST_OS="`uname -s -r -v`"
-    PACKAGE_BUILD_TSTAMP="`date -u '+%s'`"
+    PACKAGE_BUILD_TSTAMP="`TZ=UTC date -u '+%s'`"
 
     [ ! -x "$JSONSH" ] && \
 	echo "FAILED to use JSON.sh from '$JSONSH'" >&2 && exit 3
@@ -73,7 +81,7 @@ if git --help >/dev/null 2>&1 ; then
         eval echo $VE=\\\"'$'$VE\\\""\\;"
     done
 else
-    echo "FAILED to execute 'git' program" >&2
+    echo "FAILED to execute 'git' program (tried '$GIT')" >&2
     exit 2
 fi
 
