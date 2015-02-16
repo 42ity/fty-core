5bdb23cc710590211a44e687015aedf69e88b7d2
Tomas Halman <TomasHalman@eaton.com>
2015-02-16 19:08:40 +0100

    download logs instead of displaying
    Signed-off-by: Tomas Halman <TomasHalman@eaton.com>

--- tests/CI/ci-collect-logs.sh
+++ tests/CI/ci-collect-logs.sh
@@ -91,9 +91,8 @@ if [ $LOGS = 0 ] ; then
     echo "no log files"
 else
     log_list | while read file ; do
-        echo "-------------------- $file begin --------------------"
-        ssh -p $PORT root@$VM "cat \"$file\" ; /bin/rm -f \"$file\""
-        echo "-------------------- $file end --------------------"
+        echo "$file"
+        scp -P $PORT root@$VM:$file ./ 
     done
 fi
 
@@ -102,6 +101,7 @@ if [ $LOGS == 1 ] ; then
     cppcheck_list | while read file ; do
         scp -P $PORT root@$VM:$file ./
         ssh -p $PORT root@$VM "/bin/rm -f \"$file\""
+	echo $file
     done
 fi
 
