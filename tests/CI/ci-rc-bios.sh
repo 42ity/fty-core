dcda49bdff05b0f0d73442b9203ff92574420041
Tomas Halman <TomasHalman@eaton.com>
2015-02-18 14:25:20 +0100

    fixed stoping to be sure it stops added --status added output on start
    Signed-off-by: Tomas Halman <TomasHalman@eaton.com>

--- tests/CI/ci-rc-bios.sh
+++ tests/CI/ci-rc-bios.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh
 
 # Copyright (C) 2014 Eaton
 #
@@ -42,12 +42,11 @@ mlm_server
 }
 
 start_daemon(){
-    if which ${1} &> /dev/null; then
+    if which ${1} >/dev/null 2>&1; then
         prefix=`which $1`
         prefix=`dirname $prefix`
     fi
     [ \! -x ${PWD}/${1} ]       || prefix="${PWD}"
-    [ \! -x ~/bin/$1 ]          || prefix="`cd ~/bin; pwd`"
     [ \! -x ~/bin/$1 ]          || prefix="`cd ~/bin; pwd`"
     [ \! -x ~/lib/$1 ]          || prefix="`cd ~/lib; pwd`"
     [ \! -x ~/libexec/$1 ]      || prefix="`cd ~/libexec; pwd`"
@@ -57,7 +56,13 @@ start_daemon(){
         /bin/rm -rf ~/$1.log
         nohup ${prefix}/$1 >~/$1.log 2>&1 &
         sleep 5
+        echo -n "${prefix}/$1 "
         pidof $1
+        RESULT=$?
+        if [ "$RESULT" != "0" ] ; then
+            echo "failed to start $1"
+            exit 1
+        fi
     else
         echo "ERROR: $1 is missing"
         exit 1
@@ -66,8 +71,26 @@ start_daemon(){
 
 stop() {
     for d in $DAEMONS ; do
-       killall $d 2>/dev/null || true
+       pidof $d >/dev/null 2>&1 && killall $d
     done
+    sleep 1
+    for d in $DAEMONS ; do
+       pidof $d >/dev/null 2>&1 && killall -9 $d
+    done
+}
+
+status() {
+    RESULT=0
+    for d in $DAEMONS ; do
+       echo -n "$d "
+       if pidof $d >/dev/null 2>&1 ; then
+           echo "running"
+       else
+           echo "stopped"
+           RESULT=1
+       fi
+    done
+    exit $RESULT
 }
 
 start() {
@@ -79,8 +102,10 @@ start() {
 usage(){
     echo "Usage: $(basename $0) [options...]
 options:
-    --stop       stop simple and netmon daemons
-    --start      start simple (and it starts netmon)
+    --stop       stop BIOS processes
+    --start      start BIOS processes
+    --restart    restart BIOS processes
+    --status     check wether all processes are running
     --help|-h    print this help"
 }
 
@@ -97,6 +122,9 @@ while [ $# -gt 0 ] ; do
         --stop)
             OPERATION=stop
             ;;
+        --status)
+            OPERATION=status
+            ;;
         *)
             echo "Invalid option $1" 1>&2
             usage
@@ -109,14 +137,18 @@ done
 case "$OPERATION" in
     start)
         stop
-	restart_malamute
+        restart_malamute
         start
         ;;
     stop)
         stop
+        ;;
+    status)
+        status
         ;;
     help)
         usage
         exit 1
         ;;
 esac
+exit 0
