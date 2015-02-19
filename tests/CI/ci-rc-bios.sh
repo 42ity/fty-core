23d093acce52b7590f9ce0e854c1ee1e8042db32
Jim Klimov <jim@jimklimov.com>
2015-02-19 15:05:32 +0100

    Fixed usage of BUILDSUBDIR in ci-rc-bios.sh; merged Tomas PR#380 and my PR#381 fixes; some other fixes for greater script reliability
    Signed-off-by: Jim Klimov <jim@jimklimov.com>

--- tests/CI/ci-rc-bios.sh
+++ tests/CI/ci-rc-bios.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh
 
 # Copyright (C) 2014-2015 Eaton
 #
@@ -20,6 +20,25 @@
 # Description: starts or stops the $BIOS daemons installed in $HOME
 
 DAEMONS="db-ng driver-nut driver-nmap netmon"
+
+if [ "x$CHECKOUTDIR" = "x" ]; then
+    SCRIPTDIR="$(cd "`dirname $0`" && pwd)" || \
+    SCRIPTDIR="`dirname $0`"
+    case "$SCRIPTDIR" in
+        */tests/CI|tests/CI)
+           CHECKOUTDIR="$( echo "$SCRIPTDIR" | sed 's|/tests/CI$||' )" || \
+           CHECKOUTDIR="" ;;
+    esac
+fi
+[ "x$CHECKOUTDIR" = "x" ] && CHECKOUTDIR=~/project
+echo "INFO: Test '$0 $@' will (try to) commence under CHECKOUTDIR='$CHECKOUTDIR'..."
+
+[ -z "$BUILDSUBDIR" -o ! -d "$BUILDSUBDIR" ] && BUILDSUBDIR="$CHECKOUTDIR"
+[ ! -x "$BUILDSUBDIR/config.status" ] && BUILDSUBDIR="$PWD"
+if [ ! -x "$BUILDSUBDIR/config.status" ]; then
+    echo "Cannot find $BUILDSUBDIR/config.status, did you run configure?"
+    echo "Search path: $CHECKOUTDIR, $PWD"
+fi
 
 restart_malamute(){
     # NOTE: This likely needs execution as root or via sudo
@@ -43,21 +62,32 @@ mlm_server
 }
 
 start_daemon(){
-    if which ${1} &> /dev/null; then
-        prefix=`which ${1} | sed "s#/${1}\$##"`
+    prefixw=""
+    if which ${1} >/dev/null 2>&1; then
+        prefixw="`which ${1}`"
+        prefixw="`dirname $prefixw`"
     fi
-    [ \! -x ${PWD}/${1} ]       || prefix="${PWD}"
-    [ \! -x ~/bin/${1} ]          || prefix="`cd ~/bin; pwd`"
-    [ \! -x ~/bin/${1} ]          || prefix="`cd ~/bin; pwd`"
-    [ \! -x ~/lib/${1} ]          || prefix="`cd ~/lib; pwd`"
-    [ \! -x ~/libexec/${1} ]      || prefix="`cd ~/libexec; pwd`"
-    [ \! -x ~/lib/bios/${1} ]     || prefix="`cd ~/lib/bios; pwd`"
-    [ \! -x ~/libexec/bios/${1} ] || prefix="`cd ~/libexec/bios; pwd`"
-    if [ -x ${prefix}/${1} ] ; then
+
+    # Apparently from pre-existing logic, an older "prefix" may remain
+    for prefix in $prefix $prefixw "${PWD}" "$BUILDSUBDIR" "$CHECKOUTDIR" \
+        ~/bin ~/lib ~/libexec ~/lib/bios ~/libexec/bios \
+    ; do
+        if [ -n "$prefix" ] && [ -d "$prefix" -a -x "$prefix/${1}" ]; then
+            break
+        fi
+    done
+
+    if [ -x "${prefix}/${1}" ] ; then
         /bin/rm -rf ~/${1}.log
-        nohup ${prefix}/${1} >~/${1}.log 2>&1 &
+        nohup "${prefix}/${1}" >~/${1}.log 2>&1 &
         sleep 5
+        echo -n "${prefix}/$1 "
         pidof ${1}
+        RESULT=$?
+        if [ "$RESULT" != "0" ] ; then
+            echo "ERROR: failed to start $1" >&2
+            exit 1
+        fi
     else
         echo "ERROR: $1 is missing" >&2
         exit 1
@@ -66,16 +96,31 @@ start_daemon(){
 
 stop() {
     for d in $DAEMONS ; do
-       killall $d 2>/dev/null || true
+       ( pidof $d >/dev/null 2>&1 && killall $d 2>/dev/null ) || true
     done
     sleep 1
     for d in $DAEMONS ; do
-       ( pidof $d &>/dev/null && killall -9 $d 2>/dev/null ) || true
+       ( pidof $d >/dev/null 2>&1 && killall -9 $d 2>/dev/null ) || true
     done
+    # Test successful kills
     for d in $DAEMONS ; do
-	pidof $d &>/dev/null 2>&1 && return 1
+        pidof $d &>/dev/null 2>&1 && return 1
     done
     return 0
+}
+
+status() {
+    RESULT=0
+    for d in $DAEMONS ; do
+       echo -n "$d "
+       if pidof $d >/dev/null 2>&1 ; then
+           echo "running"
+       else
+           echo "stopped"
+           RESULT=1
+       fi
+    done
+    exit $RESULT
 }
 
 start() {
@@ -86,9 +131,10 @@ start() {
 
 usage(){
     echo "Usage: $(basename $0) [options...]
-options:
-    --stop       stop simple and netmon daemons
-    --start      start simple (and it starts netmon)
+Options:
+    --stop       stop BIOS processes
+    --start      start BIOS processes (does restart if BIOS is running)
+    --status     check whether all processes are running
     --help|-h    print this help"
 }
 
@@ -105,6 +151,9 @@ while [ $# -gt 0 ] ; do
         --stop)
             OPERATION=stop
             ;;
+        --status)
+            OPERATION=status
+            ;;
         *)
             echo "Invalid option $1" 1>&2
             usage
@@ -117,11 +166,15 @@ done
 case "$OPERATION" in
     start)
         stop
-	restart_malamute
+        restart_malamute && \
         start
         ;;
+
     stop)
         stop
+        ;;
+    status)
+        status
         ;;
     help)
         usage
