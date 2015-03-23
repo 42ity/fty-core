b8810775f0146e67fa39bf85a7f181b682ae9b74
Karol Hrdina <karolhrdina@eaton.com>
2015-03-23 16:59:41 +0100

    BIOS-589
    Signed-off-by: Karol Hrdina <karolhrdina@eaton.com>

--- src/shared/utils.c
+++ src/shared/utils.c
@@ -1,4 +1,5 @@
 #include "utils.h"
+#include "str_defs.h"
 
 const char *str_bool(const bool b) {
     return b ? "true" : "false";
@@ -13,4 +14,16 @@ bool streq(const char *a, const char *b) {
 		return (!a && !b);
 	}
 	return !strcmp(a, b);
+}
+
+bool is_average_step_supported (const char *step) {
+    if (!step) {
+        return false;
+    }
+    for (int i = 0; i < AVG_STEPS_SIZE; ++i) {
+        if (strcmp (step, AVG_STEPS[i]) == 0) {
+            return true;
+        }
+    }
+    return false;
 }
