1467a74e46c250caee961353a6c947a492638592
Radomir Vrajik <RadomirVrajik@eaton.com>
2015-03-24 16:19:15 +0100

    new vte tests 4
    Signed-off-by: Radomir Vrajik <RadomirVrajik@eaton.com>

--- tests/CI/vte-test_web.sh
+++ tests/CI/vte-test_web.sh
@@ -97,9 +97,7 @@ BASE_URL="http://$SUT_NAME:$SUT_HTTP_PORT/api/v1"
 PATH="$PATH:/sbin:/usr/sbin"
 
     # *** is sasl running on SUT?
-#if [ echo $(ssh -p $SUT_PORT $SUT_NAME "pidof saslauthd"|wc -l &)>/dev/null ];then
-#if [ $(echo $(ssh -p 2208 root@debian.roz.lab.etn.com "pidof saslauthd" &))>/dev/null ];then
-if [ $(ssh -p '2208' 'root@debian.roz.lab.etn.com' 'pidof saslauthd'|wc -l &) ];then
+if [ $(ssh -p $SUT_PORT $SUT_NAME 'pidof saslauthd'|wc -l &) ];then
     echo "saslauthd is running"
 else
     echo "saslauthd is not running, please start it first!" >&2
