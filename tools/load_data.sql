12f388158d94e2114e7612876114d97dac0861e5
Alena Chernikava <alenachernikava@eaton.com>
2015-03-26 15:54:28 +0100

    BIOS-619: test data inserted, corrected unit test to be more stable
    Signed-off-by: Alena Chernikava <alenachernikava@eaton.com>

--- tools/load_data.sql
+++ tools/load_data.sql
@@ -40,8 +40,67 @@ select @client_nut := id_client from t_bios_client where name = 'NUT';
 
 insert into t_bios_discovered_device (name, id_device_type) values ("select_device", @device_unclassified);
 insert into t_bios_discovered_device (name, id_device_type) values ("monitor_asset_measure", @device_unclassified);
+
 SELECT @select_device := id_discovered_device FROM t_bios_discovered_device WHERE name = "select_device";
 SELECT @monitor_asset_measure := id_discovered_device FROM t_bios_discovered_device WHERE name = "monitor_asset_measure";
+
+insert into t_bios_measurement_topic (id, device_id, units, topic) values (NULL, @monitor_asset_measure, "W", "realpower.outlet.2@monitor_asset_measure" );
+SELECT @test_topic_m_a_m_realpower_outlet_2 := id FROM t_bios_measurement_topic WHERE topic = "realpower.outlet.2@monitor_asset_measure";
+
+insert into t_bios_measurement_topic (id, device_id, units, topic) values (NULL, @monitor_asset_measure, "W", "realpower.outlet.1@monitor_asset_measure" );
+SELECT @test_topic_m_a_m_realpower_outlet_1 := id FROM t_bios_measurement_topic WHERE topic = "realpower.outlet.1@monitor_asset_measure";
+
+insert into t_bios_measurement_topic (id, device_id, units, topic) values (NULL, @monitor_asset_measure, "W", "realpower.outlet@monitor_asset_measure" );
+SELECT @test_topic_m_a_m_realpower_outlet  := id FROM t_bios_measurement_topic WHERE topic = "realpower.outlet@monitor_asset_measure";
+
+
+insert into t_bios_measurement_topic (id, device_id, units, topic) values (NULL, @select_device, "", "status.ups@select_device" );
+SELECT @test_topic_s_d_status_ups := id FROM t_bios_measurement_topic WHERE topic = "realpower.outlet.2@select_device";
+
+insert into t_bios_measurement_topic (id, device_id, units, topic) values (NULL, @select_device, "%", "charge.battery@select_device" );
+SELECT @test_topic_s_d_charge_battery := id FROM t_bios_measurement_topic WHERE topic = "realpower.outlet.2@select_device";
+
+insert into t_bios_measurement_topic (id, device_id, units, topic) values (NULL, @select_device, "%", "load.default@select_device" );
+SELECT @test_topic_s_d_load_default := id FROM t_bios_measurement_topic WHERE topic = "realpower.outlet.2@select_device";
+
+insert into t_bios_measurement_topic (id, device_id, units, topic) values (NULL, @select_device, "C", "temperature.default@select_device" );
+SELECT @test_topic_s_d_temperature_default := id FROM t_bios_measurement_topic WHERE topic = "realpower.outlet.2@select_device";
+
+insert into t_bios_measurement_topic (id, device_id, units, topic) values (NULL, @select_device, "W", "realpower.default@select_device" );
+SELECT @test_topic_s_d_realpower_defaulf := id FROM t_bios_measurement_topic WHERE topic = "realpower.outlet.2@select_device";
+
+insert into t_bios_measurement_topic (id, device_id, units, topic) values (NULL, @select_device, "A", "current.output.L1@select_device" );
+SELECT @test_topic_s_d_current_output_L1 := id FROM t_bios_measurement_topic WHERE topic = "realpower.outlet.2@select_device";
+
+insert into t_bios_measurement_topic (id, device_id, units, topic) values (NULL, @select_device, "A", "current.output@select_device" );
+SELECT @test_topic_s_d_current_output := id FROM t_bios_measurement_topic WHERE topic = "realpower.outlet.2@select_device";
+
+insert into t_bios_measurement_topic (id, device_id, units, topic) values (NULL, @select_device, "V", "voltage.output.L1-N@select_device" );
+SELECT @test_topic_s_d_voltage_otput_L1_N := id FROM t_bios_measurement_topic WHERE topic = "realpower.outlet.2@select_device";
+
+insert into t_bios_measurement_topic (id, device_id, units, topic) values (NULL, @select_device, "V", "voltage.output@select_device" );
+SELECT @test_topic_s_d_voltage_output := id FROM t_bios_measurement_topic WHERE topic = "realpower.outlet.2@select_device";
+
+
+insert into t_bios_measurement 
+    (id, timestamp, value, scale, topic_id)
+values 
+    (NULL, "2014-11-12 09:59:59", 50, 0, @test_topic_m_a_m_realpower_outlet_2 );
+
+insert into t_bios_measurement 
+    (id, timestamp, value, scale, topic_id)
+values 
+    (NULL, "2014-11-12 09:59:58", 2405, -1, @test_topic_m_a_m_realpower_outlet_1 );
+
+insert into t_bios_measurement 
+    (id, timestamp, value, scale, topic_id)
+values 
+    (NULL, "2014-11-12 09:59:57", 2405, -1, @test_topic_m_a_m_realpower_outlet );
+
+/*
+-{"current":[{"id":"6","name":"monitor_asset_measure","realpower.outlet.2":50.0,"realpower.outlet.1":240.5,"realpower.outlet":240.5}]}
+-{"current":[{"id":"7","name":"select_device","status.ups":"TRIM","charge.battery":93.1,"load.default":1.7,"temperature.default":5.6,"realpower.default":0.1,"current.output.L1":1.2,"current.output":3.1,"voltage.output.L1-N":0.1,"voltage.output":0.3}]}
+*/
 
 /* Total rack Power tests */
 INSERT INTO t_bios_discovered_device (name, id_device_type) values ("test_rc_pwr_epdu1", @device_epdu);
