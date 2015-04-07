3de82ff71c11893347ee2fa50207ba3b6120f8ab
Karol Hrdina <karolhrdina@eaton.com>
2015-04-07 15:55:34 +0200

    Removed archaic dangling src/simple
    Signed-off-by: Michal Vyskocil <michalvyskocil@eaton.com>

--- src/include/str_defs.c
+++ src/include/str_defs.c
@@ -2,12 +2,9 @@
 
 /* Implement variables declared in str_defs.h */
 /* NOTE: In header file the declaration used to be static const, now it is extern const. In source file: const in .c */
-const char* DB_SOCK = "ipc://@/poord/persistence";
-const char* FILIP_SOCK = "ipc://@/poord/netlogic";
-const char* CLI_SOCK = "ipc://@/poord/cli";
-
 const char* DRIVER_NMAP_SOCK = "ipc://@/bios/driver/nmap";
 const char* DRIVER_NMAP_REPLY = "ipc://@/bios/driver/nmap_reply";
+
 const char* MLM_ENDPOINT = "ipc://@/malamute";
 
 const char *AVG_STEPS[AVG_STEPS_SIZE] = {
@@ -20,5 +17,6 @@ const char *AVG_STEPS[AVG_STEPS_SIZE] = {
 
 const char *AVG_TYPES[AVG_TYPES_SIZE] = {
     "arithmetic_mean",
-    "arithmetic_mean_mm"
+    "min",
+    "max"
 };
