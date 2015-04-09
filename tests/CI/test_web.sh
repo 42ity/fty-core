ca8e76164039fb80a575a6608ca5b7f601d3aca2
Jim Klimov <jim@jimklimov.com>
2015-04-09 16:18:40 +0200

    BIOS-592: test_web.sh : more robust checks for a capable REST API server
    Signed-off-by: Jim Klimov <jim@jimklimov.com>

--- tests/CI/test_web.sh
+++ tests/CI/test_web.sh
@@ -94,6 +94,12 @@ if ! $SASLTEST -u "$BIOS_USER" -p "$BIOS_PASSWD" -s bios > /dev/null; then
 fi
 
 logmsg_info "Testing webserver ability to serve the REST API"
+if [ -n "`api_get "" 2>&1 | grep '< HTTP/.* 500'`" ]; then
+    logmsg_error "api_get() returned an error:"
+    api_get "" >&2
+    CODE=4 die "Webserver code is deeply broken, please fix it first!"
+fi
+
 if [ -z "`api_get "" 2>&1 | grep '< HTTP/.* 404 Not Found'`" ]; then
     # We do expect an HTTP-404 on the API base URL
     logmsg_error "api_get() returned an error:"
@@ -101,10 +107,11 @@ if [ -z "`api_get "" 2>&1 | grep '< HTTP/.* 404 Not Found'`" ]; then
     CODE=4 die "Webserver is not running or serving the REST API, please start it first!"
 fi
 
-if [ -n "`api_get "" 2>&1 | grep '< HTTP/.* 500'`" ]; then
+if [ -z "`api_get '/oauth2/token' 2>&1 | grep '< HTTP/.* 200 OK'`" ]; then
+    # We expect that the login service responds
     logmsg_error "api_get() returned an error:"
-    api_get "" >&2
-    CODE=4 die "Webserver code is deeply broken, please fix it first!"
+    api_get "/oauth2/token" >&2
+    CODE=4 die "Webserver is not running or serving the REST API, please start it first!"
 fi
 
 cd "`dirname "$0"`"
