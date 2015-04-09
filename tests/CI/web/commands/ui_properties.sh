c923b6143325ab738184691e4fd51e91399afba3
Jim Klimov <jim@jimklimov.com>
2015-04-09 22:39:28 +0200

    BIOS-592: ui_properties.sh : typo fixes; allow the first GET failure to not be fatal
    Signed-off-by: Jim Klimov <jim@jimklimov.com>

--- tests/CI/web/commands/ui_properties.sh
+++ tests/CI/web/commands/ui_properties.sh
@@ -1,19 +1,21 @@
 #TEST for /ui/properties GET/PUT
 
-test_it "GET_ui_properties"
+test_it "GET_ui_properties_1"
 api_get /ui/properties | grep -q '{"key1" : "value1", "key2" : "value2"'
-print_result $?
+WEBLIB_QUICKFAIL=no print_result $?
 
-test_it "PUT_ui_properties"
-api_auth_put /ui/properties '{"key1" : "value1", "key2" : "value42"}' | grep -q 'HTTP/1.1 200 OK'
+test_it "PUT_ui_properties_1"
+api_auth_put /ui/properties '{"key1" : "value1", "key2" : "value42"}' | \
+    grep -q 'HTTP/1.1 200 OK'
 print_result $?
 
 test_it "GET_ui_properties_2"
-api_get /ui/properties | grep -q '{"key1" : "value1", "key2" : "value42"}'
+api_get /ui/properties | grep -q '{"key1" : "value1", "key2" : "value42"'
 print_result $?
 
 test_it "PUT_ui_properties_2"
-api_auth_put /ui/properties '{"key1" : "value1", "key2" : "value2"}' | grep -q 'HTTP/1.1 200 OK'
+api_auth_put /ui/properties '{"key1" : "value1", "key2" : "value2"}' | \
+    grep -q 'HTTP/1.1 200 OK'
 print_result $?
 
 #TODO: can't validate JSON via cxxtools
