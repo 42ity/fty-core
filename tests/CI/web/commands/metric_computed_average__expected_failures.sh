f8a744fa416b5384b614ebfceed4508c9dc8b856
Jim Klimov <jim@jimklimov.com>
2015-04-10 14:44:50 +0200

    BIOS-592: metric_computed_average* tests now separated into those expected to fail and those expected to succeed (and the latter is disabled because backend is not properly implemented)
    Signed-off-by: Jim Klimov <jim@jimklimov.com>

--- tests/CI/web/commands/metric_computed_average__expected_failures.sh
+++ tests/CI/web/commands/metric_computed_average__expected_failures.sh
@@ -34,8 +34,3 @@ print_result $?
 test_it "metric_computed_average-wrong__4"
 api_get '/metric/computed/average?start_ts=20130101000000Z&end_ts=20160101000000Z&type=arithmetic_mean&step=5ft&element_id=26&source=temperature.thermal_zone0' | grep '400 Bad Request'
 print_result $?
-
-
-test_it "metric_computed_average-correct__1"
-api_get_jsonv '/metric/computed/average?end_ts=20160101000000Z&start_ts=20130101000000Z&type=arithmetic_mean&step=8h&element_id=26&source=temperature.thermal_zone0' >&5
-print_result $?
