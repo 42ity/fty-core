586299b52c99c6d6201c7111edc5930898511835
Jim Klimov <jim@jimklimov.com>
2015-04-10 16:48:43 +0200

    ci-check-vm.sh : try to avoid a full-rebuild strange failure - 3
    Signed-off-by: Jim Klimov <jim@jimklimov.com>

--- tests/CI/ci-check-vm.sh
+++ tests/CI/ci-check-vm.sh
@@ -100,15 +100,16 @@ copy_project() {
 
 remote_make() {
     BCHECKOUTDIR=$(basename $CHECKOUTDIR)
-    if ssh root@$VM -p $PORT "cd $BCHECKOUTDIR && [ -s ${MAKELOG} ]" ; then
+    BMAKELOG=$(basename $MAKELOG)
+    if ssh root@$VM -p $PORT "cd $BCHECKOUTDIR && [ -s ${BMAKELOG} ]" ; then
         # This branch was already configured and compiled on that VM, refresh only
         echo "-- compiling to refresh"
-        ssh root@$VM -t -p $PORT "/bin/bash --login -x -c 'set -o pipefail ; cd $BCHECKOUTDIR && { ./autogen.sh --nodistclean ${AUTOGEN_ACTION_MAKE} install 2>&1 | tee -a ${MAKELOG}; } && echo === remote build finished ok'"
+        ssh root@$VM -t -p $PORT "/bin/bash --login -x -c 'set -o pipefail ; cd $BCHECKOUTDIR && { ./autogen.sh --nodistclean ${AUTOGEN_ACTION_MAKE} install 2>&1 | tee -a ${BMAKELOG}; }'"
         return $?
     else
         # Newly fetched branch - clean up, configure and make it fully
         echo "-- compiling to rebuild"
-        ssh root@$VM -t -p $PORT "/bin/bash --login -x -c 'set -o pipefail ; cd $BCHECKOUTDIR && { eval ./autogen.sh --configure-flags \"--prefix=\$HOME\ --with-saslauthd-mux=/var/run/saslauthd/mux\" ${AUTOGEN_ACTION_BUILD} install 2>&1 | tee ${MAKELOG}; } && echo === remote build finished ok'"
+        ssh root@$VM -t -p $PORT "/bin/bash --login -x -c 'set -o pipefail ; cd $BCHECKOUTDIR && { eval ./autogen.sh --configure-flags \"--prefix=\$HOME\ --with-saslauthd-mux=/var/run/saslauthd/mux\" ${AUTOGEN_ACTION_BUILD} install 2>&1 | tee ${BMAKELOG}; }'"
         return $?
     fi
 }
