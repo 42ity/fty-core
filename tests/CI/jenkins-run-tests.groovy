4f745f390a29a04f883821a041743203533c0b74
Jim Klimov <jim@jimklimov.com>
2015-04-17 11:44:37 +0200

    jenkins-run-tests scripts updated to report the URL to console output of a running build ASAP
    Signed-off-by: Jim Klimov <jim@jimklimov.com>

--- tests/CI/jenkins-run-tests.groovy
+++ tests/CI/jenkins-run-tests.groovy
@@ -98,7 +98,7 @@ for(
     println "=== Starting $jobName ===";
     lastbuild = doJob(jobName, jobParams);
     if( lastbuild == 0 ) {
-        println "Job $jobName is disabled, skipped";
+        println "SKIPPED: Job $jobName is disabled, skipped";
     } else {
         result = lastbuild.getResult();
             if ( result == Result.SUCCESS ) {
@@ -107,7 +107,15 @@ for(
         } else {
             println "WARNING: " + jobName + " result is " + result.toString();
             println "see " + lastbuild.getAbsoluteUrl() + "  for failed build";
-            println "or  " + lastbuild.getAbsoluteUrl() + "console for console output.";
         }
     }
 }
+
+// get current thread / Executor
+def thr = Thread.currentThread();
+// get current build
+def thisbuild = thr?.executable;
+
+println "=== Overall build result: " + thisbuild.result;
+println "Build scheduled by " + username + " on " + thisbuild.scheduled;
+println "Build duration: " + thisbuild.duration.display;
