486b426d9cc032342dd32143b570c25f6cd5263b
Jim Klimov <jim@jimklimov.com>
2015-04-29 11:16:13 +0200

    ci-reset-virtual-machine.sh and ci-setup-test-machine.sh : the limitations to installed package set pertain more to the test/build environment setup than to general VM instantiator
    Signed-off-by: Jim Klimov <jim@jimklimov.com>

--- tests/CI/ci-setup-test-machine.sh
+++ tests/CI/ci-setup-test-machine.sh
@@ -29,14 +29,36 @@ CHECKOUTDIR=$(realpath $SCRIPTDIR/../..)
 [ -z "$TZ" ] && TZ=UTC
 export LANG LANGUAGE LC_ALL TZ
 
-update_system() {
-    # if debian
-    curl http://obs.roz.lab.etn.com:82/Pool:/master/Debian_8.0/Release.key | apt-key add -
-    # curl http://obs.mbt.lab.etn.com:82/Pool:/master/Debian_8.0/Release.key | apt-key add -
-    apt-get clean all
-    apt-get update
+limit_packages_recommends() {
+    echo "INFO: Tell APT to not install packages from 'Recommends' category"
+    mkdir -p /etc/apt/apt.conf.d
+    echo 'APT::Install-Recommends "false";' > \
+       "/etc/apt/apt.conf.d/02no-recommends"
+}
+
+limit_packages_paths() {
+    echo "INFO: Trying to influence DPKG to avoid certain large useless files"
+# see also "dpkg --set-selections" in ci-setup-test-machine.sh
+    mkdir -p /etc/dpkg/dpkg.cfg.d
+    echo '# avoid installation of docs packages except ours
+path-exclude=/usr/share/doc/*
+path-include=/usr/share/doc/bios*
+path-include=/usr/share/doc/core*
+# we need to keep copyright files for legal reasons
+path-include=/usr/share/doc/*/copyright
+# manpages can stay, we install a few ourselves
+path-exclude=/usr/share/groff/*
+path-exclude=/usr/share/info/*
+# lintian stuff is small, but really unnecessary
+path-exclude=/usr/share/lintian/*
+path-exclude=/usr/share/linda/*
+' >> /etc/dpkg/dpkg.cfg.d/excludes
+}
+
+limit_packages_docs() {
     # try to deny installation of some hugely useless packages
     # tex-docs for example are huge (850Mb) and useless on a test container
+    echo "INFO: Tell DPKG to not install some large documentation packages"
     for P in \
         docutils-doc libssl-doc python-docutils \
         texlive-fonts-recommended-doc \
@@ -48,10 +70,21 @@ update_system() {
     ; do
         apt-mark hold "$P" >&2
         echo "$P  purge"
-    done | dpkg --set-selections
+    done | dpkg --set-selections 2>/dev/null
+}
+
+update_system() {
+    # if debian
+    curl http://obs.roz.lab.etn.com:82/Pool:/master/Debian_8.0/Release.key | apt-key add -
+    # curl http://obs.mbt.lab.etn.com:82/Pool:/master/Debian_8.0/Release.key | apt-key add -
+    apt-get clean all
+    apt-get update
+    limit_packages_recommends
+    limit_packages_paths
     apt-get -f -y --force-yes --fix-missing install
+    limit_packages_docs
     apt-get -f -y --force-yes install devscripts sudo doxygen curl git python-mysqldb \
-        cppcheck msmtp libtool
+        cppcheck msmtp libtool cpp gcc autoconf automake m4 pkg-config equivs dh-make
     mk-build-deps --tool 'apt-get --yes --force-yes' --install $CHECKOUTDIR/obs/core.dsc
     # and just to be sure about these space-hungry beasts
     apt-get remove --purge \
