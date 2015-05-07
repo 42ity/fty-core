86dfbc7ebb6d49a6d60737bed5da1bf9ed194139
Jim Klimov <jim@jimklimov.com>
2015-05-07 10:08:36 +0200

    test-alert.cc : fix crash if there is a non-zero amount of alerts before the insert-select test
    Signed-off-by: Jim Klimov <jim@jimklimov.com>

--- tests/persist/test-alert.cc
+++ tests/persist/test-alert.cc
@@ -25,6 +25,12 @@ TEST_CASE("t_bios_alert INSERT/DELETE #1","[db][CRUD][insert][delete][alert][cru
     m_alrt_ntfctn_t     notification = 12;
     int64_t             date_from = 2049829;
 
+    // before first insert
+    auto reply_select = select_alert_all_opened (conn);
+    REQUIRE ( reply_select.status == 1 );
+    REQUIRE ( reply_select.item.size() >= 0 );
+    size_type           oldsize = reply_select.item.size() ;
+
     // first insert
     auto reply_insert = insert_into_alert (conn, rule_name, priority, alert_state, description, notification, date_from);
     REQUIRE ( reply_insert.status == 1 );
@@ -33,9 +39,9 @@ TEST_CASE("t_bios_alert INSERT/DELETE #1","[db][CRUD][insert][delete][alert][cru
     REQUIRE ( reply_insert.affected_rows == 1 );
 
     // check select
-    auto reply_select = select_alert_all_opened (conn);
+    reply_select = select_alert_all_opened (conn);
     REQUIRE ( reply_select.status == 1 );
-    REQUIRE ( reply_select.item.size() == 1 );
+    REQUIRE ( reply_select.item.size() == oldsize + 1 );
     REQUIRE ( reply_select.item.at(0).id == rowid );
     REQUIRE ( reply_select.item.at(0).rule_name == std::string(rule_name) );
     REQUIRE ( reply_select.item.at(0).alert_state == alert_state );
@@ -53,12 +59,12 @@ TEST_CASE("t_bios_alert INSERT/DELETE #1","[db][CRUD][insert][delete][alert][cru
     auto reply_delete = delete_from_alert (conn, rowid);
     REQUIRE ( reply_delete.affected_rows == 1 );
     REQUIRE ( reply_delete.status == 1 );
-    
+
     // check select
     reply_select = select_alert_all_opened (conn);
     REQUIRE ( reply_select.status == 1 );
-    REQUIRE ( reply_select.item.size() == 0 );
-    
+    REQUIRE ( reply_select.item.size() == oldsize );
+
     // must handle second delete without crash
     reply_delete = delete_from_alert (conn, rowid);
     REQUIRE ( reply_delete.affected_rows == 0 );
