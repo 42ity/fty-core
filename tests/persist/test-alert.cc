689047202e54fa2a489eb973c5c17e1c2aa34e7d
Jim Klimov <jim@jimklimov.com>
2015-05-07 11:30:28 +0200

    test-alert.cc : fix crash if there is a non-zero amount of alerts before the insert-select test - fix v3
    Signed-off-by: Jim Klimov <jim@jimklimov.com>

--- tests/persist/test-alert.cc
+++ tests/persist/test-alert.cc
@@ -14,10 +14,10 @@ TEST_CASE("t_bios_alert INSERT/DELETE #1","[db][CRUD][insert][delete][alert][cru
     log_open ();
 
     log_info ("=============== ALERT: ALERT DELETE/INSERT #1 ==================");
-    
+
     tntdb::Connection conn;
     REQUIRE_NOTHROW ( conn = tntdb::connectCached(url) );
-    
+
     const char         *rule_name = "this is the GREAT rule name1";
     a_elmnt_pr_t        priority  = 1;
     m_alrt_state_t      alert_state = 1;
@@ -28,7 +28,7 @@ TEST_CASE("t_bios_alert INSERT/DELETE #1","[db][CRUD][insert][delete][alert][cru
     // before first insert
     auto reply_select = select_alert_all_opened (conn);
     REQUIRE ( reply_select.status == 1 );
-    auto                oldsize = reply_select.item.size() ;
+    auto                oldsize_a = reply_select.item.size() ;
 
     // first insert
     auto reply_insert = insert_into_alert (conn, rule_name, priority, alert_state, description, notification, date_from);
@@ -40,14 +40,14 @@ TEST_CASE("t_bios_alert INSERT/DELETE #1","[db][CRUD][insert][delete][alert][cru
     // check select
     reply_select = select_alert_all_opened (conn);
     REQUIRE ( reply_select.status == 1 );
-    REQUIRE ( reply_select.item.size() == (oldsize + 1) );
-    REQUIRE ( reply_select.item.at(0).id == rowid );
-    REQUIRE ( reply_select.item.at(0).rule_name == std::string(rule_name) );
-    REQUIRE ( reply_select.item.at(0).alert_state == alert_state );
-    REQUIRE ( reply_select.item.at(0).description == std::string(description) );
-    REQUIRE ( reply_select.item.at(0).notification == notification );
-    REQUIRE ( reply_select.item.at(0).date_from == date_from );
-    REQUIRE ( reply_select.item.at(0).date_till == 0 );
+    REQUIRE ( reply_select.item.size() == (oldsize_a + 1) );
+    REQUIRE ( reply_select.item.at(oldsize_a).id == rowid );
+    REQUIRE ( reply_select.item.at(oldsize_a).rule_name == std::string(rule_name) );
+    REQUIRE ( reply_select.item.at(oldsize_a).alert_state == alert_state );
+    REQUIRE ( reply_select.item.at(oldsize_a).description == std::string(description) );
+    REQUIRE ( reply_select.item.at(oldsize_a).notification == notification );
+    REQUIRE ( reply_select.item.at(oldsize_a).date_from == date_from );
+    REQUIRE ( reply_select.item.at(oldsize_a).date_till == 0 );
 
     // must handle duplicate insert without insert
     reply_insert = insert_into_alert (conn, rule_name, priority, alert_state, description, notification, date_from);
@@ -62,7 +62,7 @@ TEST_CASE("t_bios_alert INSERT/DELETE #1","[db][CRUD][insert][delete][alert][cru
     // check select
     reply_select = select_alert_all_opened (conn);
     REQUIRE ( reply_select.status == 1 );
-    REQUIRE ( reply_select.item.size() == oldsize );
+    REQUIRE ( reply_select.item.size() == oldsize_a );
 
     // must handle second delete without crash
     reply_delete = delete_from_alert (conn, rowid);
@@ -77,16 +77,21 @@ TEST_CASE("t_bios_alert UPDATE end date #2","[db][CRUD][update][alert][crud_test
     log_open ();
 
     log_info ("=============== ALERT: ALERT UPDATE end date #2 ==================");
-    
+
     tntdb::Connection conn;
     REQUIRE_NOTHROW ( conn = tntdb::connectCached(url) );
-    
+
     const char         *rule_name = "this is the GREAT rule name2";
     a_elmnt_pr_t        priority  = 1;
     m_alrt_state_t      alert_state = 1;
     const char         *description = "very small description";
     m_alrt_ntfctn_t     notification = 12;
     int64_t             date_from = 2049829;
+
+    // before first insert
+    auto reply_select = select_alert_all_opened (conn);
+    REQUIRE ( reply_select.status == 1 );
+    auto                oldsize_a = reply_select.item.size() ;
 
     // insert
     auto reply_insert = insert_into_alert (conn, rule_name, priority, alert_state, description, notification, date_from);
@@ -99,20 +104,20 @@ TEST_CASE("t_bios_alert UPDATE end date #2","[db][CRUD][update][alert][crud_test
     REQUIRE (reply_update.affected_rows == 1);
 
     // check select
-    auto reply_select = select_alert_all_closed (conn);
+    reply_select = select_alert_all_closed (conn);
     REQUIRE ( reply_select.status == 1 );
-    REQUIRE ( reply_select.item.size() == 1 );
-    REQUIRE ( reply_select.item.at(0).id == rowid );
-    REQUIRE ( reply_select.item.at(0).rule_name == std::string(rule_name) );
-    REQUIRE ( reply_select.item.at(0).alert_state == alert_state );
-    REQUIRE ( reply_select.item.at(0).description == std::string(description) );
-    REQUIRE ( reply_select.item.at(0).notification == notification );
-    REQUIRE ( reply_select.item.at(0).date_from == date_from );
-    REQUIRE ( reply_select.item.at(0).date_till == date_till );
+    REQUIRE ( reply_select.item.size() == (oldsize_a+1) );
+    REQUIRE ( reply_select.item.at(oldsize_a).id == rowid );
+    REQUIRE ( reply_select.item.at(oldsize_a).rule_name == std::string(rule_name) );
+    REQUIRE ( reply_select.item.at(oldsize_a).alert_state == alert_state );
+    REQUIRE ( reply_select.item.at(oldsize_a).description == std::string(description) );
+    REQUIRE ( reply_select.item.at(oldsize_a).notification == notification );
+    REQUIRE ( reply_select.item.at(oldsize_a).date_from == date_from );
+    REQUIRE ( reply_select.item.at(oldsize_a).date_till == date_till );
 
     // delete
     delete_from_alert (conn, rowid);
-    
+
     log_close();
 }
 
@@ -121,16 +126,21 @@ TEST_CASE("t_bios_alert UPDATE notification #3","[db][CRUD][update][alert][crud_
     log_open ();
 
     log_info ("=============== ALERT: ALERT UPDATE notification #3 ==================");
-    
+
     tntdb::Connection conn;
     REQUIRE_NOTHROW ( conn = tntdb::connectCached(url) );
-    
+
     const char         *rule_name = "this is the GREAT rule name3";
     a_elmnt_pr_t        priority  = 1;
     m_alrt_state_t      alert_state = 1;
     const char         *description = "very small description";
     m_alrt_ntfctn_t     notification = 1;
     int64_t             date_from = 2049829;
+
+    // before first insert
+    auto reply_select = select_alert_all_opened (conn);
+    REQUIRE ( reply_select.status == 1 );
+    auto                oldsize_a = reply_select.item.size() ;
 
     // insert
     auto reply_insert = insert_into_alert (conn, rule_name, priority, alert_state, description, notification, date_from);
@@ -143,33 +153,33 @@ TEST_CASE("t_bios_alert UPDATE notification #3","[db][CRUD][update][alert][crud_
     REQUIRE (reply_update.affected_rows == 1);
 
     // check select
-    auto reply_select = select_alert_all_opened (conn);
+    reply_select = select_alert_all_opened (conn);
     REQUIRE ( reply_select.status == 1 );
-    REQUIRE ( reply_select.item.size() == 1 );
-    REQUIRE ( reply_select.item.at(0).id == rowid );
-    REQUIRE ( reply_select.item.at(0).rule_name == std::string(rule_name) );
-    REQUIRE ( reply_select.item.at(0).alert_state == alert_state );
-    REQUIRE ( reply_select.item.at(0).description == std::string(description) );
-    REQUIRE ( reply_select.item.at(0).notification == ( new_notification | notification ) );
-    REQUIRE ( reply_select.item.at(0).date_from == date_from );
-    REQUIRE ( reply_select.item.at(0).date_till == 0 );
+    REQUIRE ( reply_select.item.size() == (oldsize_a + 1) );
+    REQUIRE ( reply_select.item.at(oldsize_a).id == rowid );
+    REQUIRE ( reply_select.item.at(oldsize_a).rule_name == std::string(rule_name) );
+    REQUIRE ( reply_select.item.at(oldsize_a).alert_state == alert_state );
+    REQUIRE ( reply_select.item.at(oldsize_a).description == std::string(description) );
+    REQUIRE ( reply_select.item.at(oldsize_a).notification == ( new_notification | notification ) );
+    REQUIRE ( reply_select.item.at(oldsize_a).date_from == date_from );
+    REQUIRE ( reply_select.item.at(oldsize_a).date_till == 0 );
 
     // delete
     delete_from_alert (conn, rowid);
-    
+
     log_close();
 }
 
- 
+
 TEST_CASE("t_bios_alert_device INSERT/DELETE #4","[db][CRUD][insert][delete][alert_device][crud_test.sql]")
 {
     log_open ();
 
     log_info ("=============== ALERT DEVICE: ALERT DEVICE DELETE/INSERT #4 ==================");
-    
+
     tntdb::Connection conn;
     REQUIRE_NOTHROW ( conn = tntdb::connectCached(url) );
-    
+
     // insert alert
     const char         *rule_name = "this is the GREAT rule name4";
     a_elmnt_pr_t        priority  = 1;
@@ -203,6 +213,11 @@ TEST_CASE("t_bios_alert_device INSERT/DELETE #4","[db][CRUD][insert][delete][ale
     uint64_t rowid_ma = reply_insert_ma.rowid;
 
 
+    // before first insert
+    auto reply_select = select_alert_devices (conn, rowid_alert);
+    REQUIRE ( reply_select.status == 1 );
+    auto                oldsize_a = reply_select.item.size() ;
+
     // first insert
     auto reply_insert1 = insert_into_alert_device (conn, rowid_alert, element_name);
     REQUIRE ( reply_insert1.status == 1 );
@@ -211,10 +226,10 @@ TEST_CASE("t_bios_alert_device INSERT/DELETE #4","[db][CRUD][insert][delete][ale
     REQUIRE ( reply_insert1.affected_rows == 1 );
 
     // check select
-    auto reply_select = select_alert_devices (conn, rowid_alert);
+    reply_select = select_alert_devices (conn, rowid_alert);
     REQUIRE ( reply_select.status == 1 );
-    REQUIRE ( reply_select.item.size() == 1 );
-    REQUIRE ( reply_select.item.at(0) == rowid_device );
+    REQUIRE ( reply_select.item.size() == (oldsize_a + 1) );
+    REQUIRE ( reply_select.item.at(oldsize_a) == rowid_device );
 
     // must handle duplicate insert without insert
     reply_insert1 = insert_into_alert_device (conn, rowid_alert, element_name);
@@ -225,12 +240,12 @@ TEST_CASE("t_bios_alert_device INSERT/DELETE #4","[db][CRUD][insert][delete][ale
     auto reply_delete = delete_from_alert_device (conn, rowid);
     REQUIRE ( reply_delete.affected_rows == 1 );
     REQUIRE ( reply_delete.status == 1 );
-    
+
     // check select
     reply_select = select_alert_devices (conn, rowid_alert);
     REQUIRE ( reply_select.status == 1 );
-    REQUIRE ( reply_select.item.size() == 0 );
-    
+    REQUIRE ( reply_select.item.size() == oldsize_a );
+
     // must handle second delete without crash
     reply_delete = delete_from_alert_device (conn, rowid);
     REQUIRE ( reply_delete.affected_rows == 0 );
@@ -254,10 +269,10 @@ TEST_CASE("t_bios_alert_device INSERT/DELETE #5","[db][CRUD][insert][delete][ale
     log_open ();
 
     log_info ("=============== ALERT DEVICE: ALERT DEVICE INSERT_deviceS #5 ==================");
-    
+
     tntdb::Connection conn;
     REQUIRE_NOTHROW ( conn = tntdb::connectCached(url) );
-    
+
     // insert alert
     const char         *rule_name = "this is the GREAT rule name5";
     a_elmnt_pr_t        priority  = 1;
@@ -306,6 +321,11 @@ TEST_CASE("t_bios_alert_device INSERT/DELETE #5","[db][CRUD][insert][delete][ale
     auto reply_insert_ma2 = insert_into_monitor_asset_relation (conn, rowid_device2, rowid_element2);
     uint64_t rowid_ma2 = reply_insert_ma2.rowid;
 
+    // before first insert
+    auto reply_select = select_alert_devices (conn, rowid_alert);
+    REQUIRE ( reply_select.status == 1 );
+    auto                oldsize_d = reply_select.item.size() ;
+
     // first insert
     std::vector<std::string> names;
     names.push_back(std::string(element_name1));
@@ -316,11 +336,11 @@ TEST_CASE("t_bios_alert_device INSERT/DELETE #5","[db][CRUD][insert][delete][ale
     REQUIRE ( reply_insert1.affected_rows == 2 );
 
     // check select
-    auto reply_select = select_alert_devices (conn, rowid_alert);
+    reply_select = select_alert_devices (conn, rowid_alert);
     REQUIRE ( reply_select.status == 1 );
-    REQUIRE ( reply_select.item.size() == 2 );
-    bool first  = ( reply_select.item.at(0) == rowid_device1 ) && ( reply_select.item.at(1) == rowid_device2 );
-    bool second = ( reply_select.item.at(0) == rowid_device2 ) && ( reply_select.item.at(1) == rowid_device1 );
+    REQUIRE ( reply_select.item.size() == (oldsize_d + 2) );
+    bool first  = ( reply_select.item.at(oldsize_d) == rowid_device1 ) && ( reply_select.item.at(oldsize_d+1) == rowid_device2 );
+    bool second = ( reply_select.item.at(oldsize_d) == rowid_device2 ) && ( reply_select.item.at(oldsize_d+1) == rowid_device1 );
 
     REQUIRE ( (first || second) );
 
@@ -355,11 +375,10 @@ TEST_CASE("insert_alert_new #6","[db][CRUD][insert][delete][alert][crud_test.sql
     log_open ();
 
     log_info ("=============== insert ALERT NEW: #6 ==================");
-    
+
     tntdb::Connection conn;
     REQUIRE_NOTHROW ( conn = tntdb::connectCached(url) );
-    
-    
+
     //insert element
     const char *element_name1 = "test_element_name5.1";
     a_elmnt_tp_id_t  element_type_id = 6;
@@ -397,6 +416,11 @@ TEST_CASE("insert_alert_new #6","[db][CRUD][insert][delete][alert][crud_test.sql
     auto reply_insert_ma2 = insert_into_monitor_asset_relation (conn, rowid_device2, rowid_element2);
     uint64_t rowid_ma2 = reply_insert_ma2.rowid;
 
+    // before first insert
+    auto reply_select1 = select_alert_all_opened (conn);
+    REQUIRE ( reply_select1.status == 1 );
+    auto                oldsize_a = reply_select1.item.size() ;
+
     // first insert
     std::vector<std::string> names;
     names.push_back(std::string(element_name1));
@@ -422,17 +446,17 @@ TEST_CASE("insert_alert_new #6","[db][CRUD][insert][delete][alert][crud_test.sql
     bool first  = ( reply_select.item.at(0) == rowid_device1 ) && ( reply_select.item.at(1) == rowid_device2 );
     bool second = ( reply_select.item.at(0) == rowid_device2 ) && ( reply_select.item.at(1) == rowid_device1 );
     REQUIRE ( (first || second) );
- 
-    auto reply_select1 = select_alert_all_opened (conn);
+
+    reply_select1 = select_alert_all_opened (conn);
     REQUIRE ( reply_select1.status == 1 );
-    REQUIRE ( reply_select1.item.size() == 1 );
-    REQUIRE ( reply_select1.item.at(0).id == rowid_alert );
-    REQUIRE ( reply_select1.item.at(0).rule_name == std::string(rule_name) );
-    REQUIRE ( reply_select1.item.at(0).alert_state == alert_state );
-    REQUIRE ( reply_select1.item.at(0).description == std::string(description) );
-    REQUIRE ( reply_select1.item.at(0).notification == notification );
-    REQUIRE ( reply_select1.item.at(0).date_from == date_from );
-    REQUIRE ( reply_select1.item.at(0).date_till == 0 );
+    REQUIRE ( reply_select1.item.size() == (oldsize_a + 1) );
+    REQUIRE ( reply_select1.item.at(oldsize_a).id == rowid_alert );
+    REQUIRE ( reply_select1.item.at(oldsize_a).rule_name == std::string(rule_name) );
+    REQUIRE ( reply_select1.item.at(oldsize_a).alert_state == alert_state );
+    REQUIRE ( reply_select1.item.at(oldsize_a).description == std::string(description) );
+    REQUIRE ( reply_select1.item.at(oldsize_a).notification == notification );
+    REQUIRE ( reply_select1.item.at(oldsize_a).date_from == date_from );
+    REQUIRE ( reply_select1.item.at(oldsize_a).date_till == 0 );
 
     // delete
     delete_from_alert_device_byalert (conn, rowid_alert);
@@ -456,7 +480,7 @@ TEST_CASE("t_bios_alert UPDATE end date #7","[db][CRUD][update][alert][crud_test
     log_open ();
 
     log_info ("=============== ALERT: ALERT UPDATE end date #7 ==================");
-    
+
     tntdb::Connection conn;
     REQUIRE_NOTHROW ( conn = tntdb::connectCached(url) );
     
@@ -466,6 +490,11 @@ TEST_CASE("t_bios_alert UPDATE end date #7","[db][CRUD][update][alert][crud_test
     const char         *description = "very small description";
     m_alrt_ntfctn_t     notification = 12;
     int64_t             date_from = 2049829;
+
+    // before first insert
+    auto reply_select = select_alert_all_opened (conn);
+    REQUIRE ( reply_select.status == 1 );
+    auto                oldsize_a = reply_select.item.size() ;
 
     // insert
     auto reply_insert = insert_into_alert (conn, rule_name, priority, alert_state, description, notification, date_from);
@@ -478,16 +507,16 @@ TEST_CASE("t_bios_alert UPDATE end date #7","[db][CRUD][update][alert][crud_test
     REQUIRE (reply_update.affected_rows == 1);
 
     // check select
-    auto reply_select = select_alert_all_closed (conn);
-    REQUIRE ( reply_select.status == 1 );
+    reply_select = select_alert_all_closed (conn);
+    REQUIRE ( reply_select.status == (oldsize_a + 1) );
     REQUIRE ( reply_select.item.size() == 1 );
-    REQUIRE ( reply_select.item.at(0).id == rowid );
-    REQUIRE ( reply_select.item.at(0).rule_name == std::string(rule_name) );
-    REQUIRE ( reply_select.item.at(0).alert_state == alert_state );
-    REQUIRE ( reply_select.item.at(0).description == std::string(description) );
-    REQUIRE ( reply_select.item.at(0).notification == notification );
-    REQUIRE ( reply_select.item.at(0).date_from == date_from );
-    REQUIRE ( reply_select.item.at(0).date_till == date_till );
+    REQUIRE ( reply_select.item.at(oldsize_a).id == rowid );
+    REQUIRE ( reply_select.item.at(oldsize_a).rule_name == std::string(rule_name) );
+    REQUIRE ( reply_select.item.at(oldsize_a).alert_state == alert_state );
+    REQUIRE ( reply_select.item.at(oldsize_a).description == std::string(description) );
+    REQUIRE ( reply_select.item.at(oldsize_a).notification == notification );
+    REQUIRE ( reply_select.item.at(oldsize_a).date_from == date_from );
+    REQUIRE ( reply_select.item.at(oldsize_a).date_till == date_till );
 
     // delete
     delete_from_alert (conn, rowid);
