4d1be5fb6b67de416277893086f0412bbe1d6954
Jim Klimov <jim@jimklimov.com>
2015-05-13 09:54:09 +0200

    Made consistent use of configure-flags in CI-initiated compilations of the (tested) code
    Signed-off-by: Jim Klimov <jim@jimklimov.com>

--- tests/CI/ci-rackpower.sh
+++ tests/CI/ci-rackpower.sh
@@ -60,7 +60,9 @@ kill_daemons() {
 
 logmsg_info "Ensuring that the tested programs have been built and up-to-date"
 if [ ! -f "$BUILDSUBDIR/Makefile" ] ; then
-    ./autogen.sh --nodistclean ${AUTOGEN_ACTION_CONFIG}
+    ./autogen.sh --nodistclean --configure-flags \
+        "--prefix=$HOME --with-saslauthd-mux=/var/run/saslauthd/mux" \
+        ${AUTOGEN_ACTION_CONFIG}
 fi
 ./autogen.sh ${AUTOGEN_ACTION_MAKE} web-test-deps db-ng agent-nut driver-nmap netmon
 ./autogen.sh --noparmake ${AUTOGEN_ACTION_MAKE} web-test \
