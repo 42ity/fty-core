f9a16fd17bb115cf0974911d6a46b910c07a86b2
Karol Hrdina <karolhrdina@eaton.com>
2015-05-25 13:00:19 +0200

    Code review changes
    * including ambiguous header files in a better way
    
    Signed-off-by: Karol Hrdina <karolhrdina@eaton.com>

--- src/agents/computation/cm-agent-web.cc
+++ src/agents/computation/cm-agent-web.cc
@@ -36,7 +36,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "utils_ymsg.h"
 #include "utils_ymsg++.h"
 
-#include "measurements.h"
+#include "db/measurements.h"
 #include "cm-agent-utils.h"
 #include "cm-agent-web.h"
 #include "cleanup.h"
