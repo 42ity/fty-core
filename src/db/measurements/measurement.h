fad0b069653d115ed4376db3cfcd0eb3ee9a3ecf
Karol Hrdina <karolhrdina@eaton.com>
2015-05-26 10:42:34 +0200

    Problem: Disunite database api Solution: Based on common consensus implement a new return type in measurements.
    * persist::get_measurements_averages() expanded with return argument that returns the absolute last timestamp of a given average stored
    
    Signed-off-by: Karol Hrdina <karolhrdina@eaton.com>

--- src/db/measurements/measurement.h
+++ src/db/measurements/measurement.h
@@ -28,21 +28,29 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <map>
 #include <string>
 
+
 namespace persist
 {
 
+
+// TODO: struct reply is placed here only temporarily for the first PR, then we'll decide where to move it 
+struct reply {
+    int rv; // return value
+    uint64_t row_id; // row id 
+    uint64_t affected_rows; // number of rows affected
+};
+
 // -1 err/failure , 0 ok, 1 element_id not found, 2 topic not found    
-int
+reply
 get_measurements
 (uint64_t element_id, const char *topic, int64_t start_timestamp, int64_t end_timestamp, bool left_interval_closed, bool right_interval_closed, std::map <int64_t, double>& measurements, std::string& unit);
 
 
-// TODO: change to step
-int
+reply
 get_measurements_averages
-(uint64_t element_id, const char *topic, const char *type, int64_t start_timestamp, int64_t end_timestamp, std::map <int64_t, double>& measurements, std::string& unit);
+(uint64_t element_id, const char *topic, const char *step, int64_t start_timestamp, int64_t end_timestamp, std::map <int64_t, double>& measurements, std::string& unit, int64_t& last_timestamp);
 
-int
+reply
 get_measurements_sampled
 (uint64_t element_id, const char *topic, int64_t start_timestamp, int64_t end_timestamp, std::map <int64_t, double>& measurements, std::string& unit);
     
