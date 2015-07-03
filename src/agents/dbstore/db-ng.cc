dff4c37aebc661f7d6542ca46e9f8288ea91be55
Michal Hrusecky <MichalHrusecky@Eaton.com>
2015-07-03 13:02:07 +0200

    Fix typo preventing compilation
    Signed-off-by: Michal Hrusecky <Michal@Hrusecky.net>

--- src/agents/dbstore/db-ng.cc
+++ src/agents/dbstore/db-ng.cc
@@ -36,7 +36,7 @@ int main (int argc, char *argv []) {
 
     // We are listening for measurements
     mlm_client_set_consumer(client, bios_get_stream_main(), ".*"); // to be dropped onc we migrate to multiple streams
-    mlm_client_set_consumer(client, bios_get_stream_measurement(), ".*");
+    mlm_client_set_consumer(client, bios_get_stream_measurements(), ".*");
     while(!zsys_interrupted) {
         _scoped_ymsg_t *in = bios_agent_recv(client);
         if ( in == NULL )
