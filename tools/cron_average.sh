f26d7b1812d99bf3be65c1f176fc0acdeeb2df71
Karol Hrdina <karolhrdina@eaton.com>
2015-07-08 16:08:59 +0200

    BIOS-890: script done
    Signed-off-by: Karol Hrdina <karolhrdina@eaton.com>

--- tools/cron_average.sh
+++ tools/cron_average.sh
@@ -3,25 +3,19 @@
 # USAGE
 # cron_average 
 # At the moment script only outputs curl requests
-
+#
 # PURPOSE:
 # Have cron job periodically call this script 
 
-# TODO
-#   simple lockfile preventing unintentional double runs
-#   store value of last run in lockfile, otherwise assign start_timestamp value of 0 (== 1970-01-01) 
-#   Add <start_timestamp> <end_timestamp> AND
-#   convert `date +%s`'s format to rfc-11 format so that caller does not have to bother with this "knowledge"
-
-
-declare -r START_TIMESTAMP=
-declare -r END_TIMESTAMP=
-
+# supported average types and steps
 declare -ar TYPE=("arithmetic_mean" "min" "max")
 declare -ar STEP=("15m" "30m" "1h" "8h" "24h")
 
 declare -r SERVER="127.0.0.1"
 declare -r PORT="8000"
+
+LOCKFILE=/var/lib/bios/agent-cm/cron_average.lock
+TIMEFILE=/var/lib/bios/agent-cm/cron_average.time
 
 # Helper echo to stderr function
 echoerr() { echo "$@" 1>&2; }
@@ -30,26 +24,25 @@ echoerr() { echo "$@" 1>&2; }
 function join { local IFS="$1"; shift; echo "$*"; }
 
 # Converts asset element id to (discovered) device element id
-# Returns 1 on error
+#
+# Returns:
+#   2 - error (bad args, mysql error ...)
+#   1 - no device id exists for given asset element
+#   0 - success
 # Arguments:
-# $1 - asset element id
-# $2 - name of output parameter
+#   $1 - asset element id
 element_to_device_id () {
-    if [ $# -ne 2 ]; then
-        echoerr "Function element_to_device_id() requires two arguments; 1. element_id 2. name of output variable."
+    # arguments check
+    if [ $# -lt 1 ]; then
+        echoerr "element_to_device_id(): argument required."
         return 2
     fi
-    if [ -z "$1" -o -z "$2" ]; then
-        echoerr "One or both of the arguments to element_to_device_id() is empty."
+    if [ -z "$1" -o "$1" == "0" ]; then
+        echoerr "element_to_device_id(): argument empty or '0' which is not a valid asset element identifier."
         return 2
     fi
-    if [ "$1" == "0" ]; then
-        echoerr "First argument to element_to_device_id() is '0' which is not valid asset element identifier."
-        return 2
-    fi
-    local __element_id="$1"
-    local __resultvar="$2"
     
+    local __element_id="$1"
     local __result=$(mysql -s -u root -D box_utf8 -N -e "    
         SELECT a.id_discovered_device
         FROM
@@ -58,30 +51,26 @@ element_to_device_id () {
         WHERE
             id_asset_element = '${__element_id}'
         ")
+    if [ $? -ne 0 ]; then
+        return 2
+    fi
     if [ -z "$__result" ]; then
         return 1
     else
-        eval $__resultvar="'$__result'" #Note: This is evil, i know...
+        echo "$__result"
         return 0
     fi    
 }
 
 # Retrieve asset element id's for datacenter, rack, ups. Output parameter contains whitespace delimited id's.
 #   
-# Returns 1 on error
+# Returns:
+#   2 - error
+#   1 - no asset element identifiers of requested type found
+#   0 - success
 # Arguments:
-# $1 - name of output parameter
+#   None
 get_elements () {
-    if [ $# != 1 ]; then
-        echoerr "Function get_elements() requires one argument; 1. name of output variable."
-        return 2
-    fi
-    if [ -z "$1" ]; then
-        echoerr "Arguments to is empty."
-        return 2
-    fi
-
-    local __resultvar="$1"
     local __result=$(mysql -s -u root -D box_utf8 -N -e "    
         SELECT id FROM v_web_element
         WHERE
@@ -113,127 +102,162 @@ get_elements () {
                 )
             )
             ")
+    if [ $? -ne 0 ]; then
+        return 2
+    fi
     if [ -z "$__result" ]; then
         return 1
     else
-        eval $__resultvar="'$__result'" #Note: This is evil, i know...
+        echo "$__result"
         return 0
     fi
 }
 
 # Retrieve sources of a specific device id. Output parameter contains whitespace delimited id's.
 #   
-# Returns 1 on error
+# Returns:
+#   2 - error
+#   1 - on error
+#   0 - success
 # Arguments:
-# $1 - device id
-# $2 - name of output parameter
-# TODO: rename some local variables to stay consistent with __var
+#   $1 - device identifier
 sources_from_device_id () {
-    if [ $# -ne 2 ]; then
-        echoerr "Function sources_from_device_id() requires two arguments; 1. device id 2. name of output variable."
+    # arguments check
+    if [ $# -lt 1 ]; then
+        echoerr "sources_from_device_id(): argument required."
         return 2
     fi
-    if [ -z "$1" -o -z "$2" ]; then
-        echoerr "One or both of the arguments to sources_from_device_id() is empty."
-        return 2
-    fi
-    if [ "$1" == "0" ]; then
-        echoerr "First argument to sources_from_device_id() is '0' which is not valid asset device identifier."
+    if [ -z "$1" -o "$1" == "0" ]; then
+        echoerr "sources_from_device_id(): argument empty or '0' which is not a valid device identifier."
         return 2
     fi
 
     local __device_id="$1"
-    local __resultvar="$2"
-    local __result=$(mysql -s -u root -D box_utf8 -N -e "    
+    local __mysql=$(mysql -s -u root -D box_utf8 -N -e "    
         SELECT topic
         FROM v_bios_measurement_topic
         WHERE device_id = '${__device_id}'
     ")
-
-    if [ -z "$__result" ]; then
+    if [ $? -ne 0 ]; then
+        return 2
+    fi
+    if [ -z "$__mysql" ]; then
         return 1
     fi
 
     # all arrays are local by default
     declare -a __sources
-    declare -a __tmp
 
-    read -a __tmp <<<$__result
-    local item=
-    local template=
-    for item in "${__tmp[@]}"; do
-        item=${item%%@*}
+    local __item
 
-        local sstep=
-        for sstep in ${STEP[@]}; do
-            template="_$sstep"
-            item=${item%%$template}
+    for __item in $__mysql; do
+        __item=${__item%%@*}
+
+        local __step
+        for __step in ${STEP[@]}; do
+            __item=${__item%%_$__step}
         done
 
-        local stype=
-        for stype in ${TYPE[@]}; do
-            template="_$stype"
-            item=${item%%$template}
+        local __type
+        for __type in ${TYPE[@]}; do
+            __item=${__item%%_$__type}
         done
 
-        local already_inserted=0
-        local i=
-        for i in "${__sources[@]}"; do
-            if [ "$item" == "$i" ]; then
-                already_inserted=1
+        local __already_inserted=0
+        local __i=
+        for __i in "${__sources[@]}"; do
+            if [ "$__item" == "$__i" ]; then
+                __already_inserted=1
+                break
             fi
         done
-        if [ $already_inserted -eq 0 ]; then
-          __sources+=($item)
+        if [ $__already_inserted -eq 0 ]; then
+          __sources+=($__item)
         fi
     done
 
-    __result=$(join ' ' ${__sources[@]})
-    eval $__resultvar="'$__result'" #Note: This is evil, i know...
+    local __result=$(join ' ' ${__sources[@]})
+    echo "$__result"
     return 0
 }
 
-get_elements element_ids
-if [ $? -eq 1 ]; then
-    # No element id's for DC, rack, ups
+### script starts here ###
+
+# Previous cron_average.sh should execute successfully
+[ -f $LOCKFILE ] && exit 0
+
+trap "{ rm -f $LOCKFILE ; exit 255; }" EXIT # TODO add
+touch $LOCKFILE
+
+end_timestamp=$(date +%Y%m%d%H%M%S)
+declare -r END_TIMESTAMP="${end_timestamp}Z"
+
+if [ -f "$TIMEFILE" ] && [ -s "$TIMEFILE" ]; then
+    read -r firstline < "$TIMEFILE"
+    if [ -z "$firstline" ]; then
+        start_timestamp="19700101000000Z"
+    fi
+    # TODO: check format of the read line
+    start_timestamp="$firstline"
+else
+    start_timestamp="19700101000000Z"
+fi
+
+declare -r START_TIMESTAMP="$start_timestamp"
+
+# TODO: remove
+#echo "START_TIMESTAMP=$START_TIMESTAMP"
+#echo "END_TIMESTAMP=$END_TIMESTAMP"
+
+# 1. Get asset element identifiers
+element_ids=$(get_elements)
+if [ $? -eq 1 ]; then   # no element ids for DC, rack, ups
     exit 0
-elif [ $? -gt 1 ]; then
+elif [ $? -gt 1 ]; then # error
     exit 1
 fi
-read -a ELEMENTS <<<${element_ids}
+#read -a ELEMENTS <<<${element_ids}
 
-#TODO: better variable names
-for i in "${ELEMENTS[@]}"; do
-    echo "-> $i"
-    device_id=
-    element_to_device_id $i device_id 
-    if [ $? -eq 1 ]; then
-        # element_to_device_id() not found for this one
+# 2. Go over them and ...
+for i in $element_ids; do
+
+    # TODO: Remove the following block
+    #echo "-> $i"
+
+
+    # 3. Try to get corresponding device id from given element id
+    device_id=$(element_to_device_id $i)    
+    if [ $? -eq 1 ]; then   # device id not found for this element id
         continue
-    elif [ $? -gt 1 ]; then
+    elif [ $? -gt 1 ]; then # error
         exit 1
     fi
-    echo "$i"
-    echo "device id: '$device_id'"
-    echo -e "\tsources:"
-    sources=    
-#set -x
-    sources_from_device_id $device_id sources
-#set +x
-    if [ $? -eq 1 ]; then
+
+    # TODO: Remove the following block
+    #echo "$i"
+    #echo "device id: '$device_id'"
+    #echo -e "\tsources:"
+
+    # 4. Try to get distinct sources from all the topics of a given device id
+    sources=$(sources_from_device_id $device_id)
+    if [ $? -eq 1 ]; then   # no sources/topics exist for given device id
         continue
-    elif [ $? -gt 1 ]; then
+    elif [ $? -gt 1 ]; then # error
         exit 1
     fi
     read -a SOURCES <<<${sources}
+    # 5. Generate curl request for each combination of source, step, type for the given element id
     for s in ${SOURCES[@]}; do
         for stype in ${TYPE[@]}; do
             for sstep in ${STEP[@]}; do
+                # TODO: change this to a command instead of echo
                 echo "curl 'http://${SERVER}:${PORT}/api/v1/metric/computed/average?start_ts=${START_TIMESTAMP}&end_ts=${END_TIMESTAMP}&type=${stype}&step=${sstep}&element_id=${i}&source=$s'"
+                # TODO: Does it make sense to check for 404/500? What can we do?
             done
         done
     done
-
 done
 
+echo "$END_TIMESTAMP" > "$TIMEFILE"
 
+exit 0
