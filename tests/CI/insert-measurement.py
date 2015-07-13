3910d10af3f4f4e0b1c2acbb9d8eadd3d0900a02
Tomas Halman <TomasHalman@eaton.com>
2015-07-13 14:25:27 +0200

    better lace, license
    Signed-off-by: Tomas Halman <TomasHalman@eaton.com>

--- tests/CI/insert-measurement.py
+++ tests/CI/insert-measurement.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python
 
-#
+# Copyright (C) 2014 Eaton
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # Author: THA
-# generate random measurement data
+# inserting measurement from command line
+# automatically creates topic item, discovered device item
+# and link in t_bios_monitor_asset_relation
 #
 
 import MySQLdb as db
