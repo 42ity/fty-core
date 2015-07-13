8039b90f33ab57455b0e4d6f5a0e7b6367fc26df
Jim Klimov <jim@jimklimov.com>
2015-07-13 11:58:43 +0200

    Unified agent naming pattern: renamed agent-autoconfig.cc and .h
    Signed-off-by: Jim Klimov <jim@jimklimov.com>

--- src/agents/autoconfig/agent-autoconfig.h
+++ src/agents/autoconfig/agent-autoconfig.h
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-/*! \file autoconfig.h
+/*! \file agent-autoconfig.h
     \brief autoconfiguration agent
     \author Tomas Halman <tomashalman@eaton.com>
 */
