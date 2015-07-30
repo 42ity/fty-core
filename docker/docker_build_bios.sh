31e5651dc60b5b0cd41dd8f7dba04c586b6b45e6
Gerald Guillaume <geraldguillaume@eaton.com>
2015-07-30 17:36:17 +0200

    add augeas support and declare two dummy eth interfaces(eth1,eth2)
    Signed-off-by: Gerald Guillaume <geraldguillaume@eaton.com>

--- docker/docker_build_bios.sh
+++ docker/docker_build_bios.sh
@@ -19,21 +19,20 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 #   Description: Script to generate eaton/bios container
-export build_dir=../build/
+export build_dir=..
 export temp_dir=./temp 
 
 echo "preparing $temp_dir .. "
 mkdir -p $temp_dir
+export DESTDIR=`cd $temp_dir; pwd`
 CURDIR=`pwd`
 cd $build_dir
 make install
+
 cd $CURDIR
 cp ./resources/Dockerfile $temp_dir
-cp /usr/share/compiled-webapp.tar $temp_dir/usr/share
-
-#in case of network timeout between mbt and roz, force the way for getting libcidr0 and cidrcalc packages like this
-#cp ../../temp/cidrcalc_1.2.3-1_amd64.deb $temp_dir
-#cp ../../temp/libcidr0_1.2.3-1_amd64.deb $temp_dir
+mkdir $temp_dir/usr/share
+cp ~/compiled-webapp.tar $temp_dir/usr/share
 
 echo "building docker eaton/bios .."
 sudo docker build --no-cache=true --rm=true -t eaton/bios $temp_dir
