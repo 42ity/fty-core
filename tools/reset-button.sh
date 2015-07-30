5b1ca19cab1d1be932e03369c11f53b9877c5afd
Michal Hrusecky <MichalHrusecky@Eaton.com>
2015-07-30 09:39:50 +0000

    minor polishing
    Signed-off-by: Michal Hrusecky <Michal@Hrusecky.net>

--- tools/reset-button.sh
+++ tools/reset-button.sh
@@ -9,27 +9,32 @@
 RESET_DOWN="AQBUAAEAAAA="
 RESET_UP="AQBUAAAAAAA="
 
-RESET_TIME=5
+log() {
+    echo "$1"
+    logger -p daemon.notice "$1"
+}
+
+[ -n "$LONG_PRESS_TIME" ] || LONG_PRESS_TIME=5
 
 while true; do
     key="`head -c 16 /dev/input/event0 | tail -c 8 | base64`"
-    case $key in
-    $RESET_DOWN)
+    case "$key" in
+    "$RESET_DOWN")
         echo "Asked for factory reset"
-        RESET_ST="`date +%s`"
+        RESET_ST="`date -u +%s`"
         ;;
-    $RESET_UP)
+    "$RESET_UP")
         echo "Checking time interval"
-        RESET_END="`date +%s`"
+        RESET_END="`date -u +%s`"
         [ -n "$RESET_ST" ] || continue
         RESET_REAL_TIME="`expr $RESET_END - $RESET_ST`"
-        if [ 0$RESET_REAL_TIME -gt $RESET_TIME ]; then
-            echo "Doing factory reset"
+        if [ $RESET_REAL_TIME -gt $LONG_PRESS_TIME ]; then
+            log "Long reset button press - doing factory reset"
             touch /mnt/nand/factory_reset
         else
-            echo "Doing normal reset"
+            log "Short reset button press - doing normal reset"
         fi
-	reboot
+        reboot
         RESET_ST=""
         ;;
     *)
