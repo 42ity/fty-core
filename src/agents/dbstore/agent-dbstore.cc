92b61894f421cef35aaccc96d1c200e734e7b72e
Alena Chernikava <alenachernikava@eaton.com>
2015-07-31 13:47:14 +0200

    dbstore and inventory messages
    Problem:
        dbstore receives the inventory data and trats them as measurement
    Solution:
        temporary: ignore them.
    
    Signed-off-by: Alena Chernikava <alenachernikava@eaton.com>

--- src/agents/dbstore/agent-dbstore.cc
+++ src/agents/dbstore/agent-dbstore.cc
@@ -87,6 +87,9 @@ int main (int argc, char *argv []) {
         else if (streq (command, "STREAM DELIVER")) {
 
             const char *stream = bios_agent_address(client);
+            if (strncmp(bios_agent_subject(client), "inventory.", 10) == 0 ) {
+                log_debug ("inventory message recieved, ingore it. In future this should never happen");
+            }
             if (strncmp(bios_agent_subject(client), "alert.", 6) == 0 ) {
                 ymsg_t* out = NULL;
                 char* out_subj = NULL;
