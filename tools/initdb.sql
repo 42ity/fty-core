23dd4bf63b347698f87393f9f184c18593164841
Alena Chernikava <alenachernikava@eaton.com>
2015-07-31 17:01:33 +0200

    some tests fixed. Power topology not yet
    Signed-off-by: Alena Chernikava <alenachernikava@eaton.com>

--- tools/initdb.sql
+++ tools/initdb.sql
@@ -505,7 +505,8 @@ SELECT v1.id_asset_element,
        v4.id_parent AS id_parent4,
        v1.status, 
        v1.priority, 
-       v1.business_crit
+       v1.business_crit,
+       v1.id_type
 FROM t_bios_asset_element v1 
      LEFT JOIN t_bios_asset_element v2 
         ON (v1.id_parent = v2.id_asset_element) 
