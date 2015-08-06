ea8d99fe06397bfb64af6f6c01eee7f23ab3ada6
Michal Vyskocil <michalvyskocil@eaton.com>
2015-08-06 08:39:12 +0200

    Problem: need to merge the code
    Solution: fix some imports
    
    Signed-off-by: Michal Vyskocil <michalvyskocil@eaton.com>

--- src/db/assets/assetr.h
+++ src/db/assets/assetr.h
@@ -1,16 +1,16 @@
 /*
 Copyright (C) 2014-2015 Eaton
- 
+
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 3 of the License, or
 (at your option) any later version.
- 
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
- 
+
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -18,12 +18,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /*! \file   assetr.h
     \brief  Basic select-functions for assets
     \author Alena Chernikava <alenachernikava@eaton.com>
+    \author Michal Vyskocil <michalvyskocil@eaton.com>
 */
 #ifndef SRC_DB_ASSETS_ASSETR_H
 #define SRC_DB_ASSETS_ASSETR_H
 
 #include <functional>
 #include <vector>
+#include <map>
 
 #include <tntdb/connect.h>
 #include "dbtypes.h"
--- src/db/inout/exportcsv.cc
+++ src/db/inout/exportcsv.cc
@@ -1,16 +1,16 @@
 /*
 Copyright (C) 2014-2015 Eaton
- 
+
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 3 of the License, or
 (at your option) any later version.
- 
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
- 
+
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -22,9 +22,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <functional>
 
-#include "cxxtools/csvserializer.h"
-#include "tntdb/row.h"
+#include <cxxtools/csvserializer.h>
+#include <tntdb/row.h>
 
 #include "db/assets.h"
 #include "dbpath.h"
