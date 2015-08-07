3bb8493ac80b14d1bcbadee05c84c2549e47cc5c
Jim Klimov <jim@jimklimov.com>
2015-08-08 01:33:29 +0200

    BIOS-793: udhcpc-ntp.sh : adapted closer to udhcpc expectations
    Signed-off-by: Jim Klimov <jim@jimklimov.com>

--- tools/udhcpc-ntp.sh
+++ tools/udhcpc-ntp.sh
@@ -1,5 +1,6 @@
 # Based on /etc/dhcp/dhclient-exit-hooks.d/ntp from Debian8 dhclient package
 # Included from (patched) /etc/udhcpc/default.script for $BIOS project
+# Adapted by Jim Klimov <EvgenyKlimov@eaton.com>, Copyright (C) 2015, Eaton
 
 NTP_CONF=/etc/ntp.conf
 NTP_DHCP_CONF=/var/lib/ntp/ntp.conf.dhcp
@@ -11,10 +12,10 @@ ntp_server_restart() {
 
 
 ntp_servers_setup_remove() {
-	if [ ! -e $NTP_DHCP_CONF ]; then
+	if [ ! -e "$NTP_DHCP_CONF" ]; then
 		return
 	fi
-	rm -f $NTP_DHCP_CONF
+	rm -f "$NTP_DHCP_CONF"
 	ntp_server_restart
 }
 
@@ -30,8 +31,8 @@ ntp_servers_setup_add() {
 	fi
 
 	tmp=$(mktemp "$NTP_DHCP_CONF.XXXXXX") || return
-	chmod --reference=$NTP_CONF $tmp
-	chown --reference=$NTP_CONF $tmp
+	chmod --reference="$NTP_CONF" "$tmp"
+	chown --reference="$NTP_CONF" "$tmp"
 
 	(
 	  echo "# This file was copied from $NTP_CONF with the server options changed"
@@ -43,17 +44,17 @@ ntp_servers_setup_add() {
 		echo "server $server iburst"
 	  done
 	  echo
-	  sed -r -e '/^ *(server|peer).*$/d' $NTP_CONF
-	) >>$tmp
-	
-	mv $tmp $NTP_DHCP_CONF
+	  sed -r -e '/^ *(server|peer).*$/d' "$NTP_CONF"
+	) >>"$tmp"
+
+	mv "$tmp" "$NTP_DHCP_CONF"
 
 	ntp_server_restart
 }
 
 
 ntp_servers_setup() {
-	case $reason in
+	case "$reason" in
 		bound|renew|BOUND|RENEW|REBIND|REBOOT)
 			ntp_servers_setup_add
 			;;
@@ -64,4 +65,19 @@ ntp_servers_setup() {
 }
 
 [ -z "${reason-}" -a -n "$1" ] && reason="$1"
+[ -z "${new_ntp_servers-}" -a -n "${ntpsrv-}" ] && \
+        new_ntp_servers="`for S in $ntpsrv; do echo "$S"; done | sort | uniq`"
+if [ -z "${old_ntp_servers-}" ]; then
+        old_ntp_servers=""
+        if [ -s "$NTP_DHCP_CONF" ]; then
+                old_ntp_servers="`cat "$NTP_DHCP_CONF" | egrep '^[ \t]*(server|peer)' | awk '{print $2}' | sort | uniq`"
+        else
+                old_ntp_servers="`cat "$NTP_CONF" | egrep '^[ \t]*(server|peer)' | awk '{print $2}' | sort | uniq`"
+        fi
+fi
+
+#set >&2
+#set +x
+
 ntp_servers_setup
+
