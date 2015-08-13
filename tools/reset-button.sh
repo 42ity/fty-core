f00a747c04b334741114af665e81f672954e239a
Arnaud Quette <arnaud.quette@free.fr>
2015-08-13 13:40:38 +0200

    Add missing headers and complete others
    Signed-off-by: Arnaud Quette <arnaud.quette@free.fr>

--- tools/reset-button.sh
+++ tools/reset-button.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
-
-# Simple button handling script to provide reset and factory reset features.
-
-# Copyright (C) 2014-2015 by Eaton
-# Author: Michal Hrusecky <MichalHrusecky@eaton.com>
-
+#
+#   Copyright (c) 2014-2015 Eaton
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along
+#   with this program; if not, write to the Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+#! \file   reset-button.sh
+#  \brief  Simple button handling script to provide reset and factory reset features.
+#  \author Michal Hrusecky <MichalHrusecky@eaton.com>
 
 RESET_DOWN="AQBUAAEAAAA="
 RESET_UP="AQBUAAAAAAA="
