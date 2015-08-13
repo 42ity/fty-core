0eb6d289449ec06e20694ed14bf8abad6eaf9eb9
Arnaud Quette <arnaud.quette@free.fr>
2015-08-13 10:42:48 +0200

    Comply to the latest Licensing & Copyright Policies
    Fix legal headers to comply to the latest Licensing & Copyright
    Policies, including:
    - Fix Opensource headers, and switch to GPL v2+ only
    - Fix Doxygen headers, and apply only to headers that have direct
    implementation files (i.e x.h => x.h). Also align shell script to the
    source-code-like Doxygen format
    
    Reference: Jira #1046
    
    Signed-off-by: Arnaud Quette <arnaud.quette@free.fr>

--- tests/CI/weblib.sh
+++ tests/CI/weblib.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-
+#
 # Copyright (C) 2014 Eaton
 #
-# This program is free software: you can redistribute it and/or modify
+# This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
+# the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
@@ -12,16 +12,18 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
-# Author(s): Michal Hrusecky <MichalHrusecky@eaton.com>,
-#            Jim Klimov <EvgenyKlimov@eaton.com>
-#            Karol Hrdina <KarolHrdina@eaton.com>
-#
-# Description: This is library of functions usefull for REST API testing,
-#              which can be sourced to interactive shell
-#              You can 'export TESTWEB_QUICKFAIL=yes' to abort on first failure
+#! \file    weblib.sh
+#  \brief   library of functions usefull for REST API testing
+#  \author  Michal Hrusecky <MichalHrusecky@eaton.com>,
+#  \author  Jim Klimov <EvgenyKlimov@eaton.com>
+#  \author  Karol Hrdina <KarolHrdina@eaton.com>
+#  \details This is library of functions usefull for REST API testing,
+#           which can be sourced to interactive shell
+#           You can 'export TESTWEB_QUICKFAIL=yes' to abort on first failure
 
 # ***********************************************
 echo "INFO-WEBLIB: Initial  BASE_URL = '$BASE_URL'"
