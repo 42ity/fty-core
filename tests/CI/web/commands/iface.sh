a072994d21a67e339f5e4b5c77e51777209009b7
Karol Hrdina <karolhrdina@eaton.com>
2015-08-13 19:51:41 +0200

    iface test case revised
    Signed-off-by: Karol Hrdina <karolhrdina@eaton.com>

--- tests/CI/web/commands/iface.sh
+++ tests/CI/web/commands/iface.sh
@@ -1,12 +1,8 @@
 do_test_match "ifaces_get" api_get_json '/admin/ifaces' '.*ifaces.*lo.*'
 print_result $?
 
-test_it "iface_lo_get"
-api_get_json '/admin/iface/lo' >&5
-print_result $?
-
 test_it "iface_foobar_get"
-curlfail_push_expect_404
+curlfail_push_expect_400
 api_get_json '/admin/iface/foobar' >&5
 print_result $?
 curlfail_pop
