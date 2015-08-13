9d18b5fb6e7e5edf1d5eb9596d7ea94529b8ed7f
Arnaud Quette <arnaud.quette@free.fr>
2015-08-13 13:40:38 +0200

    Add missing headers and complete others
    Signed-off-by: Arnaud Quette <arnaud.quette@free.fr>

--- obs/preinstallimage-bios.sh
+++ obs/preinstallimage-bios.sh
@@ -1,26 +1,28 @@
 #!/bin/sh
-
-#   Copyright (c) 2014-2015 Eaton Corporation <www.eaton.com>
+#
+#   Copyright (c) 2014-2015 Eaton
 #
 #   This file is part of the Eaton $BIOS project.
 #
-#   This is free software; you can redistribute it and/or modify
+#   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation; either version 3 of the License, or
+#   the Free Software Foundation; either version 2 of the License, or
 #   (at your option) any later version.
 #
-#   This software is distributed in the hope that it will be useful,
+#   This program is distributed in the hope that it will be useful,
 #   but WITHOUT ANY WARRANTY; without even the implied warranty of
 #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #   GNU General Public License for more details.
 #
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#   You should have received a copy of the GNU General Public License along
+#   with this program; if not, write to the Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
-#   Author(s): Michal Hrusecky <MichalHrusecky@eaton.com>
-#              Jim Klimov <EvgenyKlimov@eaton.com>
-#
-#   Description: Script to generate the expected directory structure
+#! \file    restapi-request.sh
+#  \brief   Script to generate the expected directory structure and configuration files
+#  \author  Michal Hrusecky <MichalHrusecky@eaton.com>
+#  \author  Jim Klimov <EvgenyKlimov@eaton.com>
+#  \details Script to generate the expected directory structure
 #   and configuration files "baked into" the read-only OS images that
 #   are prepared by OBS for dev/test X86 containers as well as the
 #   ultimate RC3 environments. Any changes to the files "hardcoded"
--- tests/CI/cmpjson.sh
+++ tests/CI/cmpjson.sh
@@ -1,11 +1,30 @@
 #!/usr/bin/env bash
-
-# Compare REST API call outputs with expected ones - it assumes
-# one normalized JSON document per line to support more results
-# in one file! That is, file1:lineN is compared to file2:lineN.
-# NOTE: Most of the logic is in separate tools/JSON.sh script!
+#
+#   Copyright (c) 2014 Eaton
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along
+#   with this program; if not, write to the Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+#! \file    cmpjson.sh
+#  \brief   Compare REST API call outputs with expected ones
+#  \author  Jim Klimov <EvgenyKlimov@eaton.com>
+#  \details Compare REST API call outputs with expected ones - it assumes
+#           one normalized JSON document per line to support more results
+#           in one file! That is, file1:lineN is compared to file2:lineN.
+#  \note    Most of the logic is in separate tools/JSON.sh script!
+#
 # Usage: bash cmpjson.sh file1 file2
-# Authors: Jim Klimov <EvgenyKlimov@eaton.com>
 
 # Include our standard routines for CI scripts
 . "`dirname $0`"/scriptlib.sh || \
