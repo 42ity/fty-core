860fa8e756529460b693791fb3c446e16ce604a4
Arnaud Quette <arnaud.quette@free.fr>
2015-08-13 11:45:06 +0200

    Fix typo error
    Signed-off-by: Arnaud Quette <arnaud.quette@free.fr>

--- tests/CI/ci-test-libbiosapi.sh
+++ tests/CI/ci-test-libbiosapi.sh
@@ -17,7 +17,7 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 #! \file   ci-test-libbiosapi.sh
-#  \brief  ests build and run against libbiosapi.so
+#  \brief  Tests build and run against libbiosapi.so
 #  \author Michal Vyskocil <MichalVyskocil@eaton.com>
 #
 # Requirements:
