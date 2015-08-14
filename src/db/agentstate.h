b766cf6658e01f5518a2a68f0eb7f0ec229af5b0
Alena Chernikava <alenachernikava@eaton.com>
2015-08-14 10:51:01 +0200

    forgotten files
    Signed-off-by: Alena Chernikava <alenachernikava@eaton.com>

---
+++ src/db/agentstate.h
@@ -0,0 +1,29 @@
+/*
+Copyright (C) 2014-2015 Eaton
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*! \file  agentstate.h
+    \brief API for storing agent state as binary entity
+
+    \author Alena Chernikava <AlenaChernikava@eaton.com>
+*/
+
+#ifndef SRC_DB_AGENTSTATE_H_
+#define SRC_DB_AGENTSTATE_H_
+
+#include "db/agentstate/agentstate.h"
+
+#endif // SRC_DB_AGENTSTATE_H_
---
+++ src/db/agentstate/agentstate.cc
@@ -0,0 +1,113 @@
+/*
+Copyright (C) 2014-2015 Eaton
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*! \file  agentstate.cc
+    \brief API for storing agent state as binary entity
+
+    \author Alena Chernikava <AlenaChernikava@eaton.com>
+*/
+
+#include "agentstate.h"
+
+#include <tntdb/row.h>
+#include <tntdb/error.h>
+
+#include "log.h"
+#include "utils.h"
+
+
+int
+    update_agent_info(
+        tntdb::Connection &conn,
+        const std::string &agent_name,
+        const char        *data,
+        int                size,
+        uint16_t          &affected_rows
+        )
+{
+    LOG_START;
+
+    try{
+        tntdb::Statement st = conn.prepareCached(
+            " INSERT INTO t_bios_agent_info (agent_name, info) "
+            " VALUES "
+            "   (:name, :info) "
+            " ON DUPLICATE KEY "
+            "   UPDATE "
+            "       info = :info "
+        );
+        tntdb::Blob blobData(data, size);
+
+        affected_rows = st.set("name", agent_name).
+                           setBlob("info", blobData).
+                           execute();
+        log_debug ("[t_bios_agent_info]: %" PRIu16 " rows ", affected_rows);
+        LOG_END;
+        return 0;
+    }
+    catch (const std::exception &e) {
+        LOG_END_ABNORMAL(e);
+        return 1;
+    }
+}
+
+int
+    select_agent_info(
+        tntdb::Connection &conn,
+        const std::string &agent_name,
+        char             **data,
+        int               &size
+        )
+{
+    LOG_START;
+
+    if ( data == NULL )
+        return 5;
+    try{
+        tntdb::Statement st = conn.prepareCached(
+            " SELECT "
+            "   v.info "
+            " FROM "
+            "   v_bios_agent_info v "
+            " WHERE "
+            "   v.agent_name = :name "
+        );
+
+        tntdb::Row row = st.set("name", agent_name).
+                            selectRow();
+
+        tntdb::Blob myBlob;
+        row[0].get(myBlob);
+
+        size = myBlob.size();
+        *data = new char[size];
+        memcpy(*data, myBlob.data(), size);
+        //*data = strdup ( myBlob.data());
+        log_debug ("  data = %s", *data);
+        log_debug ("  size = %i", size);
+        LOG_END;
+        return 0;
+    }
+    catch (const tntdb::NotFound &e) {
+        log_debug ("end: nothing was found");
+        return 0;
+    }
+    catch (const std::exception &e) {
+        LOG_END_ABNORMAL(e);
+        return 1;
+    }
+}
