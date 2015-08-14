c95fe5a121f920aa9cd9d65b80a1f2d14e9c09d1
Arnaud Quette <arnaud.quette@free.fr>
2015-08-14 14:34:24 +0200

    Complementary fixes for PR#900
    Fix remaining headers for license and copyright compliance
    
    Signed-off-by: Arnaud Quette <arnaud.quette@free.fr>

--- tools/udhcpc-ntp.sh
+++ tools/udhcpc-ntp.sh
@@ -1,6 +1,28 @@
-# Based on /etc/dhcp/dhclient-exit-hooks.d/ntp from Debian8 dhclient package
-# Included from (patched) /etc/udhcpc/default.script for $BIOS project
-# Adapted by Jim Klimov <EvgenyKlimov@eaton.com>, Copyright (C) 2015, Eaton
+#!/bin/bash
+#
+# Copyright (c) 2010 Debian
+# Copyright (c) 2015 Eaton
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+#! \file    udhcpc-ntp.sh
+#  \brief   NTP synchronization for udhcpc
+#  \author  Jim Klimov <EvgenyKlimov@Eaton.com>
+#  \details Based on /etc/dhcp/dhclient-exit-hooks.d/ntp from Debian8 ntp
+#           package, adapted for the $BIOS project.
+#           Included from (patched) /etc/udhcpc/default.script for $BIOS project
 
 NTP_CONF=/etc/ntp.conf
 NTP_DHCP_CONF=/var/lib/ntp/ntp.conf.dhcp
