85ad4f7b5fbc885c1cf0772740e6ea81b4941b93
Michal Hrusecky <MichalHrusecky@Eaton.com>
2015-08-20 13:19:30 +0200

    Rewritten to include Doxygen and use same licence everywhere
    Signed-off-by: Michal Hrusecky <Michal@Hrusecky.net>

--- tools/license_add.sh
+++ tools/license_add.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-BASEDIR="$1"
-[ -n "$BASEDIR" ] || BASEDIR="`pwd`"
-FILE="`mktemp -t bios_license.XXXXXX`"
-
-find "$BASEDIR" -name '*.sh' -o -name '*.sh.in' | while read fl; do
-    if [ -z "`grep 'Copyright ' "$fl"`" ]; then
-        sed '1 p' "$fl" | grep '^#!' > "$FILE"
-        cat - "$fl" >> "$FILE" << EOF
-
 #
 # Copyright (C) 2015 Eaton
 #
@@ -27,60 +18,88 @@ find "$BASEDIR" -name '*.sh' -o -name '*.sh.in' | while read fl; do
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 
+#!
+# \file license_add.sh
+# \author Michal Hrusecky
+# \brief Not yet documented file
+
+BASEDIR="$1"
+[ -n "$BASEDIR" ] || BASEDIR="`pwd`"
+FILE="`mktemp -t bios_license.XXXXXX`"
+
+add_license() {
+    SH=""
+    SD=""
+    ED=""
+    MD=""
+    SDD=""
+    EDD=""
+    MDD=""
+    case "$1" in
+        *.sh|*.sh.in)
+            MD="#"
+            SH="true"
+            SDD="#!"
+            MDD="#"
+            ;;
+        *.cc|*.h|*.c)
+            SD="/*"
+            MD=" *"
+            ED=" */"
+            SDD="/*!"
+            MDD=" *"
+            EDD=" */"
+            ;;
+        *.ecpp)
+            SD="<#"
+            MD=" #"
+            ED=" #>"
+            SDD="<#\n/*!"
+            MDD=" *"
+            EDD=" */\n#>"
+            ;;
+        *)
+            echo "Skipping invalid file $1"
+            continue;
+            ;;
+    esac
+    if [ -z "`grep "Copyright " "$1"`" ]; then
+        echo -n '' > "$FILE"
+        [ -z "$SH" ] || sed '1 p' "$fl" | grep '^#!' > "$FILE"
+        cat - "$fl" >> "$FILE" << EOF
+$SD
+$MD
+$MD Copyright (C) `date +%Y` Eaton
+$MD
+$MD This program is free software; you can redistribute it and/or modify
+$MD it under the terms of the GNU General Public License as published by
+$MD the Free Software Foundation; either version 2 of the License, or
+$MD (at your option) any later version.
+$MD
+$MD This program is distributed in the hope that it will be useful,
+$MD but WITHOUT ANY WARRANTY; without even the implied warranty of
+$MD MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+$MD GNU General Public License for more details.
+$MD
+$MD You should have received a copy of the GNU General Public License along
+$MD with this program; if not, write to the Free Software Foundation, Inc.,
+$MD 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+$MD
+$ED
+
+$SDD
+$MDD \\file `basename $1`
+`git blame -c "$1" | sed 's|.*(\([^\t]*\)\t.*|\1|' | sort | uniq -c | sort -nr | sed "s|^[[:blank:]]*[0-9]\\+[[:blank:]]\\(.*\\)|$MDD\\ \\\\\\\\author\\ \\1|"`
+$MDD \\brief Not yet documented file
+$EDD
 EOF
         cat "$FILE" > "$fl"
     fi
-done
-find "$BASEDIR" -name '*.c' -o -name '*.cc' -o -name '*.h' | while read fl; do
-    if [ -z "`grep 'Copyright ' "$fl"`" ]; then
-        cat - "$fl" > "$FILE" << EOF
-/*
+}
 
-Copyright (C) 2015 Eaton
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-*/
-EOF
-        cat "$FILE" > "$fl"
-    fi
-done
-find "$BASEDIR" -name '*.ecpp' | while read fl; do
-    if [ -z "`grep 'Copyright ' "$fl"`" ]; then
-        cat - "$fl" > "$FILE" << EOF
-<#
-
-Copyright (C) 2015 Eaton
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-#>
-EOF
-        cat "$FILE" > "$fl"
-    fi
+find "$BASEDIR" -name '*.sh' -o -name '*.sh.in' -o \
+    -name '*.cc' -o -name '*.h' -o -name '*.c' -o -name '*.ecpp' \
+| while read fl; do
+    add_license "$fl"
 done
 rm "$FILE"
