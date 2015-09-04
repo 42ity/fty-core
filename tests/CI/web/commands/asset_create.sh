57ab23117e3f00ed14508c798a4e83872930632e
Michal Vyskocil <michalvyskocil@eaton.com>
2015-09-04 13:44:36 +0200

    Problem: asset_create test is too verbose
    Solution: /dev/null
    
    Signed-off-by: Michal Vyskocil <michalvyskocil@eaton.com>

--- tests/CI/web/commands/asset_create.sh
+++ tests/CI/web/commands/asset_create.sh
@@ -21,31 +21,31 @@
 
 curlfail_push_expect_400
 test_it "asset_no_content"
-api_auth_post /asset ''
+api_auth_post /asset '' >/dev/null
 print_result $?
 curlfail_pop
 
 curlfail_push_expect_400
 test_it "asset_no_content2"
-api_auth_post /asset '{}'
+api_auth_post /asset '{}' >/dev/null
 print_result $?
 curlfail_pop
 
 test_it "asset_create"
-api_auth_post /asset '{"name":"dc_name_test","type":"datacenter","sub_type":"","location":"","status":"active","business_critical":"yes","priority":"P1","ext":{"asset_tag":"A123B123","address":"ASDF"}}'
+api_auth_post /asset '{"name":"dc_name_test","type":"datacenter","sub_type":"","location":"","status":"active","business_critical":"yes","priority":"P1","ext":{"asset_tag":"A123B123","address":"ASDF"}}' >/dev/null
 PARSED_REPLY=`echo ${OUT_CURL} | $JSONSH -x id`
 #test the reply
 echo "${PARSED_REPLY}" | cut -d '"' -f 4 | grep -q -E '^[0-9]+$'
 print_result $?
 
 curlfail_push_expect_400
-test_it "asset_create_dup"
-api_auth_post /asset '{"name":"dc_name_test","type":"datacenter","sub_type":"","location":"","status":"active","business_critical":"yes","priority":"P1","ext":{"asset_tag":"A123B123","address":"ASDF"}}'
+test_it "asset_create_dup" >/dev/null
+api_auth_post /asset '{"name":"dc_name_test","type":"datacenter","sub_type":"","location":"","status":"active","business_critical":"yes","priority":"P1","ext":{"asset_tag":"A123B123","address":"ASDF"}}' >/dev/null
 print_result $?
 curlfail_pop
 
 # remove the asset - it'll fail if previous attempt will fail too
 test_it "asset_delete_previously_inserted"
 ID=`echo "${PARSED_REPLY}" | cut -d '"' -f 4`
-api_auth_delete /asset/${ID}
+api_auth_delete /asset/${ID} >/dev/null
 print_result $?
