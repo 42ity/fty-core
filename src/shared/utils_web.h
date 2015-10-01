bed9c8d52d2ef63c775cb92dad3c899c2df42501
Alena Chernikava <alenachernikava@eaton.com>
2015-10-01 13:32:11 +0200

    one file looks more pretty
    Signed-off-by: Alena Chernikava <alenachernikava@eaton.com>

--- src/shared/utils_web.h
+++ src/shared/utils_web.h
@@ -63,7 +63,7 @@ static constexpr const _WSErrors _errors = { {
     {.key = "bad-request-document",     .http_code = HTTP_BAD_REQUEST,              .err_code = 48,     .message = "Request document has invalid syntax. %s" },
     {.key = "data-conflict",            .http_code = HTTP_CONFLICT,                 .err_code = 50,     .message = "Element '%s' cannot be processed because of conflict. %s"},
     {.key = "action-forbidden",         .http_code = HTTP_FORBIDDEN,                .err_code = 51,     .message = "%s is forbidden. %s"},
-    {.key = "parameter-conflict",       .http_code = HTTP_BAD_REQUEST,                 .err_code = 52,     .message = "Request cannot be processed because of conflict in parameters. %s"}
+    {.key = "parameter-conflict",       .http_code = HTTP_BAD_REQUEST,              .err_code = 52,     .message = "Request cannot be processed because of conflict in parameters. %s"}
     } };
 
 template <size_t N>
