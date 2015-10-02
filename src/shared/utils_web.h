e668cb48aecc7047daf95f1b16072206432de27c
Karol Hrdina <karolhrdina@eaton.com>
2015-10-02 13:52:32 +0200

    BIOS-1279
    Signed-off-by: Karol Hrdina <karolhrdina@eaton.com>

--- src/shared/utils_web.h
+++ src/shared/utils_web.h
@@ -48,7 +48,7 @@ typedef struct _wserror {
 } _WSError;
 
 // size of _errors array, keep this up to date otherwise code won't build
-static constexpr size_t _WSErrorsCOUNT = 11;
+static constexpr size_t _WSErrorsCOUNT = 12;
 
 // typedef for array of errors
 typedef std::array<_WSError, _WSErrorsCOUNT> _WSErrors;
@@ -70,7 +70,8 @@ static constexpr const _WSErrors _errors = { {
     {.key = "bad-request-document",     .http_code = HTTP_BAD_REQUEST,              .err_code = 48,     .message = "Request document has invalid syntax. %s" },
     {.key = "data-conflict",            .http_code = HTTP_CONFLICT,                 .err_code = 50,     .message = "Element '%s' cannot be processed because of conflict. %s"},
     {.key = "action-forbidden",         .http_code = HTTP_FORBIDDEN,                .err_code = 51,     .message = "%s is forbidden. %s"},
-    {.key = "parameter-conflict",       .http_code = HTTP_BAD_REQUEST,              .err_code = 52,     .message = "Request cannot be processed because of conflict in parameters. %s"}
+    {.key = "parameter-conflict",       .http_code = HTTP_BAD_REQUEST,              .err_code = 52,     .message = "Request cannot be processed because of conflict in parameters. %s"},
+    {.key = "start-gt-end-timestamp",   .http_code = HTTP_BAD_REQUEST,              .err_code = 101,    .message = "Start timestamp '%s' is greater than '%s' end timestamp."}
     } };
 
 template <size_t N>
