9f81d59b7beb0c225d3aefd8518314628b66a749
Radomir Vrajik <RadomirVrajik@eaton.com>
2015-10-09 15:38:58 +0200

    cos
    Signed-off-by: Radomir Vrajik <RadomirVrajik@eaton.com>

--- tools/initdb_ci_patch_2.sql
+++ tools/initdb_ci_patch_2.sql
@@ -1,2 +1,2 @@
 use box_utf8
-alter table t_bios_asset_element modify column asset_tag CHAR(10) NOT NULL DEFAULT "1234567890";
+alter table t_bios_asset_element add UNIQUE INDEX `UI_t_bios_asset_element_ASSET_TAG` (`asset_tag`  ASC);
