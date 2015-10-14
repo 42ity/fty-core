fd761631b31f077579ac7470f4980b5af7b304dc
Radomir Vrajik <RadomirVrajik@eaton.com>
2015-10-14 10:50:13 +0200

    asset_devices fix
    Signed-off-by: Radomir Vrajik <RadomirVrajik@eaton.com>

--- tests/CI/web/commands/asset_devices.sh
+++ tests/CI/web/commands/asset_devices.sh
@@ -21,8 +21,92 @@
 #! \file asset_devices.sh
 #  \author Michal Hrusecky <MichalHrusecky@Eaton.com>
 #  \author Jim Klimov <EvgenyKlimov@Eaton.com>
+#  \author Radomir Vrajik <RadomirVrajik@Eaton.com>
 #  \brief Not yet documented file
 
-test_it
+echo "********* 1. list_of_all_devices **********************************************************************"
+echo "***************************************************************************************************"
+test_it "list_of_all_devices"
 api_get_json /asset/devices >&5
+print_result $?
+
+echo "********* 2. list_of_devices_with_subtype_ups *****************************************************"
+echo "***************************************************************************************************"
+test_it "list_of_devices_with_subtype_ups"
+api_get_json /asset/devices?subtype="ups" >&5
+print_result $?
+
+echo "********* 3. list_of_devices_with_subtype_feed *****************************************************"
+echo "***************************************************************************************************"
+test_it "list_of_devices_with_subtype_feed"
+api_get_json /asset/devices?subtype="feed" >&5
+print_result $?
+
+echo "********* 4. list_of_devices_with_subtype_server **************************************************"
+echo "***************************************************************************************************"
+test_it "list_of_devices_with_subtype_server"
+api_get_json /asset/devices?subtype=server >&5
+print_result $?
+
+echo "********* 5. list_of_devices_with_subtype_epdu ****************************************************"
+echo "***************************************************************************************************"
+test_it "list_of_devices_with_subtype_epdu"
+api_get_json /asset/devices?subtype=epdu >&5
+print_result $?
+
+echo "********* 6. list_of_devices_with_wrong_subtype_upsx **********************************************"
+echo "***************************************************************************************************"
+test_it "list_of_devices_with_wrong_subtype_upsx"
+api_get_json /asset/devices?subtype=upsx >&5
+print_result $?
+
+echo "********* 7. list_of_devices_with_two_subtypes ****************************************************"
+echo "***************************************************************************************************"
+test_it "list_of_devices_with_two_subtypes"
+api_get_json /asset/devices?subtype="ups","feed" >&5
+print_result $?
+
+echo "********* 8. list_of_devices_with_empty_subtype ****************************************************"
+echo "***************************************************************************************************"
+test_it "8. list_of_devices_with_empty_subtype"
+api_get_json /asset/devices?subtype= >&5
+print_result $?
+
+echo "********* 9. list_of_devices_with_missing_subtype ****************************************************"
+echo "***************************************************************************************************"
+test_it "list_of_devices_with_missing_subtype"
+api_get_json /asset/devices?subtype= >&5
+print_result $?
+
+echo "********* 10. no_devices_present ***************************************************************"
+echo "***************************************************************************************************"
+# delete all assets, no DC are present
+DB_BASE="initdb.sql"
+DB_DATA="load_data.sql"
+DB_DATA_TESTREST="load_data_test_restapi.sql"
+DB_ASSET_TAG_NOT_UNIQUE="initdb_ci_patch.sql"
+DB_LOADDIR=$BUILDSUBDIR/tools
+loaddb_file "$DB_LOADDIR/$DB_BASE"
+
+test_it "no_devices_present"
+#curlfail_push_expect_404
+api_get_json /asset/devices >&5
+print_result $?
+#curlfail_pop
+
+#fill DB again
+DB_BASE="initdb.sql"
+DB_DATA="load_data.sql"
+DB_DATA_TESTREST="load_data_test_restapi.sql"
+DB_ASSET_TAG_NOT_UNIQUE="initdb_ci_patch.sql"
+DB_LOADDIR=$BUILDSUBDIR/tools
+
+for data in "$DB_BASE" "$DB_ASSET_TAG_NOT_UNIQUE" "$DB_DATA" "$DB_DATA_TESTREST"; do
+    loaddb_file "$DB_LOADDIR/$data" || return $?
+done
+
+echo "********* 11. list_of_devices_with_wrong_format ****************************************************"
+echo "***************************************************************************************************"
+test_it "list_of_devices_with_missing_subtype"
+api_get_json /asset/devices?subXtype=epdu >&5
 print_result $?
