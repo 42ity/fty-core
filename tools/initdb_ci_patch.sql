6eaa3860f0c03eda830b0815ff2633c1f29bec17
Alena Chernikava <alenachernikava@eaton.com>
2015-10-22 08:34:57 +0200

    Problem: ci_patch did an unsafe DRPO index Solution: drop index if exists
    Signed-off-by: Alena Chernikava <alenachernikava@eaton.com>

--- tools/initdb_ci_patch.sql
+++ tools/initdb_ci_patch.sql
@@ -1,4 +1,4 @@
 use box_utf8
 alter table t_bios_asset_element modify column asset_tag VARCHAR(50) DEFAULT "1234567890";
-drop index UI_t_bios_asset_element_ASSET_TAG on t_bios_asset_element;
+drop index if exists UI_t_bios_asset_element_ASSET_TAG on t_bios_asset_element;
 
