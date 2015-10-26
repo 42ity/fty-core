9627fcfa42a7c69b9d161de26c20a6fa1d3da690
Jim Klimov <jim@jimklimov.com>
2015-10-26 17:37:31 +0100

    testlib.sh : try to track mismatched test_it/print_result pairs
    Signed-off-by: Jim Klimov <jim@jimklimov.com>

--- tests/CI/testlib.sh
+++ tests/CI/testlib.sh
@@ -140,6 +140,10 @@ print_result() {
 }
 
 test_it() {
+    if [ x"${_testlib_result_printed}" = xnotyet ]; then
+        logmsg_warning "Starting a new test_it() while an old one was not followed by a print_result()!"
+        print_result 128
+    fi
     _testlib_result_printed=notyet
     [ -n "$TNAME" ] || TNAME="$NAME"
     if [ "$1" ]; then
