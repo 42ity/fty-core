16c8822667b94092ebf303a1f650c09ef873d80b
Michal Vyskocil <michalvyskocil@eaton.com>
2015-10-27 11:40:42 +0100

    Problem: no testable contracts
    Solution: add selftest
    
    Signed-off-by: Michal Vyskocil <michalvyskocil@eaton.com>

--- src/api/build.sh
+++ src/api/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc -lmlm -lczmq -lzmq proto.c -o proto
+gcc -ggdb -lmlm -lczmq -lzmq proto.c -o proto
--- src/api/proto.c
+++ src/api/proto.c
@@ -12,19 +12,19 @@ int metric_send (
         char *type,             // type of metric
         char *element_src,      // source element
         char *value,            // value of metric
-        char *units,            // units ('%', 'kWh', '', ...)
+        char *unit,             // unit ('%', 'kWh', '', ...)
         int64_t   time,         // (optional) unix time of measurement, -1 means current system time
         char *element_dest      // (optional) destionation element or NULL
         ) {
 
-    if (!cl || !type || !element_src || !value || !units) {
+    if (!cl || !type || !element_src || !value || !unit) {
         return -1;
     }
 
     char buf[21];
     memset (buf, '\0', 21);
     sprintf (buf, "%"PRIi64, time);
-    return mlm_client_sendx (cl, type, element_src, value, units, buf, element_dest, NULL);
+    return mlm_client_sendx (cl, "SUBJECT", type, element_src, value, unit, buf, element_dest, NULL);
 }
 
 int metric_encode (
@@ -32,19 +32,19 @@ int metric_encode (
         char **type,             // type of metric
         char **element_src,      // source element
         char **value,            // value of metric
-        char **units,            // units ('%', 'kWh', '', ...)
-        int64_t   *time,         // (optional) unix time of measurement, -1 means current system time
+        char **unit,            // unit ('%', 'kWh', '', ...)
+        int64_t   *tme,         // (optional) unix time of measurement, -1 means current system time
         char **element_dest      // (optional) destionation element or NULL
         ) {
 
     //TODO return -1??
-    zmsg_t *msg;
+    zmsg_t *msg = *msg_p;
 
     *type = zmsg_popstr(msg);
     *element_src = zmsg_popstr(msg);
     *value = zmsg_popstr(msg);
-    *units = zmsg_popstr(msg);
-    *time = atol(zmsg_popstr(msg));
+    *unit = zmsg_popstr(msg);
+    *tme = atol(zmsg_popstr(msg));
     *element_dest = zmsg_popstr(msg);
 
     zmsg_destroy (&msg);
@@ -53,6 +53,35 @@ int metric_encode (
 
 int main() {
 
-// there will be the test
+    static const char *endpoint = "inproc://@/malamute";
+
+    zactor_t *server = zactor_new (mlm_server, "Malamute");
+    zstr_sendx (server, "BIND", endpoint, NULL);
+
+    mlm_client_t *producer = mlm_client_new();
+    mlm_client_connect (producer, endpoint, 5000, "producer");
+    mlm_client_set_producer (producer, "ALERTS");
+
+    mlm_client_t *consumer = mlm_client_new();
+    mlm_client_connect (consumer, endpoint, 5000, "consumer");
+    mlm_client_set_consumer (consumer, "ALERTS", ".*");
+
+    // send
+    int r = metric_send (producer, "TYPE", "ELEMENT_SRC", "VALUE", "UNITS", -1, "ELEMENT_DEST");
+    assert (r == 0);
+
+    // recceive
+    zmsg_t *msg = mlm_client_recv (consumer);
+    assert (msg);
+
+    char *type, *element_src, *value, *unit, *element_dest;
+    int64_t tme;
+    r = metric_encode (&msg, &type, &element_src, &value, &unit, &tme, &element_dest);
+
+    assert (streq (type, "TYPE"));
+
+    mlm_client_destroy (&producer);
+    mlm_client_destroy (&consumer);
+    zactor_destroy (&server);
 
 }
