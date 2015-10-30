2cc38db4cfec5d7c866bd52ba385cd6bcded29d2
Alena Chernikava <alenachernikava@eaton.com>
2015-10-30 23:54:49 +0100

    Problem: sending alerts doesn't work Solution:     1. Update rfc ( add description/ remove out)     2. Update api     3. update selftest     4. _send functions should have const arguments     5. update rfc with "IS"/"ISNT" requirement ( need to have status RESOLVED)     6. implement "IS"/"ISNT" requirement     7. update rfc ( add severity/state)     8. Fix bug with uninitialized fields ( added Rule() constructor)     9. Fix bug with map.find()->second when was nothing found     10. restructure the code
    Signed-off-by: Alena Chernikava <alenachernikava@eaton.com>

--- libbiosproto/include/bios_proto.h
+++ libbiosproto/include/bios_proto.h
@@ -9,13 +9,13 @@ extern "C" {
 
 
 int metric_send (
-        mlm_client_t *cl,       // malamute client publish the metric, caller is responsible for correct initialization
-        char *type,             // type of metric
-        char *element_src,      // source element
-        char *value,            // value of metric
-        char *unit,             // unit ('%', 'kWh', '', ...)
-        int64_t  timestamp,     // (optional) unix time of measurement, -1 means current system time
-        char *element_dest      // (optional) destionation element or NULL
+        mlm_client_t *cl,        // malamute client publish the metric, caller is responsible for correct initialization
+        const char *type,        // type of metric
+        const char *element_src, // source element
+        const char *value,       // value of metric
+        const char *unit,        // unit ('%', 'kWh', '', ...)
+        int64_t  timestamp,      // (optional) unix time of measurement, -1 means current system time
+        const char *element_dest // (optional) destionation element or NULL
         );
 
 int metric_decode (
@@ -29,12 +29,13 @@ int metric_decode (
         );
 
 int alert_send (
-        mlm_client_t *cl,       // malamute client publish the metric, caller is responsible for correct initialization
-        char *rule_name,        // rule name that case alert evaluation
-        char *element_name,     // element where alert was evaluated
-        int64_t  timestamp,     // unix time when state was changed, -1 means current system time
-        char *state,            // state of the alert
-        char *severity          // severity of the alert
+        mlm_client_t *cl,         // malamute client publish the metric, caller is responsible for correct initialization
+        const char *rule_name,    // rule name that case alert evaluation
+        const char *element_name, // element where alert was evaluated
+        const int64_t  timestamp, // unix time when state was changed, -1 means current system time
+        const char *state,        // state of the alert
+        const char *severity,     // severity of the alert
+        const char *description   // description of the alert
         );
 
 int alert_decode (
@@ -43,7 +44,8 @@ int alert_decode (
         char **element_name,     // element where alert was evaluated
         int64_t  *timestamp,     // unix time when state was changed, -1 means current system time
         char **state,            // state of the alert
-        char **severity          // severity of the alert
+        char **severity,         // severity of the alert
+        char **description       // description of the alert
         );
 
 #ifdef __cplusplus
