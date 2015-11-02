41b0079da1e266fe335351ed1ad6af47f7be9b5e
Alena Chernikava <alenachernikava@eaton.com>
2015-11-02 15:52:14 +0100

    Problem: legacy stuff was found Solution: remove it
    Signed-off-by: Alena Chernikava <alenachernikava@eaton.com>

--- src/web/src/location_helpers.cc
+++ src/web/src/location_helpers.cc
@@ -25,45 +25,13 @@
  * \author Jim Klimov <EvgenyKlimov@Eaton.com>
  * \brief Not yet documented file
  */
-#include <cstdio>
-#include <cstring>
-#include <exception>
-#include <algorithm>
+#include "location_helpers.h"
+
 #include <list>
 #include <tnt/http.h>
 
 #include "asset_types.h"
-#include "asset_msg.h"
-#include "location_helpers.h"
 #include "cleanup.h"
-
-// Note: in the future, if more than one operation requires parsing these
-//       strings on input, we should consider moving this to a general
-//       helpers.cc file and rewrite these functions for a more general case
-//       e.g. namespace shared:: src/include/utils.h src/shared/utils.c and
-//       have asset_types in src/include.
-//       There is no need to wrap a class around simple functions.
-
-// TODO:case "api/v1/asset/5" (missing element type, i.e. room, datacenter...) not covered
-int element_id (const std::string& from, int& element_id) {
-    if (from.empty()) {
-        return -1;
-    }
-    if (from.find ("api/v1/asset/") != 0) {
-        return -1;
-    }
-    std::size_t last = from.find_last_of ("/");
-    std::string number = from.substr (last + 1, from.length() - last);
-    if (number.length () == 0) {
-        return -1;
-    }
-    try {
-        element_id = std::stoi (number);
-    } catch (std::exception& e) {
-        return -1;
-    }
-    return 0;
-}
 
 int asset_location_r(asset_msg_t** asset_msg, std::string& json) {
     int element_id = asset_msg_element_id(*asset_msg);
@@ -169,39 +137,3 @@ err_cleanup:
     json = "";
     return HTTP_INTERNAL_SERVER_ERROR;
 }
-
-int asset (const std::string& from) {
-    if (from.empty()) {
-        return -1;
-    }
-    if (from.find ("api/v1/asset/") != 0) {
-        return -1;
-    }
-    std::size_t pos = strlen ("api/v1/asset/");   
-    std::string cut = from.substr (pos, from.length () - pos);
-    if (cut.empty()) {
-        return -1;
-    }
-    pos = cut.find_first_of ("/");
-    if (pos == 0) {
-        return -1;
-    }
-    cut = cut.substr(0, pos);
-// TODO USE FUNCTIONS
-    int ret = persist::asset_type::TUNKNOWN;
-    if (cut == "datacenter") {
-        ret = persist::asset_type::DATACENTER;
-    } else if (cut == "room") {
-        ret = persist::asset_type::ROOM;
-    } else if (cut == "row") {
-        ret = persist::asset_type::ROW;
-    } else if (cut == "rack") {
-        ret = persist::asset_type::RACK;
-    } else if (cut == "group") {
-        ret = persist::asset_type::GROUP;
-    } else if (cut == "device") {
-        ret = persist::asset_type::DEVICE;
-    }
-    return ret;
-}
-
