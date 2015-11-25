e03ba6260c15facd3f48b991ab423baa25bf1822
Jim Klimov <jim@jimklimov.com>
2015-11-25 15:13:49 +0100

    testlib.sh : report test_it() and print_results() a bit more visibly
    Signed-off-by: Jim Klimov <jim@jimklimov.com>

--- tests/CI/testlib.sh
+++ tests/CI/testlib.sh
@@ -93,6 +93,7 @@ print_result() {
         TESTLIB_LASTTESTTAG="`echo "$NAME::$TNAME(${_ret})" | sed 's, ,__,g'`"
     fi
 
+    logmsg_info "Completed test $TNAME :"
     if [ "$_ret" -eq 0 ]; then  # should include "-0" too
         echo " * PASSED"
         TESTLIB_COUNT_PASS="`expr $TESTLIB_COUNT_PASS + 1`"
@@ -143,6 +144,7 @@ print_result() {
 test_it() {
     if [ x"${_testlib_result_printed}" = xnotyet ]; then
         logmsg_warn "Starting a new test_it() while an old one was not followed by a print_result()!"
+        logmsg_warn "Closing old test with result code 128 ..."
         print_result 128
     fi
     _testlib_result_printed=notyet
@@ -153,9 +155,9 @@ test_it() {
     [ -n "$TNAME" ] || TNAME="$0"
     TNAME="`basename "$TNAME" .sh | sed 's, ,__,g'`"
     if [ "$TNAME" = "`basename $NAME .sh`" ]; then
-        echo "Running test $TNAME :"
+        logmsg_info "Running test $TNAME :"
     else
-        echo "Running test $NAME::$TNAME :"
+        logmsg_info "Running test $NAME::$TNAME :"
     fi
     TESTLIB_COUNT_TOTAL="`expr $TESTLIB_COUNT_TOTAL + 1`"
 }
