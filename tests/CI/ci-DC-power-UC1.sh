9a3b1cd9ab4466f3b448f22f558ae98b43729f20
Jim Klimov <jim@jimklimov.com>
2015-12-30 07:15:26 +0100

    Simplify generic CI RESTAPI enabled scripts to just call accept_license() from weblib.sh
    Signed-off-by: Jim Klimov <jim@jimklimov.com>

--- tests/CI/ci-DC-power-UC1.sh
+++ tests/CI/ci-DC-power-UC1.sh
@@ -202,12 +202,7 @@ start_bios_daemons
 start_tntnet
 
 # Try to accept the BIOS license on server
-( . $CHECKOUTDIR/tests/CI/web/commands/00_license-CI-forceaccept.sh.test 5>&2 ) || \
-    if [ x"$CITEST_QUICKFAIL" = xyes ] ; then
-        die "BIOS license not accepted on the server, subsequent tests will fail"
-    else
-        logmsg_warn "BIOS license not accepted on the server, subsequent tests may fail"
-    fi
+accept_license
 
 logmsg_info "starting the test"
 
