07044a5d4d1e8470aaa0e9a01891102baf315c60
Jim Klimov <jim@jimklimov.com>
2016-01-21 18:45:41 +0100

    CI/VTE scripts using settraps() are revised to use ERRCODE and not reinvent the wheel
    Signed-off-by: Jim Klimov <jim@jimklimov.com>

--- tests/CI/vte-test_web.sh
+++ tests/CI/vte-test_web.sh
@@ -138,7 +138,8 @@ PATH="$PATH:/sbin:/usr/sbin"
 
 # Note: this default log filename will be ignored if already set by caller
 init_summarizeTestlibResults "${BUILDSUBDIR}/tests/CI/web/log/`basename "${_SCRIPT_NAME}" .sh`.log" ""
-settraps 'wTRAP_RES=$?; exit_summarizeTestedScriptlets ; exit_summarizeTestlibResults $wTRAP_RES || exit $?; exit $wTRAP_RES'
+# ERRCODE is maintained by settraps()
+settraps 'exit_summarizeTestedScriptlets ; exit_summarizeTestlibResults $ERRCODE'
 
 # A bash-ism, should set the exitcode of the rightmost failed command
 # in a pipeline, otherwise e.g. exitcode("false | true") == 0
