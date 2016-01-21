bad1d94d27537bd09449cec61432986fe4761a32
Jim Klimov <jim@jimklimov.com>
2016-01-21 17:16:23 +0100

    asset_create_one_device.sh : missed a fix location for expected errors
    Signed-off-by: Jim Klimov <jim@jimklimov.com>

--- tests/CI/web/commands/asset_create_one_device.sh
+++ tests/CI/web/commands/asset_create_one_device.sh
@@ -71,18 +71,17 @@ done
 curlfail_pop
 
 # ERROR MESSAGES
+curlfail_push_expect_400
 
 echo "********* asset_create_one_device.sh **************************************************************"
 echo "********* ${No}. Request document have wrong format or error in the syntax ************************"
 echo "***************************************************************************************************"
 test_it "Request_document_have_wrong_format_or_error_in_the_syntax"
 
-curlfail_push_expect_400
 ent=feed
 PARAM='{"name":"'${ent}'_0","type":"device","sub_type":"'${ent}'","location":"'${loc}'","status":"active","business_critical":"yes","priority":"P1","ext":{"asset_tag":"TEST00'${SEQUE}'","address":"ASDF","serial_no":"ABCD00'${SEQUE}'","ip.1":::"10.229.5.'${SEQUE}'"}}'
 api_auth_post_json "/asset" "${PARAM}" >&5
 print_result $?
-curlfail_pop
 SEQUE="$(expr $SEQUE + 1)"
 No="$(expr $No + 1)"
 
@@ -102,18 +101,20 @@ echo "********* asset_create_one_device.sh *************************************
 echo "********* ${No}. Request document have keys that are not implemented yet **************************"
 echo "***************************************************************************************************"
 test_it "Request_document_have_keys_that_are_not_implemented_yet"
+curlfail_push_expect_4xx5xx
 PARAM='{"name":"'${ent}'_0","type":"device","sub_type":"'${ent}'","location":"'${loc}'","status":"active","business_critical":"yes","priority":"P1","nonexist":"item","ext":{"asset_tag":"TEST00'${SEQUE}'","address":"ASDF","serial_no":"ABCD00'${SEQUE}'","ip.1":"10.229.5.'${SEQUE}'"}}'
 api_auth_post_json "/asset" "${PARAM}" >&5
 print_result $?
 SEQUE="$(expr $SEQUE + 1)"
 No="$(expr $No + 1)"
+curlfail_pop
 fi
 
 #*#*#*#*#*#*# TODO : The response had another msg then is in RFC-11 - Request document has invalid syntax. key 'id' is forbidden to be used","code":48
 echo "********* asset_create_one_device.sh **************************************************************"
 echo "********* ${No}. Request document has key "id" ****************************************************"
 echo "***************************************************************************************************"
-test_it "Request_document_have_keys_that_are_not_implemented_yet"
+test_it "Request_document_has_key_by_ID"
 PARAM='{"name":"'${ent}'_0","type":"device","sub_type":"'${ent}'","location":"'${loc}'","status":"active","business_critical":"yes","priority":"P1","id":"item","ext":{"asset_tag":"TEST00'${SEQUE}'","address":"ASDF","serial_no":"ABCD00'${SEQUE}'","ip.1":"10.229.5.'${SEQUE}'"}}'
 api_auth_post_json "/asset" "${PARAM}" >&5
 print_result $?
@@ -121,9 +122,9 @@ SEQUE="$(expr $SEQUE + 1)"
 No="$(expr $No + 1)"
 
 echo "********* asset_create_one_device.sh **************************************************************"
-echo "********* ${No}. Request document key "type" has unsupported value ********************************"
+echo "********* ${No}. Request document key 'type' has unsupported value ********************************"
 echo "***************************************************************************************************"
-test_it "Request_document_key_"type"_has_unsupported_value"
+test_it "Request_document_key_type_has_unsupported_value"
 PARAM='{"name":"'${ent}'_0","type":"Dejvice","sub_type":"'${ent}'","location":"'${loc}'","status":"active","business_critical":"yes","priority":"P1","id":"item","ext":{"asset_tag":"TEST00'${SEQUE}'","address":"ASDF","serial_no":"ABCD00'${SEQUE}'","ip.1":"10.229.5.'${SEQUE}'"}}'
 api_auth_post_json "/asset" "${PARAM}" >&5
 print_result $?
@@ -149,6 +150,9 @@ api_auth_post_json "/asset" "${PARAM}" >&5
 print_result $?
 SEQUE="$(expr $SEQUE + 1)"
 No="$(expr $No + 1)"
+
+curlfail_pop
+### End of expected ERRORS
 
 echo
 echo "###################################################################################################"
