45a383a0bf59e64cb372be4f1c904d3269f75122
Jim Klimov <jim@jimklimov.com>
2016-01-21 16:44:48 +0100

    asset_create_one_device.sh : missed a fix location for push/pop balancing
    Signed-off-by: Jim Klimov <jim@jimklimov.com>

--- tests/CI/web/commands/asset_create_one_device.sh
+++ tests/CI/web/commands/asset_create_one_device.sh
@@ -19,6 +19,8 @@
 #  \brief  CI tests for asset create and delete DC
 #  \author Radomir Vrajik <RadomirVrajik@Eaton.com>
 init_script
+[ x"${JSONSH_CLI_DEFINED-}" = xyes ] || CODE=127 die "jsonsh_cli() not defined"
+
 No=1
 SEQUE=0
 ent=""
@@ -32,7 +34,7 @@ for cube in datacenter; do
     echo "***************************************************************************************************"
     test_it "Create_${cube}"
     PARAM='{"name":"'${cube}'_0","type":"'${cube}'","sub_type":"","location":"'${loc}'","status":"active","business_critical":"yes","priority":"P1","ext":{"asset_tag":"TEST00'${SEQUE}'","address":"ASDF","serial_no":"ABCD00'${SEQUE}'","ip.1":"10.229.5.'${SEQUE}'"}}'
-api_auth_post_json "/asset" "${PARAM}" >&5
+    api_auth_post_json "/asset" "${PARAM}" >&5
     print_result $?
     loc="$cube"_0
     SEQUE="$(expr $SEQUE + 1)"
@@ -44,7 +46,6 @@ for ent in feed ups genset server storage switch vm; do
     echo "********* ${No}. Create_${ent} ********************************************************************"
     echo "***************************************************************************************************"
     test_it "Create_${ent}"
-    curlfail_push_expect_noerrors
     PARAM='{"name":"'${ent}'_0","type":"device","sub_type":"'${ent}'","location":"'${loc}'","status":"active","business_critical":"yes","priority":"P1","ext":{"asset_tag":"TEST00'${SEQUE}'","address":"ASDF","serial_no":"ABCD00'${SEQUE}'","ip.1":"10.229.5.'${SEQUE}'"}}'
     api_auth_post_json "/asset" "${PARAM}" >&5
     print_result $?
@@ -52,7 +53,6 @@ for ent in feed ups genset server storage switch vm; do
     SEQUE="$(expr $SEQUE + 1)"
     No="$(expr $No + 1)"
 done
-curlfail_pop
 
 loc="datacenter_0"
 wpos="left"
@@ -62,12 +62,13 @@ for ent in epdu pdu; do
     echo "***************************************************************************************************"
     test_it "Create_${ent}"
     PARAM='{"name":"'${ent}'_0","type":"device","sub_type":"'${ent}'","location":"'${loc}'","status":"active","business_critical":"yes","priority":"P1","location_w_pos":"'${wpos}'","ext":{"asset_tag":"TEST00'${SEQUE}'","address":"ASDF","serial_no":"ABCD00'${SEQUE}'","ip.1":"10.229.5.'${SEQUE}'"}}'
-api_auth_post_json "/asset" "${PARAM}" >&5
+    api_auth_post_json "/asset" "${PARAM}" >&5
     print_result $?
     SEQUE="$(expr $SEQUE + 1)"
     No="$(expr $No + 1)"
     wpos="right"
 done
+curlfail_pop
 
 # ERROR MESSAGES
 
