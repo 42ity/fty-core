b73bb4f84060a15661aed9ad4180ba3e1d920b2f
Michal Vyskocil <michalvyskocil@eaton.com>
2016-02-09 12:50:04 +0100

    Problem: asset import got stuck
    Solution: Revert "Problem: asset information not available for third party agents"
    
    This reverts commit cdc71dcbfb08f76aaed58bcf3407b95d97d19f69.
    
    Signed-off-by: Michal Vyskocil <michalvyskocil@eaton.com>

--- src/agents/configure_inform/configure_inform.cc
+++ src/agents/configure_inform/configure_inform.cc
@@ -25,8 +25,6 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include "str_defs.h"
 #include "agents.h"
 
-#include <biosproto.h>
-
 static zhash_t*
 s_map2zhash (const std::map<std::string, std::string>& m)
 {
@@ -47,78 +45,27 @@ void
     if ( agent == NULL )
         throw std::runtime_error(" bios_agent_new () failed.");
 
-    // Hack for the new protocols
-    mlm_client_t *client = mlm_client_new ();
-    if (!client) {
-        bios_agent_destroy (&agent);
-        throw std::runtime_error(" mlm_client_new () failed.");
-    }
-
-    int r = mlm_client_connect (client, MLM_ENDPOINT, 5000, agent_name.c_str ());
-    if (r == -1) {
-        bios_agent_destroy (&agent);
-        mlm_client_destroy (&client);
-        throw std::runtime_error("mlm_client_connect () failed.");
-    }
     bios_agent_set_producer (agent, bios_get_stream_main());
-    mlm_client_set_producer (client, "ASSETS");
-
     for ( auto &oneRow : rows )
     {
         zhash_t *ext = s_map2zhash (oneRow.first.ext);
-        zhash_t *ext2 = zhash_dup (ext);
-
         ymsg_t *msg = bios_asset_extra_encode (oneRow.first.name.c_str(), &ext, oneRow.first.type_id,
 		oneRow.first.subtype_id, oneRow.first.parent_id, oneRow.first.status.c_str(),
 		oneRow.first.priority, static_cast<int8_t>(oneRow.second));
-
-        zhash_insert (ext2, "type",
-                (void*) persist::typeid_to_type (oneRow.first.type_id).c_str ());
-        zhash_insert (ext2, "subtype",
-                (void*) persist::subtypeid_to_subtype (oneRow.first.subtype_id).c_str ());
-        //TODO: what to do with parent_id?
-        zhash_insert (ext2, "status",
-                (void*) oneRow.first.status.c_str());
-        zhash_insert (ext2, "priority",
-                (void*) std::to_string (oneRow.first.priority).c_str ());
-
-        zmsg_t *zmsg = bios_proto_encode_asset (
-                NULL,
-                oneRow.first.name.c_str(),
-                "TODO",
-                ext2);
-        //TODO: maybe cleanup the zhash_t
-        zhash_destroy (&ext2);
-
-        if ( msg == NULL || zmsg == NULL)
+        if ( msg == NULL )
         {
             bios_agent_destroy (&agent);
-            mlm_client_destroy (&client);
             throw std::runtime_error("bios_asset_encode () failed.");
         }
-
         const std::string topic = "configure@" + oneRow.first.name;
         int rv = bios_agent_send (agent, topic.c_str(), &msg);
         if ( rv != 0 )
         {
-            zmsg_destroy (&zmsg);
             bios_agent_destroy (&agent);
-            mlm_client_destroy (&client);
             throw std::runtime_error("bios_agent_send () failed.");
         }
-
-        std::string subject = "@" + oneRow.first.name;
-        rv = mlm_client_send (client, subject.c_str (), &zmsg);
-        if (rv == -1)
-        {
-            bios_agent_destroy (&agent);
-            mlm_client_destroy (&client);
-            throw std::runtime_error("bios_agent_send () failed.");
-        }
-
     }
     bios_agent_destroy (&agent);
-    mlm_client_destroy (&client);
 }
 
 void
