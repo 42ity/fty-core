6b6b53a5915e34a93ca1eed4a02c53c8c80a553c
Jim Klimov <jim@jimklimov.com>
2016-04-01 14:47:37 +0200

    BIOS-1964 : preinstallimage-bios.sh : report IMGTYPE into image-version.txt too
    Signed-off-by: Jim Klimov <jim@jimklimov.com>

--- obs/preinstallimage-bios.sh
+++ obs/preinstallimage-bios.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#   Copyright (c) 2014-2015 Eaton
+#   Copyright (c) 2014-2016 Eaton
 #
 #   This file is part of the Eaton $BIOS project.
 #
@@ -557,8 +557,9 @@ esac
     echo "WARNING: Do not have /usr/share/bios/.git_details"
 
 # Timestamp the end of OS image generation
-echo "OSimage:build-ts: `LANG=C date -R -u`" > /usr/share/bios-web/image-version.txt || \
-    echo "WARNING: Could not record OBS image-building timestamp"
+echo "OSimage:build-ts: `LANG=C date -R -u`
+OSimage:img-type: $IMGTYPE" > /usr/share/bios-web/image-version.txt || \
+    echo "WARNING: Could not record OBS image-building timestamp and type"
 
 # Get rid of static qemu binaries needed for crossinstallation
 # TODO: Integrate this better into build-recipe-preinstallimage/init_buildsystem
