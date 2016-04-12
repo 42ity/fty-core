4d9c165bb8a93f8b96ad750047a293be97e38abc
Alena Chernikava <AlenaChernikava@Eaton.com>
2016-04-12 17:22:19 +0200

    Problem: BIOS-2023: I don't want to delete all ext attributes, just "not read only" Solution: introduce new function for "delete" ext attribute
    Signed-off-by: Alena Chernikava <AlenaChernikava@Eaton.com>

--- src/db/assets/assetd.h
+++ src/db/assets/assetd.h
@@ -69,6 +69,12 @@ db_reply_t
          a_elmnt_id_t asset_element_id);
 
 db_reply_t
+    delete_asset_ext_attributes_with_ro
+        (tntdb::Connection &conn,
+         a_elmnt_id_t asset_element_id,
+         bool read_only);
+
+db_reply_t
     delete_asset_element
         (tntdb::Connection &conn,
          a_elmnt_id_t asset_element_id);
