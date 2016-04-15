ee637acb63ff0724023cd2cf651c943276191600
Jim Klimov <jim@jimklimov.com>
2016-04-15 14:55:57 +0200

    ci-nut.sh and ci-rackpower.sh can now sleep less - updated agent-dbstore should flush into DB upon tiomeout
    Signed-off-by: Jim Klimov <jim@jimklimov.com>

--- tests/CI/ci-nut.sh
+++ tests/CI/ci-nut.sh
@@ -257,10 +257,10 @@ for UPS in $UPS1 $UPS2 ; do
 
         logmsg_debug "`date`: Sleeping some time and restarting agent-dbstore to propagate measurements and flush database..."
         sleep 18  # some time for propagating into DB (poll every 5s in nut actor + some time to process), and agent-dbstore can have its own delays to queue up writes (10 sec by default)
-        stop_dbstore
-        sleep 8
-        start_dbstore
-        sleep 8
+        #stop_dbstore
+        #sleep 8
+        #start_dbstore
+        #sleep 8
         logmsg_debug "`date`: Sleep time is over!"
 
         for i in $(seq 0 $PARAMSCNT); do
