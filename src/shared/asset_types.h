b726308391e06005005776f8e9d72572cfca5169
Alena Chernikava <AlenaChernikava@Eaton.com>
2016-06-02 18:05:28 +0200

    problem: BSIO-2293: add "sensor" subtype
    Signed-off-by: Alena Chernikava <AlenaChernikava@Eaton.com>

--- src/shared/asset_types.h
+++ src/shared/asset_types.h
@@ -59,7 +59,8 @@ enum asset_subtype {
     N_A = 11,
     /* ATTENTION: don't change N_A id. It is used as default value in initdb.sql for types, that don't have N_A */
     ROUTER,
-    RACKCONTROLLER
+    RACKCONTROLLER,
+    SENSOR
 };
 
 enum asset_operation
