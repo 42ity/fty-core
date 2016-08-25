4410dcebcfcbf204747250fe4d3b1f74001fe5f4
Michal Vyskocil <michalvyskocil@eaton.com>
2016-08-25 08:30:47 +0200

    Problem: mathematically educated ppl don't like 5th step of 4
    Solution: lets have 5 steps
    
    Signed-off-by: Michal Vyskocil <michalvyskocil@eaton.com>

--- src/shared/data.cc
+++ src/shared/data.cc
@@ -89,7 +89,7 @@ db_reply <db_web_element_t>
         log_debug ("connection was successful");
 
         auto basic_ret = persist::select_asset_element_web_byId(conn, id);
-        log_debug ("1/4 basic select is done");
+        log_debug ("1/5 basic select is done");
 
         if ( basic_ret.status == 0 )
         {
@@ -100,11 +100,11 @@ db_reply <db_web_element_t>
             log_warning (ret.msg.c_str());
             return ret;
         }
-        log_debug ("    1/4 no errors");
+        log_debug ("    1/5 no errors");
         ret.item.basic = basic_ret.item;
 
         auto ext_ret = persist::select_ext_attributes(conn, id);
-        log_debug ("2/4 ext select is done");
+        log_debug ("2/5 ext select is done");
 
         if ( ext_ret.status == 0 )
         {
@@ -115,11 +115,11 @@ db_reply <db_web_element_t>
             log_warning (ret.msg.c_str());
             return ret;
         }
-        log_debug ("    2/4 no errors");
+        log_debug ("    2/5 no errors");
         ret.item.ext = ext_ret.item;
 
         auto group_ret = persist::select_asset_element_groups(conn, id);
-        log_debug ("3/4 groups select is done, but next one is only for devices");
+        log_debug ("3/5 groups select is done, but next one is only for devices");
 
         if ( group_ret.status == 0 )
         {
@@ -130,13 +130,13 @@ db_reply <db_web_element_t>
             log_warning (ret.msg.c_str());
             return ret;
         }
-        log_debug ("    3/4 no errors");
+        log_debug ("    3/5 no errors");
         ret.item.groups = group_ret.item;
 
         if ( ret.item.basic.type_id == persist::asset_type::DEVICE )
         {
             auto powers = persist::select_asset_device_links_to (conn, id, INPUT_POWER_CHAIN);
-            log_debug ("4/4 powers select is done");
+            log_debug ("4/5 powers select is done");
 
             if ( powers.status == 0 )
             {
@@ -147,14 +147,14 @@ db_reply <db_web_element_t>
                 log_warning (ret.msg.c_str());
                 return ret;
             }
-            log_debug ("    4/4 no errors");
+            log_debug ("    4/5 no errors");
             ret.item.powers = powers.item;
         }
 
         // parents select
-        log_debug ("5/4 parents select");
+        log_debug ("5/5 parents select");
         ret.item.parents = s_get_parents (conn, id);
-        log_debug ("     5/4 no errors");
+        log_debug ("     5/5 no errors");
 
         ret.status = 1;
         return ret;
