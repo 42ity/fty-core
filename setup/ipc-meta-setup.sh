85e63398cf5f2cfda9251efdf4c34eea71ba828f
Michal Vyskocil <michalvyskocil@eaton.com>
2017-01-04 09:03:16 +0100

    Problem: New daemons use new config file location
    Solution: add a script which will move and link back the file, so system
    will just works in all cases
    
    Signed-off-by: Michal Vyskocil <MichalVyskocil@eaton.com>

---
+++ setup/ipc-meta-setup.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+#
+#   Copyright (c) 2014-2017 Eaton
+#
+#   This file is part of the Eaton 42ity project.
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along
+#   with this program; if not, write to the Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+#! \file    ipc-meta-setup.sh
+#  \brief   Script to manage modular initial self-configuration of an IPC
+#  \author  Michal Vyskocil <MichalVyskocil@Eaton.com>
+#  \author  Jim Klimov <EvgenyKlimov@Eaton.com>
+#  \author  Tomas Halman <TomasHalman@Eaton.com>
+#
+
+LC_ALL=C
+LANG=C
+TZ=UTC
+export LC_ALL LANG TZ
+
+BASEDIR="$(dirname $(readlink -f ${0}))"
+SETUPDIR=/var/lib/fty/ipc-meta-setup/
+
+die () {
+    echo "FATAL: " "${@}" >&2
+    exit 1
+}
+
+[ -n "${BASEDIR}" ] && [ -d "${BASEDIR}" ] || die "BASEDIR '${BASEDIR}' is not available"
+
+mkdir -p "${SETUPDIR}"
+
+ls -1 "${BASEDIR}"/[0-9]*.sh | sort | while read SCRIPT; do
+
+    SCRIPT_NAME="$(basename "${SCRIPT}")"
+    if [ -f "${SETUPDIR}/${SCRIPT_NAME}.done" ]; then
+        echo "SKIP: ${SCRIPT_NAME} has already succeeded before"
+    else
+        echo "APPLY: running ${SCRIPT_NAME}..."
+        ./${SCRIPT} || die "${SCRIPT}"
+        touch "${SETUPDIR}/${SCRIPT_NAME}.done"
+    fi
+
+done
