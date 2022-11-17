b85a0faa15c69c2616ff48a3e518196cdd528b4e
sinanyvs <sinanyavas4@gmail.com>
2022-11-17 09:44:27 +0000

    add fty-datetime.cfg file

--- setup/92-datetime-fty.sh
+++ setup/92-datetime-fty.sh
@@ -19,10 +19,20 @@
 #   with this program; if not, write to the Free Software Foundation, Inc.,
 #   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-FPATH='/etc/fty/fty-timedate.cfg'
-mkdir -p /etc/fty && touch $FPATH
-chown www-data $FPATH
-echo '#   fty-timezone config' > $FPATH
-echo '' >> $FPATH
-echo 'server = ""' >> $FPATH
-echo '    timezone = ""' >> $FPATH
+file_name='fty-datetime.cfg'
+file_dir='/etc/fty'
+file_path="$file_dir/$file_name"
+
+if [ ! -d "$file_dir" ]; then
+    mkdir -p "$file_dir"
+fi
+
+if [ ! -f "$file_path" ]; then
+    touch "$file_path"
+    cat > $file_path << EOF
+server
+    timezone = ""
+EOF
+fi
+
+exit 0
