34e8dc0722ec6a49e61354424832d87963150e57
François-Régis Degott <FrancoisRegisDegott@eaton.com>
2025-08-26 14:59:25 +0200

    setup/08-blacklist-modules.everytime.sh : SPI module blacklisted
    Signed-off-by: François-Régis Degott <FrancoisRegisDegott@eaton.com>

--- setup/08-blacklist-modules.everytime.sh
+++ setup/08-blacklist-modules.everytime.sh
@@ -12,30 +12,31 @@ fi
 # Create or overwrite the blacklist file
 echo "# Blacklist modules" > "$BLACKLIST_FILE"
 
-
-// ATH12K
-
 # Add ath12k module to the blacklist
+echo "\n# ATH12K" >> "$BLACKLIST_FILE"
 echo "blacklist ath12k" >> "$BLACKLIST_FILE"
 
-
-// WIFI
-
 # Add generic wifi module to the blacklist
+echo "\n# WIFI" >> "$BLACKLIST_FILE"
 echo "blacklist iwlwifi" >> "$BLACKLIST_FILE"
 
-
-// SND
-
+# Add generic & each snd module to the blacklist
+echo "\n# SND" >> "$BLACKLIST_FILE"
 # List of snd-related modules to blacklist
 SND_MODULES=$(lsmod | awk '/^snd/ {print $1}' | sort -u)
-# Add generic & each snd module to the blacklist
 echo "blacklist snd" >> "$BLACKLIST_FILE"
 for module in $SND_MODULES; do
     echo "blacklist $module" >> "$BLACKLIST_FILE"
 done
 
-//
+# Add spi modules to the blacklist
+echo "\n# SPI" >> "$BLACKLIST_FILE"
+echo "blacklist spi_amd_platform" >> "$BLACKLIST_FILE"
+echo "blacklist spi_bitbang" >> "$BLACKLIST_FILE"
+echo "blacklist spi_bcm2835" >> "$BLACKLIST_FILE"
+echo "blacklist spidev" >> "$BLACKLIST_FILE"
 
 
 echo "Blacklisted modules written to $BLACKLIST_FILE"
+
+exit 0
